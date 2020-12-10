f011b1d48326befde9cbc3cc295097f7c65318ce
dhakalsamundra <seapush02@gmail.com>
2020-12-10 13:17:45 +0200

    exercise finished upto 11.9

--- cypress/integration/examples/pokeman_test.js
+++ cypress/integration/examples/pokeman_test.js
@@ -5,4 +5,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokeman page can be navigate', function() {
+    cy.visit('http://localhost:8080/pokemon/ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
