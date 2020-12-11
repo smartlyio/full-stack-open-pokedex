e0aaa47f89c25937c7af87caa8766783b571d0bb
dhakalsamundra <seapush02@gmail.com>
2020-12-11 17:05:37 +0200

    rename

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+
 app.use(express.static('dist'))
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
