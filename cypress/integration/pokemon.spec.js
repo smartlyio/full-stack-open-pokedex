c97b4825568d050c61968d488e4963838464ad01
eelsie <elisa.bestetti@hs.fi>
2020-12-11 14:06:33 +0200

    11.9

---
+++ cypress/integration/pokemon.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+
+describe('Pokedex', function() {
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('nidorina').click()
+    cy.contains('nidorina')
+  })
+})
