88e6a01eee8e0d06f76ad5f3effb2c5212cc6e8b
Samundra <40796489+dhakalsamundra@users.noreply.github.com>
2020-12-12 10:55:36 +0200

    Update app.js
    add endpoint

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('2')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${PORT}`)
