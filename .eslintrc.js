94a6a78f76510147289438551349c23d0e36a4f3
John Lee <delphi1004@hanmail.net>
2020-12-13 14:03:06 +0900

    added exercise 11.07

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,10 +29,7 @@ module.exports = {
       "error",
         2
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
+    "linebreak-style": 0,
     "quotes": [
       "error",
       "single"
