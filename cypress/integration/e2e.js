352b4b01ef0abe01dbef242a6b63fadad74d913a
veliValentine <nicolas.valentine@helsinki.fi>
2020-12-16 05:23:23 +0200

    11.9

--- cypress/integration/e2e.js
+++ cypress/integration/e2e.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can open Ivysaur', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+
 })
