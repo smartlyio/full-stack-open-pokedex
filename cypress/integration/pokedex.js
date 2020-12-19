160186a3fd86b2e1e880b9262ec71fbef9172c9e
Lari Löfman <larwazor@gmail.com>
2020-12-19 16:28:32 +0200

    add e2e test to open pokepage, modify gitignore

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,10 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
