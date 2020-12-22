e6f07253da6b734b4607225ff26e4278451fd1cc
Lari Löfman <larwazor@gmail.com>
2020-12-22 14:36:11 +0200

    test #skip

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('8') // change this string to ensure a new version deployed
+  res.send('9') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
