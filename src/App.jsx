b4132ee45a215f4c8b8eec394bfbcf7e7699b4ed
John <delphi1004@hanmail.net>
2020-12-22 18:31:22 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
   id: parseInt(url.match(/\/(\d+)\//)[1])
 })))
 
-const App = () => {
+const AppFailTest = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
   if (isLoading) {
     return <LoadingSpinner />
