449a9728523d7d8983ce9efde0dc51454b89151d
abkhale <abkhale.17@gmail.com>
2020-12-22 22:40:18 +0530

    disabled eslint errors

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -15,6 +16,7 @@
 /**
  * @type {Cypress.PluginConfig}
  */
+// eslint-disable-next-line no-undef
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
