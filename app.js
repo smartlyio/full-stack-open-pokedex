fe3c62ab0baeaf540e12f53120bbb8af485ed0fe
Tapani Honkanen <tapani.honkanen@hotmail.com>
2020-12-23 17:06:44 +0200

    Updated /version #skip

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('0.0.1') // change this string to ensure a new version deployed
+  res.send('0.0.5') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
