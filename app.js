390d6af024633ed21bfecc74582229f1e61f13b2
Layth Barzangi <thebarzangi@gmail.com>
2020-12-30 23:19:03 +0400

    Solved: Exercise 11.11 Added endpoint to facilitate health check during Heroku deployment step in pipeline workflow

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// health check
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
