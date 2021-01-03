3ddc2daddf2b94e6878449160728bdaf09135748
Emmanuel <emmopk11@outlook.com>
2021-01-03 02:26:59 +0100

    Added second test scenario and fixed steps typo

--- cypress/integration/PokemonPage.js
+++ cypress/integration/PokemonPage.js
@@ -5,4 +5,17 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate from main page to individual pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('pidgeotto').click()
+    cy.contains('speed')
+    cy.contains('71')
+    cy.contains('special defense')
+    cy.contains('50')
+    cy.contains('Hidden ability')
+    cy.contains('keen eye')
+    cy.contains('Hidden ability')
+    cy.contains('big pecks')
+  })
 })
