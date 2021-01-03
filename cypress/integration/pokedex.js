5e734c3b41c08aab56082b1dc545658675f2f888
ndeamador <ndeamador@gmail.com>
2021-01-04 01:17:53 +1100

    11.9 done (Part11b done)

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,13 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('one can navigate from main page to the page of a particular pokemon', function () {
+    cy.visit('http://localhost:5000')
+
+
+    cy.contains('pikachu').click()
+    cy.url().should('eq', 'http://localhost:5000/pokemon/pikachu')
+    cy.contains('lightning rod')
+  })
 })
