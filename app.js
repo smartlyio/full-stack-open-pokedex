a8df8394330924c4d87352fcb97dec76108573c1
Marcel <Marcel.Klotz@posteo.de>
2021-01-04 19:31:15 +0100

    fixed the bad extra endpoints

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const app = express()
 
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-
-app.get('/health', (req, res) => {
+const healthRouter = require('express').Router()
+healthRouter.get('/', (req, res) => {
   res.send('ok')
 })
 
-app.get('/version', (req, res) => {
+const versionRouter = require('express').Router()
+versionRouter.get('/', (req, res) => {
   res.send('1.0.1') // change this string to ensure a new version deployed
 })
+
+app.use('/health', healthRouter)
+app.use('/version', versionRouter)
+app.use(express.static('dist'))
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
