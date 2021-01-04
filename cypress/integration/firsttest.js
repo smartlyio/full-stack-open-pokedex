6051c021d8e08400e394d40f8154a2be4d149fd7
Marcel <Marcel.Klotz@posteo.de>
2021-01-04 17:21:25 +0100

    Part 11.9 part 2 - added second test.

--- cypress/integration/firsttest.js
+++ cypress/integration/firsttest.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('Pokemon details can be navigated to from Pokemon list', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
