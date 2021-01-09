d51c35c0e644b232d24e1ca16a4f0f320ecbdef5
ryankwei <rkwei@usc.edu>
2021-01-09 00:33:08 -0800

    e2e tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "es6": true,
     "jest/globals": true,
     "node": true,
+    "cypress/globals": true
     //"commonjs": true
   },
   "extends": [
@@ -18,7 +19,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
