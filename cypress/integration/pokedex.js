8e48dd38f82ddb1b537fa008f2c5e2890dc82ef9
JOuto <jouni.toni@gmail.com>
2021-01-10 18:09:45 +0200

    ->11.9

---
+++ cypress/integration/pokedex.js
@@ -0,0 +1,16 @@
+describe('Pokedex', function () {
+  beforeEach(function () {
+    cy.visit('http://localhost:5000');
+  });
+
+  it('front page can be opened', function () {
+    cy.contains('ivysaur');
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    );
+  });
+  it('page for single pokemon can be opened', function () {
+    cy.contains('charmander').click();
+    cy.contains('charmander');
+  });
+});
