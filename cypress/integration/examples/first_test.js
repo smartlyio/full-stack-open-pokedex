f7be3a4f47e8f0a5a25654350fb8a3bf59c1117f
Nasir Lawal <nasirlawal001@gmail.com>
2021-01-11 07:28:55 +0100

    Editing eslint for cypress

--- cypress/integration/examples/first_test.js
+++ cypress/integration/examples/first_test.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable indent */
 describe('Pokedex', function() {
     it('front page can be opened', function() {
         cy.visit('http://localhost:5000')
