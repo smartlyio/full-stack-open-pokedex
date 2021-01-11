8d13d3abb64fdf70f7d052c4936cbfa8d341a9b9
Nasir Lawal <nasirlawal001@gmail.com>
2021-01-11 07:45:49 +0100

    Fixed the eslint

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
