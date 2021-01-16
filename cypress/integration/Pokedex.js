25bb312c00fb8b09bd021671467ebe88addbc040
RomanVasilyev <roman.m.vasilyev@gmail.com>
2021-01-16 22:21:52 +0300

    Ex.11.9 - fixed lint errors in cypress files and added e2e test with navigation to a secondary page

--- cypress/integration/Pokedex.js
+++ cypress/integration/Pokedex.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Rattata page can be accessed from front page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('rattata').click()
+    cy.contains('rattata')
+    cy.contains('run away')
+    cy.contains('hustle')
+  })
 })
