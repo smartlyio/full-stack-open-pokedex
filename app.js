1fdbf24c4465c483837d1698e473a1be3cab4b8e
RomanVasilyev <roman.m.vasilyev@gmail.com>
2021-01-17 16:20:24 +0300

    Ex.11.11 - health check with wrong checkstring

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+const project_version = require('./package.json').version
 const express = require('express')
 const app = express()
 
@@ -5,6 +7,16 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log(project_version)
+  res.send(project_version)
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
