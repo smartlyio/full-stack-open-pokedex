4cbf43728c4067167751fa32e915bf40d1186871
Tambourin <olavi.hartonen@gmail.com>
2021-01-17 10:39:11 +0200

    Added second Cypress test

--- cypress/integration/pokemonTest.js
+++ cypress/integration/pokemonTest.js
@@ -1,9 +1,19 @@
 //import cy from 'cypress'
 
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.get('.pokemon-name').first().contains('ivysaur')
   })
 })
