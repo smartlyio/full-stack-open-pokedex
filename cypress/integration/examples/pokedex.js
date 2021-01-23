28adc295b77b9160cec1d22855a321bf79696f20
Admin <abdulgumi77@gmail.com>
2021-01-23 23:34:43 +0100

    finished exercise 11.9

--- cypress/integration/examples/pokedex.js
+++ cypress/integration/examples/pokedex.js
@@ -4,9 +4,4 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
-  
-  it('pokemon page can be navigated to', function() {
-    cy.contains('ivysaur').click()
-    cy.contains('chlorophyll')
-  })
 })
