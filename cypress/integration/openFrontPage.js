54366c206f3deda4073f8007e0c0a4891b31eadf
Sofia Bogdanova <sofia.bogdanova@kraftvaerk.com>
2021-01-30 15:12:14 +0300

    11.9 Simple end to end -tests

--- cypress/integration/openFrontPage.js
+++ cypress/integration/openFrontPage.js
@@ -4,15 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
-  it('pokemon can be navigated to'), function () {
-
-  }
-})
-
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  it('pokemon can be navigated to', function() {
     cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.contains('metapod').click()
+    cy.contains('shed skin')
   })
 })
