a750269c8ed9ef0823f0c3f0330035cabc4f5964
Laura Byman <laura.a.byman@gmail.com>
2021-02-03 13:53:46 +0200

    11.9 valmis

--- cypress/integration/pokedex_test.js
+++ cypress/integration/pokedex_test.js
@@ -6,3 +6,11 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokedex', function() {
+  it('pokemon details page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
