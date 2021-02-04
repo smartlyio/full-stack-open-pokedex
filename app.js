8e38bddb820bedc4cc4cfb82a5320d51f5681494
Laura Byman <laura.a.byman@gmail.com>
2021-02-04 12:12:24 +0200

    #skip  testing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
