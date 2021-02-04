05bf0ce07ebc812a21433aa3c09985319c430dc5
Divyaswor Makai <makaidivya@gmail.com>
2021-02-04 17:50:13 +0545

    Proc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     "node": true,
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -22,7 +23,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
---
+++ test/e2e.spec.jsx
@@ -0,0 +1,7 @@
+describe('Note app', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2020')
+  })
+})
