485398e9ab53c2c3c119e4d121c23cbd4a233e58
Ting Chen <chenting_cn@foxmail.com>
2021-02-07 12:53:25 +0200

    Exercises 11.9

---
+++ cypress/integration/Pokedex.js
@@ -0,0 +1,13 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('Pikachu page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('pikachu').click()
+    cy.contains('pikachu')
+    cy.contains('Home')
+  })
+})
