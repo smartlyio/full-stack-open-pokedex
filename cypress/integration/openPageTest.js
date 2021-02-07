9238a94326e16137bc3d3457c247be90441508c3
Jonas Vaquet(@wombat) <jonas@vaquet.de>
2021-02-07 14:03:23 +0200

    11.9

--- cypress/integration/openPageTest.js
+++ cypress/integration/openPageTest.js
@@ -1,7 +1,17 @@
 describe('Pokedex', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:5000')
+  })
+
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can open page for Ivysaur', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
   })
 })
