c76c66910d25a3a365dcdbc94512dc3486098053
Dicky59 <pekkaarvolaine@gmail.com>
2021-02-10 21:14:41 +0200

    11.11 Health check fixing

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
+})
+
 app.get('/health', (_req, res) => {
   res.send('ok')
 })
 
 app.get('/version', (_req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
-})
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  res.send('3.6') // change this string to ensure a new version deployed
 })
