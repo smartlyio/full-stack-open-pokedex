f03111c7dfc25c39d6f530531df7ab2e8a1646d2
Fabio Colella <fcole90@gmail.com>
2021-02-10 23:05:08 +0200

    Add rollback, expected to succeed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('KO')
+  res.send('OK')
 })
 
 app.get('/version', (req, res) => {
