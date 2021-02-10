43b9c2e35cd46acb7359e9c20f2b540347812978
Fabio Colella <fcole90@gmail.com>
2021-02-10 22:51:18 +0200

    Add rollback, expected to fail

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  res.send('KO')
 })
 
 app.get('/version', (req, res) => {
-  res.send('1')
+  res.send('added_rollback_on_fail')
 })
 
 app.listen(PORT, () => {
