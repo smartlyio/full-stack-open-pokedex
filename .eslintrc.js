5768ec160b97b437d03cbd369374496ea8410ef8
Dicky59 <pekkaarvolaine@gmail.com>
2021-02-10 13:33:01 +0200

    11.9 Fixing

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true,
-    "cypress": true
+    "jest/globals": true
   },
   "extends": [
     "eslint:recommended",
