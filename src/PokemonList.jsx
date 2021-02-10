0aae64a925c3435c989bdb2a43b8aa4f1d4efd47
ikohyppa <iko.hyppanen@gmail.com>
2021-02-10 21:23:07 +0200

    11.9 Simple end to end tests - individual pokemon page

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -5,7 +5,7 @@ const PokemonList = ({ pokemonList }) => {
   return (
     <div className="list-container">
       {pokemonList.map(({ id, name }) => (
-        <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
+        <Link id={name} key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
           <div
             className="list-item-name"
           >
