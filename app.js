093f4760a27616bc40d7f9ddf6747feebd622aa7
ikohyppa <iko.hyppanen@gmail.com>
2021-02-14 19:28:03 +0200

    11.15 - testing skip reading

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('23 master') // change this string to ensure a new version deployed
+  res.send('24 master') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
