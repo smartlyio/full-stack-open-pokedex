482b0f9c28b0efb6ecc8f5ae5b0a2b5043102257
ikohyppa <iko.hyppanen@gmail.com>
2021-02-14 11:11:28 +0200

    11.16 Adding versioning

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('15 feature-branch') // change this string to ensure a new version deployed
+  res.send('16 feature-branch') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
