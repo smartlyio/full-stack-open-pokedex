bcede64c83868f3632b72abb88b8def197aa86e4
ikohyppa <iko.hyppanen@gmail.com>
2021-02-14 17:30:57 +0200

    11.16 Adding versioning - tagging job

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('19 feature-branch') // change this string to ensure a new version deployed
+  res.send('20 feature-branch') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
