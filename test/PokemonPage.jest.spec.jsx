4e221c035e2f4820e332ab1c58776a7d7cdca440
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2021-02-23 17:00:00 +0530

    test changed

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -119,7 +119,7 @@ describe('<PokemonPage />', () => {
     })
 
     expect(screen.getByText('Previous')).toHaveAttribute('href', '/pokemon/ditto')
-    expect(screen.getByText('Next')).toHaveAttribute('href', '/pokemon/vaporeon')
+    expect(screen.getByText('Next')).toHaveAttribute('href', '/pokemon/ditto')
   })
 
   it('should not render previous and next urls if none exist', async () => {
