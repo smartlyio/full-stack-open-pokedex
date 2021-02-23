e79f1a2fc8ad1f4abd563187d272f9dc4a448a63
Markus Erapolku <markus.erapolku@gmail.com>
2021-02-23 07:43:17 +0200

    Make exercise 11.10

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
