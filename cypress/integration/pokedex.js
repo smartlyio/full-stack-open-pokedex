a143194f8449972bee220fc2c3df932156cd2585
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2021-02-23 18:09:21 +0530

    irvasur has ability cholorphyll

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,9 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
-  it(' ivysaur can be opened',function() {
+  it(' ivysaur can be opened and ability chlorophyll is found',function() {
 
     cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
 
