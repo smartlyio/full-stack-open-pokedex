7bc550eeb3fe819c76285e13b5192aa149072dae
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-24 01:17:37 -0800

    disable next line

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
