12dbad39eba5c7fdc3dbb7de52c7bbfad7f63412
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-02-25 20:04:57 +1100

    Add rollback on fail

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
