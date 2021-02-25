c122b10be05087bc18939b96ea9a216afbf158b1
Matti Aho <ookooko@gmail.com>
2021-02-25 07:49:59 +0200

    teht 11.9 torstain ratoksi taas. open-->run

--- cypress/integration/cypress_test.js
+++ cypress/integration/cypress_test.js
@@ -5,3 +5,10 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Navigation', function() {
+  it('navigates from main page to particular page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+  }
+})
