4511b23346bbb9a2356226f3341a498aa409c104
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-02-25 17:16:58 +1100

    Navigation test

--- cypress/integration/pokedex_app.js
+++ cypress/integration/pokedex_app.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('navigates from the main page to ivysaur', function () {
+    cy.get('a[href="/pokemon/ivysaur"').click()
+    cy.contains('chlorophyll')
+  })
 })
