b8ca845d6e8cde0555c315dabb49d57cd91dde07
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-02-25 18:59:32 +1100

    Add basic healthcheck

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
