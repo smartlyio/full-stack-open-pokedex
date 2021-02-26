8f0bab82ebe5f9ae03c3ef9922be8b14175a97e1
Greenlucid <greenfirechaos@gmail.com>
2021-02-26 20:23:52 +0100

    make it #skip snek

--- src/LoadingSpinner.jsx
+++ src/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const LoadingSpinner = () => (
-  <img className="loading-spinner" alt="Loading..." src="https://pbs.twimg.com/profile_images/561115148912295936/uWmwj2S2_400x400.jpeg" />
+  <img className="loading-spinner" alt="Loading..." src="https://pbs.twimg.com/profile_images/1250126900254715904/dmoNRGWb_400x400.jpg" />
 )
 
 export default LoadingSpinner
