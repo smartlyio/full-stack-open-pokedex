fb857e2ace8b79b43b976f67bf03d40f0206a8ce
Greenlucid <greenfirechaos@gmail.com>
2021-02-26 20:14:09 +0100

    rallo pina #skip

--- src/LoadingSpinner.jsx
+++ src/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const LoadingSpinner = () => (
-  <img className="loading-spinner" alt="Loading..." src="https://pbs.twimg.com/profile_images/1363839783370448902/YksqSHgK_400x400.jpg" />
+  <img className="loading-spinner" alt="Loading..." src="https://pbs.twimg.com/profile_images/561115148912295936/uWmwj2S2_400x400.jpeg" />
 )
 
 export default LoadingSpinner
