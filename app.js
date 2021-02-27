c0a284d1ff1c837ba0d2ba42d4cada1239a31d46
Matti Aho <ookooko@gmail.com>
2021-02-27 14:23:10 +0200

    tehtävää 11.15

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('77') // change this string to ensure a new version deployed
+  res.send('007') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
