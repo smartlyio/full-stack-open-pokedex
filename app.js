a818aa2a1cc928a02ea4947fb827808c5770caa2
Matti Aho <ookooko@gmail.com>
2021-02-28 07:44:12 +0200

    teht 11.16

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('7:27') // change this string to ensure a new version deployed
+  res.send('7:43') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
