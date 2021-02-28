f9baee16fd5ca2ddc630186730bba7101d8ff5e7
Matti Aho <ookooko@gmail.com>
2021-02-28 10:27:26 +0200

    joopas1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('10:26') // change this string to ensure a new version deployed
+  res.send('10:27') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
