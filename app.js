1e98d183d2351939fbc0cbc8831f3f9a6ec02922
Markku Kolkka <markku.kolkka@iki.fi>
2021-03-03 10:22:41 +0200

    Tehtävä 11.16 #minor

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('13') // change this string to ensure a new version deployed
+  res.send('14') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
