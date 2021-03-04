e271ec91a99d35581d4cfca0828a99b122b76622
Matti Aho <ookooko@gmail.com>
2021-03-04 02:01:32 +0200

    torstai branch

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1:50') // change this string to ensure a new version deployed
+  res.send('2:01') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
