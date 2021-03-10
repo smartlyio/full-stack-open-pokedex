6ba23fd6650b1cbe7eff28630ecd377efa6524aa
Matti Tuomikoski <mtuomikoski@gmail.com>
2021-03-10 13:46:47 +0200

    Add Ivysaur page test to e2e tests

--- cypress/integration/pokedex_app.js
+++ cypress/integration/pokedex_app.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Ivysaur page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
