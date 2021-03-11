7bd084529a44d36a991800aca146c583a2c8c574
SonTrungTo <son.trung.to@gmail.com>
2021-03-11 09:17:25 +0200

    added codeceptjs for testing...

---
+++ codeceptjs_helper.js
@@ -0,0 +1,26 @@
+const Helper = require('@codeceptjs/helper')
+
+class Codeceptjs extends Helper {
+
+  // before/after hooks
+  /**
+   * @protected
+   */
+  _before() {
+    // remove if not used
+  }
+
+  /**
+   * @protected
+   */
+  _after() {
+    // remove if not used
+  }
+
+  // add custom methods here
+  // If you need to access other helpers
+  // use: this.helpers['helperName']
+
+}
+
+module.exports = Codeceptjs
