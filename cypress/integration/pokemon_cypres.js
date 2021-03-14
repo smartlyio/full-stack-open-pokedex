c3b1022f8629eb440d482ca2b589440b4edd4d04
Petrimus <petrimus76@gmail.com>
2021-03-14 20:40:47 +0200

    cypress changed from open to run

--- cypress/integration/pokemon_cypres.js
+++ cypress/integration/pokemon_cypres.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('ivysaur form can be opened', function() {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
