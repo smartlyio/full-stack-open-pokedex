1150343b522b18a9d243e822e4bd0ebd9aeae7d8
Petrimus <petrimus76@gmail.com>
2021-03-14 19:36:20 +0200

    some cypres tests added

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     'node': true,
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
