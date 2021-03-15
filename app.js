449946ce8fa2eb76fe4af996f9cde136c694097b
leaf_hsiao <leaf_hsiao@outlook.com>
2021-03-15 14:54:10 +0800

    no message

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.get('/health', (req, res)) => {
+app.get('/health', (req, res) => {
   res.send('ok')
-}
+})
 
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
