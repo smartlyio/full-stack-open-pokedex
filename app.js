7119c2b198310c83c4f57483ecc836dca10b3d44
Petrimus <petrimus76@gmail.com>
2021-03-21 19:43:36 +0200

    skip 5

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ app.get('/version', (req, res) => {
   res.send('1.1') // change this string to ensure a new version deployed
 })
 
-app.get('/hello', (req, res) => {
-  res.send('hllo') // change this string to ensure a new version deployed
-})
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
