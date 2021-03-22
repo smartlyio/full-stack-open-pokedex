d72beb602eb3a9a449c037ba3a52d8837230b69e
Rami Piik <rami.piik@gmail.com>
2021-03-23 01:03:24 +0200

    11.9 Simple end to end -tests

--- cypress/integration/e2e-test.js
+++ cypress/integration/e2e-test.js
@@ -5,4 +5,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('it is possible to navigate from main page to charmander', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('charmander').click()
+    cy.contains('blaze')
+  })
 })
