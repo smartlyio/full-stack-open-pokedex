806b15b7323f3c5100406b0ef6daa8cd4978c9d2
Rami Piik <rami.piik@gmail.com>
2021-03-23 22:52:05 +0200

    x

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+console.log('hello world!')
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
