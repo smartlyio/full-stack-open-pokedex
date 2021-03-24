19d25e17ce2b175a2853ca92d447bad1642ae64f
Saska Rauhala <saskarauhala@MacBook-Pro-2.local>
2021-03-24 14:11:44 +0200

    new version5

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('35') // change this string to ensure a new version deployed
+  res.send('40') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
