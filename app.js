21bbc04fe856d027591a69b642e5f09ad8afb4e6
JuusoVe <juuso.vesanto@gmail.com>
2021-03-30 17:29:12 +0300

    #skip this time

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('10') // change this string to ensure a new version deployed
+  res.send('11') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
