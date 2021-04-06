a081d52a98eb6cc6cde489672660e7a96605e45c
giovanni gibelli <giogibelli@icloud.com>
2021-04-06 15:57:56 +1200

    exercise 11.9

--- cypress/integration/e2e.cypress.jsx
+++ cypress/integration/e2e.cypress.jsx
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('a pokemon page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+  })
 })
