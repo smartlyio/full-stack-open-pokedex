7231c7eb7071a6dba734e5638e771d86aa7088e2
Keinänen Markus <markusvk@protonmail.fi>
2021-04-12 16:22:53 +0300

    fixed errors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -7,6 +8,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-	// eslint-disable-next-line no-use-before-define
+	//eslint-disable-next-line
 	console.log('server started on port 5000')
 })
