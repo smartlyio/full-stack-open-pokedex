ed7b1b61a36b6622800c21fedd26e46562c9e635
Keinänen Markus <markusvk@protonmail.fi>
2021-04-12 16:47:00 +0300

    fixed eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
 	},
 	plugins: ['react', 'jest'],
 	rules: {
-		indent: ['error', 2],
+		indent: ['error', 'tab'],
 		'linebreak-style': ['error', 'unix'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'never'],
