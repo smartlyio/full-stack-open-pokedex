b4e0c77275a93697a8c375ec4f862a6faafb9b38
CakeCrusher <1sebastian1sosa1@gmail.com>
2021-04-17 15:46:23 -0500

    e2e test CI

--- cypress/integration/examples/DynamicPage.js
+++ cypress/integration/examples/DynamicPage.js
@@ -1,6 +1,6 @@
 describe('Ivysaur page', function() {
   it('pokemon page can be navigated to', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
