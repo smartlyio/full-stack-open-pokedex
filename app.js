f379a384b84d8d8ffb813f38cb10c516d234e41e
veravolkova <verap87@mail.ru>
2021-04-22 23:02:24 +0300

    fix heroku access

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`)
 })
