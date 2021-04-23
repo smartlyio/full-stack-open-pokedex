2f57bd272ba20077b2f65090fc9f8534c970b098
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-04-23 15:58:49 +0300

    ex 11.20 periodic tests 2nd test run

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 
 app.get('/version', (req, res) => {
   res.send('new release v9') // change this string to ensure a new version deployed
-  // another comment for ex 11.20 periodic tests (first with also push trigger)
+  // another comment for ex 11.20 periodic tests (first with also push trigger - run 2)
 })
 
 app.listen(PORT, () => {
