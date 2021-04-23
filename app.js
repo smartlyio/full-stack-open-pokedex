02687e7a3c88f93b9e62b472dbd7d8f572d74a2d
Tommi Tampio <tommi.teetee@hotmail.com>
2021-04-23 15:36:43 +0300

    haloo

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('version is 2') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
