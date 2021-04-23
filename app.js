ed0f5f54f2c293d92d12a2d4496ed4dd7c43345d
Tommi Tampio <tommi.teetee@hotmail.com>
2021-04-23 17:19:45 +0300

    test rollback

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  res.send('ol')
 })
 
 app.get('/version', (req, res) => {
-  res.send('version is 2') // change this string to ensure a new version deployed
+  res.send('version is 3') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
