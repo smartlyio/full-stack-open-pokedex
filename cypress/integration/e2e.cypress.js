dedd00665899fdde0fe6ae5a24b460023c1ec6e0
ndkv9 <vu.nguyen101@gmail.com>
2021-04-25 21:57:23 +0300

    modify e2e tests

--- cypress/integration/e2e.cypress.js
+++ cypress/integration/e2e.cypress.js
@@ -1,16 +1,14 @@
 /* eslint-disable no-undef, no-console*/
 describe('Pokedex', function () {
-	beforeEach(function () {
-		cy.visit('http://localhost:5000')
-	})
-
 	it('front page can be opened', function () {
+		cy.visit('http://localhost:5000')
 		cy.contains(
 			'Pokémon and Pokémon character names are trademarks of Nintendo.',
 		)
 	})
 	// navigating test
 	it('pokemon page can be navigated to', function () {
+		cy.visit('http://localhost:5000')
 		cy.contains('charmander').click()
 		cy.contains('solar power')
 	})
