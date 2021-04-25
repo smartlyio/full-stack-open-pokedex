8c4f0077db8ff806eb069c5821dd79d406473a61
ndkv9 <vu.nguyen101@gmail.com>
2021-04-25 21:26:32 +0300

    into windows invironment

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
 	plugins: ['react', 'jest', 'cypress'],
 	rules: {
 		indent: [2, 'tab'],
-		'linebreak-style': ['error', 'unix'],
+		'linebreak-style': ['error', 'windows'],
 		quotes: ['error', 'single'],
 		semi: ['error', 'never'],
 		eqeqeq: 'error',
