f4b878ba275bfd93595109c2e7969f93bbc69538
jethoo <jeewanbhusal@gmail.com>
2021-04-26 00:48:47 -0400

    Corrected some linting errors

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -16,6 +17,7 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-undef
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
