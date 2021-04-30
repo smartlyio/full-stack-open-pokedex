7e0278adb5df55459d39c98a18a9ee56976329c9
Jusa Mankki <jusamankki@gmail.com>
2021-04-30 11:58:52 +0300

    fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
       "error", { "before": true, "after": true }
     ],
     "no-console": "error",
-    "react/prop-types": 0
+    "react/prop-types": 0,
+    "no-undef": 0
   }
 }
