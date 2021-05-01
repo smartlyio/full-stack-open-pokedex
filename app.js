afc41fe4ce9132ce026e2408296426c7ac494099
Jusa Mankki <jusamankki@gmail.com>
2021-05-01 09:45:34 +0300

    try npm run start-prod instead of npm start

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`) // eslint-disable-line
+  console.log(`Server started on port ${PORT}`) // eslint-disable-line
 })
