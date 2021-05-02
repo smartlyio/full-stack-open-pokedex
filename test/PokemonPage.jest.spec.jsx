a58fe0e13d6d181f9aa517387d71b2ab592ec09d
ndkv9 <vu.nguyen101@gmail.com>
2021-05-02 13:08:32 +0300

    remove eslint warnings

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -104,7 +104,9 @@ describe('<PokemonPage />', () => {
 			)
 		})
 
-		expect(screen.getByTestId('stats')).toHaveTextContent('hp55attack55')
+		expect(screen.getByTestId('stats')).toHaveTextContent(
+			'hp55attack55',
+		)
 	})
 
 	it('should render previous and next urls if they exist', async () => {
