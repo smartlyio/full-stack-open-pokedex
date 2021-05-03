6ba80e2190b8bd7d14f6ed3281d005b5dab49fd3
Joel6677 <joel.heusala@gmail.com>
2021-05-03 15:10:46 +0300

    console fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": false,
     "react/prop-types": 0
   }
 }
