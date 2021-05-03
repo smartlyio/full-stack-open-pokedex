7b0675582306a395aa185af86cc62baa07f14af9
Joel6677 <joel.heusala@gmail.com>
2021-05-03 15:13:19 +0300

    more console rules fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": false,
+    "no-console": 0,
     "react/prop-types": 0
   }
 }
