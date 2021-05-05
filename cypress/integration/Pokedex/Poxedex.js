c60322f96a44b2d9da5b09cdd8df155cec6b55fc
SpLiYvIiI <atukadagio123@gmail.com>
2021-05-05 17:00:14 +0400

    Update Poxedex.js

--- cypress/integration/Pokedex/Poxedex.js
+++ cypress/integration/Pokedex/Poxedex.js
@@ -5,9 +5,9 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('can visit charmander', function(){
-    cy.visit('http://localhost:5000');
+    cy.visit('http://localhost:5000')
     cy.get('a[href="/pokemon/charmander"]').click()
     cy.contains('charmander')
-  }) 
-  
+  })
+
 })
