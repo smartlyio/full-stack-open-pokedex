c0c7c7100a9b3161f555bde0f7e43cfa56917abc
SpLiYvIiI <atukadagio123@gmail.com>
2021-05-06 16:29:57 +0400

    ss

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
 app.use(express.static('dist'))
 
