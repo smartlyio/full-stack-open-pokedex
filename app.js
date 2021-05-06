1c3fff2216d2926fe89645489d9c56d9c6c3918b
SpLiYvIiI <atukadagio123@gmail.com>
2021-05-06 16:25:41 +0400

    add healthcheck

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
+})
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
