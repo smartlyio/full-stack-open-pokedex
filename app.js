de218fd80e3d757e66979ad3f6d0c77c3287d6cc
Lawrence Onaulogho <onaulogholawrence@gmail.com>
2021-05-09 12:16:24 +0100

    fix:  error fixed

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable semi */
+/* eslint-disable quotes */
 const express = require("express");
 const app = express();
 
 // Heroku dynamically sets a port
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
