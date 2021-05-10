4c3b6ea675b7f2b03e38bd7846e1488aa4525cf7
Ojala Okko <okko.ojala@hiq.fi>
2021-05-10 11:53:25 +0300

    11.7

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
           const pokemonId = pokemonList.find(({ name }) => name === routeParams.match.params.name).id
           const previous = pokemonList.find(({ id }) => id === pokemonId - 1)
           const next = pokemonList.find(({ id }) => id === pokemonId + 1)
-          return <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
+          return <PokemonPage pokemonList={pokemonList} previous={previous ? previous : null} next={next ? next : null} />
         }} />
       </Switch>
     </Router>
