3d5bdf9e5972fe47c1c8a75247d9c84ae0a676c2
Ojala Okko <okko.ojala@hiq.fi>
2021-05-10 14:45:51 +0300

    Change pokemonpage e2e test

--- cypress/integration/frontpage.js
+++ cypress/integration/frontpage.js
@@ -1,4 +1,4 @@
-describe('Pokedex', function() {
+describe('Pokedex frontpage', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
--- cypress/integration/pokemonpage.js
+++ cypress/integration/pokemonpage.js
@@ -1,8 +1,9 @@
-describe('Pokedex', function() {
+describe('Pokedex Pokemon page', function() {
   it('Single pokemon page can be opened', function() {
     const pokemonName = 'ivysaur'
     cy.visit('http://localhost:5000')
-    cy.get(`.list-item:contains(${pokemonName})`).click()
-    cy.contains(pokemonName)
+    cy.get(`.list-item:contains(${pokemonName})`).click().then(() => {
+      cy.get('.pokemon-name').contains(pokemonName)
+    })
   })
 })
