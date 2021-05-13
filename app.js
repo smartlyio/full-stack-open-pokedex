16d9471bf7f59b656536fd89a578ad398f4d2441
Joel6677 <joel.heusala@gmail.com>
2021-05-13 14:42:20 +0300

    11.1

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
