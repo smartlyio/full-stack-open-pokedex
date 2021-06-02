a3bc0aaecbad412475d022d2332ba1abde0b3fbf
Oliver <oliverquixchan@gmail.com>
2021-06-01 18:10:01 -0600

    disable undef for cypress file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
