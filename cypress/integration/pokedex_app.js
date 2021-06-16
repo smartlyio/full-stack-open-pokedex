852fbe4229d931a5609b919c2ba4017a46746bb2
natasharp <natasa_sladojevic@yahoo.com>
2021-06-16 14:54:14 +0200

    Add cypress test

--- cypress/integration/pokedex_app.js
+++ cypress/integration/pokedex_app.js
@@ -6,3 +6,10 @@ describe('Pokedex', function () {
   })
 })
 
+describe('Pokedex', function() {
+  it('specific pokemon page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.get('.pokemon-ability-name').contains('chlorophyll')
+  })
+})
