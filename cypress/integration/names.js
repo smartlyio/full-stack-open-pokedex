b7c43f8970fb436c551382315a9b8cb74d6a18a9
JoakimRiistaniemi <79593942+JoakimRiistaniemi@users.noreply.github.com>
2021-06-21 16:20:42 +0300

    testing pipeline

--- cypress/integration/names.js
+++ cypress/integration/names.js
@@ -7,5 +7,6 @@ describe('Pokedex', function() {
   it('pokemon page can be opened', function() {
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
+    cy.contains('overgrow')
   })
 })
