f8804f0a4a0aa26cc1b1e69c228075c006ad8e2a
jethoo <jeewanbhusal@gmail.com>
2021-06-22 19:11:49 -0400

    added healthcheck endpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { useApi } from './useApi'
+import { useApi, getHealth } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
@@ -14,6 +14,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
+
   if (isLoading) {
     return <LoadingSpinner />
   }
