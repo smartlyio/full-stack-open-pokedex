16c10d60c3d323b1fcd7e69761de3e1c9b54f1da
Sagar <acharyasaagar@gmail.com>
2021-07-05 17:22:25 +0300

    Fixes typo issue in healthcheck uri Modifies app.js to return version number from package.json file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const express = require('express')
+const packageJson = require('./package.json')
 const app = express()
-const { exec } = require('child_process')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
@@ -17,20 +17,5 @@ app.get('/healthcheck', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  const query = req.query
-
-  const SHA_COMMAND =
-    query.short === 'true' ? 'git rev-parse --short HEAD' : 'git rev-parse HEAD'
-
-  exec(SHA_COMMAND, (error, stdout, stderr) => {
-    if (error) {
-      res.send(`error: ${error.message}`)
-      return
-    }
-    if (stderr) {
-      res.send(stderr)
-      return
-    }
-    res.send(stdout)
-  })
+  res.send(packageJson.version)
 })
