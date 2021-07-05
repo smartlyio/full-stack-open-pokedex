c3c22258ae31bc609ccc761fa86658601d2edf81
Sagar <acharyasaagar@gmail.com>
2021-07-05 16:51:44 +0300

    Adds healthcheck api and healthcheck step in deployment pipeline

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 const express = require('express')
 const app = express()
+const { exec } = require('child_process')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
@@ -7,6 +9,28 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
+})
+
+app.get('/healthcheck', (req, res) => {
+  res.send('okiee')
+})
+
+app.get('/version', (req, res) => {
+  const query = req.query
+
+  const SHA_COMMAND =
+    query.short === 'true' ? 'git rev-parse --short HEAD' : 'git rev-parse HEAD'
+
+  exec(SHA_COMMAND, (error, stdout, stderr) => {
+    if (error) {
+      res.send(`error: ${error.message}`)
+      return
+    }
+    if (stderr) {
+      res.send(stderr)
+      return
+    }
+    res.send(stdout)
+  })
 })
