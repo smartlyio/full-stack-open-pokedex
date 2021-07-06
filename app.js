7946c4ad2439a596e18a6babe9f2e222138207cb
Ehsun Amanolahi <ehsun.amanolahi@movial.com>
2021-07-06 13:40:20 +0300

    11.11 Health check

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
