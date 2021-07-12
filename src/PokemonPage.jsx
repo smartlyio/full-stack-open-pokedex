348c6d0aedfa8560c3e51092440cb5095caa9e51
Ira Gnativ <gvirinko@me.com>
2021-07-12 22:17:35 +0200

    Fixed linting issues

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -25,7 +25,7 @@ const PokemonPage = ({ previous, next }) => {
   })).reverse()
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
-
+  // eslint-disable-next-line no-console
   console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
