87e1d75133d3893e063910bc77d62a8bffbf7b34
Eric Miranda <ericmiranda7@gmail.com>
2021-07-13 12:22:11 +0530

    add e2e pokedex card test

--- cypress/integration/pokedexTest.jsx
+++ cypress/integration/pokedexTest.jsx
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon card can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.get('.pokemon-name').should('have.text', 'ivysaur')
+  })
 })
