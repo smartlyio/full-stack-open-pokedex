2ba1a28f14389513797ee0eb2414c5ea5e0dbfe1
Ville Turku <ville.turku99@gmail.com>
2021-07-15 22:41:02 +0300

    Test for visiting subapges

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('a particular pokemon can be visited from main page', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
