95cc6485ff88ccefe1f4052d8e6d2de28bdb836f
LauriKajakko <kajakko.lauri@gmail.com>
2021-07-19 23:33:23 +0300

    9

--- cypress/integration/End2End.js
+++ cypress/integration/End2End.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Pokemon page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('charizard').click()
+    cy.contains('solar power')
+  })
 })
