73ab488b69d61086b3b4f078d5fa2f743d06fa03
testi <web0testing0et@protonmail.com>
2021-07-23 08:37:38 +0300

    Endpoints

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+var uuid = require('uuid')
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
@@ -7,6 +8,14 @@ const PORT = process.env.PORT || 5000
 
 // testt234
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(uuid.v4()) // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
