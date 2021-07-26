81259c77d9a486119701dbe64d6acd058179bf00
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-07-26 20:45:43 +0200

    test e2e

--- cypress/integration/pekemonTestE2E.jsx
+++ cypress/integration/pekemonTestE2E.jsx
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('click in link pikachu and go intro in page pikachu', ()=>{
+    cy.visit('http://localhost:5000')
+    cy.get('[href="/pokemon/pikachu"]').click()
+    cy.get('.pokemon-name').contains('pikachu')
+    cy.get('.links > :nth-child(1)').click()
+    cy.get('.pokemon-name').contains('arbok')
+  })
+
 })
