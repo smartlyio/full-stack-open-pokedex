e8e3535b30178ef175462fcb615a9982958cac3b
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-07-26 17:39:36 +0200

    eslint confing

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true,
-    "cypress":true
+    "jest/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -17,7 +16,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "cypress", "react", "jest", 
   ],
   "rules": {
     "indent": [
