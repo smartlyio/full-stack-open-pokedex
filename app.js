3fb9474da4434a97c777a9ea2654ec12ecc83f5c
Juan Lopez <aragonlopezjuan87@gmail.com>
2021-07-27 19:35:40 +0200

    eee

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('todo bien') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
