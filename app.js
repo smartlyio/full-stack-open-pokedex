f72ae2704f148857342000bd53463f894d08ba22
AYAYAehehe <AYAYAehehe@pm.me>
2021-07-27 12:37:28 +0100

    eslint fix

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
+  /* eslint-disable no-console */
   console.log(`server started on port ${PORT}`);
+  /* eslint-enable no-console */
 });
