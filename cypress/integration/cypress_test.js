269c78ae79ff8b8caf984edb28c98709500b9388
samuelabc <samuelthien@yahoo.com>
2021-07-27 21:44:30 +0800

    add e2e test

--- cypress/integration/cypress_test.js
+++ cypress/integration/cypress_test.js
@@ -1,9 +1,18 @@
 describe("Pokedex", function () {
-  it("front page can be opened", function () {
+  // it("front page can be opened", function () {
+  //   cy.visit("http://localhost:5000");
+  //   cy.contains("ivysaur");
+  //   cy.contains(
+  //     "Pokémon and Pokémon character names are trademarks of Nintendo."
+  //   );
+  // });
+  it("ivysaur page can be open", function () {
     cy.visit("http://localhost:5000");
+    cy.contains("ivysaur")
+      .should("have.prop", "href")
+      .and("equal", "http://localhost:5000/pokemon/ivysaur");
+    cy.contains("ivysaur").click();
     cy.contains("ivysaur");
-    cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
-    );
+    cy.contains("chlorophyll");
   });
 });
