a25106dff64c96bf730985838e9bbbf219fb338e
Tommi <akuankka75@gmail.com>
2021-07-31 23:04:21 +0300

    11.9

---
+++ cypress/integration/pokemontest.js
@@ -0,0 +1,13 @@
+describe('Pokedex', function() {
+  beforeEach(function () {
+    cy.visit('http://localhost:5000')
+  })
+  it('front page can be opened', function() {
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('single page for pokemon can be opened', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
