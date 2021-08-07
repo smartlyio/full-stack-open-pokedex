e4be6ea55935e76904e10ac0675900d80657977c
olliglorioso <olli.glorioso@gmail.com>
2021-08-07 19:08:02 +0300

    11.6 pt4

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,37 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "jest/globals": true,
-    "node": true,
-    "commonjs": true
+  'env': {
+      'commonjs': true,
+      'es2021': true,
+      'node': true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+      'ecmaVersion': 12
   },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-        2
+  'rules': {
+      'indent': [
+          'error',
+          4
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
-    "react/prop-types": 0
+      'linebreak-style': [
+          'error',
+          'unix'
+      ],
+      'quotes': [
+          'error',
+          'single'
+      ],
+      'semi': [
+          'error',
+          'never'
+      ],
+      'eqeqeq': 'error',
+          'no-trailing-spaces': 'error',
+  'object-curly-spacing': [
+      'error', 'always'
+  ],
+  'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+  ]
   }
 }
