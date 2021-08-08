dd49afe3c0726d826c11cb83534dc7693043a991
olliglorioso <olli.glorioso@gmail.com>
2021-08-08 13:20:10 +0300

    11.9 pt3

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
         'sourceType': 'module'
     },
     'plugins': [
-        'react', 'jest'
+        'react', 'jest', 'cypress'
     ],
     'rules': {
         'indent': [
