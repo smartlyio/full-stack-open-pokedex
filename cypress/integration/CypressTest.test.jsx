f934ac27599df0c6b627a2f8d239dcc4135efec5
olliglorioso <olli.glorioso@gmail.com>
2021-08-08 19:12:38 +0300

    11.9

--- cypress/integration/CypressTest.test.jsx
+++ cypress/integration/CypressTest.test.jsx
@@ -1,12 +1,14 @@
+import {cy} from 'cypress'
+
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
-  // it('information of particular pokemons are reachable', function() {
-  //   cy.visit('http://localhost:5000')
-  //   cy.contains('ivysaur').click()
-  //   cy.contains('60')
-  // })
+  it('information of particular pokemons are reachable', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('60')
+  })
 })
