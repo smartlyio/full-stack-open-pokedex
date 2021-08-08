61b50046c5db602efdbf73999d23687b61fb3bcf
olliglorioso <olli.glorioso@gmail.com>
2021-08-08 17:20:43 +0300

    11.9

--- cypress/integration/CypressTest.test.jsx
+++ cypress/integration/CypressTest.test.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable jest/expect-expect */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-undef */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
