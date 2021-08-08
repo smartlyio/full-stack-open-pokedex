5db00b0db106e913e9c513254009150ff6f2a0b3
olliglorioso <olli.glorioso@gmail.com>
2021-08-08 10:27:01 +0300

    11.6 pt 13

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,45 @@
 module.exports = {
-  'env': {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true,
-    "node": true,
-    "commonjs": true
-  },
-  'extends': ['eslint:recommended', "plugin:jest/recommended"],
-  'parserOptions': {
-      'ecmaVersion': 6
-  },
-  'rules': {
-      'indent': [
-          'error',
-          4
-      ],
-      'linebreak-style': [
-          'error',
-          'unix'
-      ],
-      'quotes': [
-          'error',
-          'single'
-      ],
-      'semi': [
-          'error',
-          'never'
-      ],
-      'eqeqeq': 'error',
-          'no-trailing-spaces': 'error',
-  'object-curly-spacing': [
-      'error', 'always'
-  ],
-  'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-  ]
-  }
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true,
+        "jest/globals": true
+    },
+    'extends': ['plugin:react/recommended', 'plugin: jest/recommended', 'eslint:recommended'],
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react', 'jest'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'double'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'eqeqeq': 'error',
+            'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+        'error', 'always'
+    ],
+    'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+    ]
+    }
 }
