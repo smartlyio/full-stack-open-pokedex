5981af7984db7d6502abb894b9e2713d0ba3132d
Pia Ruusukivi <ruusukivi@gmail.com>
2021-08-08 19:36:01 +0300

    11.9

--- cypress/integration/pokemon-tests.js
+++ cypress/integration/pokemon-tests.js
@@ -3,8 +3,5 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    cy.request('pokemon/charmeleon')
-    cy.contains('charmeleon')
-
   })
 })
