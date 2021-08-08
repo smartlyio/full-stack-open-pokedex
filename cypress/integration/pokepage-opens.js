86516521c8b9b6b5c65d06a1a88ca5136a118ce2
Pia Ruusukivi <ruusukivi@gmail.com>
2021-08-08 19:28:15 +0300

    11.9

--- cypress/integration/pokepage-opens.js
+++
@@ -1,8 +0,0 @@
-describe('Pokedex2', function() {
-  it('pokepage can be opened', function() {
-    cy.visit('http://localhost:5000')
-    cy.request('pokemon/ivysaur')
-    cy.contains('chlorophyll')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })
-})
