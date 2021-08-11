fc67763dbcf85b8d979cc499464a98176f0c163f
Rasmus Paltschik <rjpalt@gmail.com>
2021-08-11 16:11:11 +0300

    11.10 initial

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('a given pokemon can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('charmander').click()
+    cy.contains('blaze')
+    cy.contains('solar power')
+  })
 })
