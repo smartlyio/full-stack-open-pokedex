78246c468241006f68ca4a4b08418f199093eb41
jiiaa <jukkaa@live.com>
2021-08-17 15:38:20 +0300

    Add new e2e test. Remove useless specs.

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be navigated to', function() {
+    cy.get('.list-item').eq(1).click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
