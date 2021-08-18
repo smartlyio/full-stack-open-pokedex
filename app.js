1ba5337e071e46212907c3fba3f2ce2593dae241
jiiaa <jukkaa@live.com>
2021-08-18 19:11:56 +0300

    Add healthceck to Heroku deployment

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const app = express()
 const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('v1.0')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log("server started on port", PORT);
