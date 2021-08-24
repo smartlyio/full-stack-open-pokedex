957ffa1415bd999c03d1f4398b98c182f30f1581
JiiT2020 <jtenhun@gmail.com>
2021-08-24 12:17:26 +0300

    1.16 full

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   "env": {
-    "node": true,
-    'commonjs': true,
-    'es2021': true,
+    "browser": true,
+    "node": true, 
     "es6": true,
     "jest/globals": true
   },
