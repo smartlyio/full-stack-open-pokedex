0feed41f561e48d57d6f34bc5f0463552b8bbf35
JiiT2020 <jtenhun@gmail.com>
2021-08-27 15:02:12 +0300

    11.12 simuloidaan virhe

--- app.js
+++ app.js
@@ -15,5 +15,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
