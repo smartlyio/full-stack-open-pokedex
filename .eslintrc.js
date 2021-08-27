2ec1222352a7d65205af37d895838020a1f7a72e
Pekka Tuukkanen <petuukka@gmail.com>
2021-08-27 16:44:18 +0300

    ex 11.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
