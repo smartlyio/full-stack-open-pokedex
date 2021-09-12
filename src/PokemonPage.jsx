f7f4df7d54aed5419e0c87171cb3d9eab9bf90bc
dank <dan.khalid3@gmail.com>
2021-09-12 17:48:06 -0400

    fix lint errors

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -9,7 +9,11 @@ const formatName = (nameWithDash) => nameWithDash.replace('-', ' ')
 
 const PokemonPage = ({ previous, next }) => {
   const { name } = useParams()
-  const { data: pokemon, error, isLoading } = useApi(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  const {
+    data: pokemon,
+    error,
+    isLoading,
+  } = useApi(`https://pokeapi.co/api/v2/pokemon/${name}`)
 
   if (isLoading) {
     return <LoadingSpinner />
@@ -19,40 +23,56 @@ const PokemonPage = ({ previous, next }) => {
   }
 
   const { type } = pokemon.types.find((type) => type.slot === 1)
-  const stats = pokemon.stats.map((stat) => ({
-    name: formatName(stat.stat.name),
-    value: stat.base_stat
-  })).reverse()
+  const stats = pokemon.stats
+    .map((stat) => ({
+      name: formatName(stat.stat.name),
+      value: stat.base_stat,
+    }))
+    .reverse()
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
-  const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
+  const hiddenAbility = pokemon.abilities.find(
+    (ability) => ability.is_hidden === true
+  )
 
+  // eslint-disable-next-line no-console
   console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
-      <div className="links">
+      <div className='links'>
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
-        <Link to="/">Home</Link>
+        <Link to='/'>Home</Link>
         {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
-        <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
-        <div className="pokemon-info">
-          <div className="pokemon-name">{pokemon.name}</div>
-          <div className="pokemon-stats" data-testid="stats">
+        <div
+          className='pokemon-image'
+          style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }}
+        />
+        <div className='pokemon-info'>
+          <div className='pokemon-name'>{pokemon.name}</div>
+          <div className='pokemon-stats' data-testid='stats'>
             <table>
               <tbody>
                 {stats.map(({ name, value }) => (
                   <tr key={name}>
-                    <td className="pokemon-stats-name">{name}</td>
-                    <td className="pokemon-stats-value">{value}</td>
+                    <td className='pokemon-stats-name'>{name}</td>
+                    <td className='pokemon-stats-value'>{value}</td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-          <div className="pokemon-abilities">
-            {normalAbility && <PokemonAbility abilityName={formatName(normalAbility.ability.name)} />}
-            {hiddenAbility && <PokemonAbility abilityName={formatName(hiddenAbility.ability.name)} />}
+          <div className='pokemon-abilities'>
+            {normalAbility && (
+              <PokemonAbility
+                abilityName={formatName(normalAbility.ability.name)}
+              />
+            )}
+            {hiddenAbility && (
+              <PokemonAbility
+                abilityName={formatName(hiddenAbility.ability.name)}
+              />
+            )}
           </div>
         </div>
       </div>
