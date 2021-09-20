10a43f64e6ecd8a285e18328c5fca6d5607c507c
Riku Sarlin <riku.sarlin@gmail.com>
2021-09-20 21:30:48 +0300

    First e2e test with Cypress

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const dotenv = require('dotenv')
+require('dotenv').config()
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = dotenv.process.env.PORT || 5000
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
