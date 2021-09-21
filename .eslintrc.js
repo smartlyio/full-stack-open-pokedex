0d85260c4d22344e2baca605b2cfe4f145ba173c
Haksa6 <hakonenweeti@gmail.com>
2021-09-21 11:44:22 +0300

    11.6 Fix the code

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,10 +31,6 @@ module.exports = {
       "error",
       "single"
     ],
-    "semi": [
-      "error",
-      "never"
-    ],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": [
