02afc19edfa0879e159ff43ae452bc9c14eeb39c
tonghe <tonghe84@gmail.com>
2021-09-30 20:45:39 +0200

    made changes to be reivewed

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 5000 // eslint-disable-line
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('nokey')
+  res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('wrong version')
+  res.send('2')
 })
 
 app.listen(PORT, () => {
