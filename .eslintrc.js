de9bfc71ced6ff6b05e163b10e8833e847458a2b
ronituohino <tuohinoroni@gmail.com>
2021-09-30 19:28:38 +0300

    Fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    "node": true
+    "node": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -17,13 +18,13 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
       "error",
-        2
-      ],
+      2
+    ],
     "linebreak-style": [
       "error",
       "unix"
--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,15 +1,17 @@
 describe('Pokedex', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:5000')
-    })
+  beforeEach(() => {
+    cy.visit('http://localhost:5000')
+  })
 
-    it('front page can be opened', () => {
-        cy.contains('ivysaur')
-        cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    })
+  it('front page can be opened', () => {
+    cy.contains('ivysaur')
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
 
-    it('pokemon info can be opened', () => {
-        cy.contains('charmander').click()
-        cy.contains('blaze')
-    })
+  it('pokemon info can be opened', () => {
+    cy.contains('charmander').click()
+    cy.contains('blaze')
+  })
 })
