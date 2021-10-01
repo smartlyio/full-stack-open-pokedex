6e9b169a45b3e1d0e70331b200de235d94c7a235
tonghe <tonghe84@gmail.com>
2021-10-01 23:48:08 +0200

    Removed that damn semicolon

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/health', (req, res) => {
 app.get('/version', (req, res) => {
   // Let's create a linting error
   // by adding a semicolon
-  res.send('17:12 — This commit will be deployed version number will increase by one.');
+  res.send('23:47 — This commit will be deployed version number will increase by one.')
 })
 
 app.listen(PORT, () => {
