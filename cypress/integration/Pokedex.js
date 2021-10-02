b90f4891269ebcf4cb67f26a7345b2144c3d3dc6
Shawn <codingshawn@gmail.com>
2021-10-02 21:36:54 +0800

    Added test to check if able to view correct information for ivysaur

--- cypress/integration/Pokedex.js
+++ cypress/integration/Pokedex.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('able to view correct information for ivysaur', function() {
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
