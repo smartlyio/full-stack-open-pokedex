f7fc49fe4c29c63ec9dac2a760598275515a8a2b
Jonas <youneslamkhanter02@gmail.com>
2021-10-12 18:10:59 +0100

    Solve Lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     "jest/globals": true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "cypress"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
