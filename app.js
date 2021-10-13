9386b800aa2c06fa890a9b095c054f7395b2e105
xavier <laishaoliang@sina.cn>
2021-10-13 15:13:26 +0800

    d

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-console */
 const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-// eslint-disable-next-line no-undef
-// const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000;
 
 app.use(express.static('dist'))
 
 // eslint-disable-next-line no-undef
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env)
+})
