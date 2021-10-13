e2c28360f2170d3c342932c8fb40aa3eb1497bf4
xavier <laishaoliang@sina.cn>
2021-10-13 16:07:04 +0800

    ud

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = express()
 
 app.use(express.static('dist'))
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Our app is running on port ${ PORT }`)
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80
+var server_host = process.env.YOUR_HOST || '0.0.0.0'
+server.listen(server_port, server_host, function() {
+  console.log('Listening on port %d', server_port)
 })
