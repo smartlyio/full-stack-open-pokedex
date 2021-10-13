6cc204f9b1442150d3ebf574ceaa0b6f21644bb4
Jonas <youneslamkhanter02@gmail.com>
2021-10-13 15:52:18 +0100

    Update server default port

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get("/version", (req, res) => {
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log("server started on port 5000");
+  console.log(`server started on port ${PORT}`);
 });
