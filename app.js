46e771cece03a02aa918d405cfb894168f263617
xavier <laishaoliang@sina.cn>
2021-10-13 15:05:25 +0800

    ud

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const app = express()
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  // libe
-  // console.log('server started on port 5000') change
-})
+// eslint-disable-next-line no-undef
+app.listen(process.env.PORT || 5000)
