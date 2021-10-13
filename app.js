133d3debf637d951ef38d40aa80e5e417e7e6de1
xavier <laishaoliang@sina.cn>
2021-10-13 16:11:14 +0800

    dd

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express()
 
 app.use(express.static('dist'))
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80
-var server_host = process.env.YOUR_HOST || '0.0.0.0'
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5001
+var server_host = '0.0.0.0'
 server.listen(server_port, server_host, function() {
   console.log('Listening on port %d', server_port)
 })
