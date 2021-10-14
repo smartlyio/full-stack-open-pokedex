eda18bee7ffc64ba9bca4348adb8f43aa3fca685
Jonas <youneslamkhanter02@gmail.com>
2021-10-14 15:22:49 +0100

    Update app #patch

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
-app.get("/health", (req, res) => {
-  res.send("ok");
-});
-
 app.get("/version", (req, res) => {
   res.send("1.1.0"); // change this string to ensure a new version deployed
 });
