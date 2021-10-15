ce1d9fb67e9eb0294602fc83293bc4c0e5c49990
teukkis <teemu.varsala@gmail.com>
2021-10-15 19:28:54 +0300

    ..

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -18,7 +19,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest",
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
