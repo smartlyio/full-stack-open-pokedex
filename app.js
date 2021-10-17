8ad6df880209c657b2a5692aafa68b8b8f4d4d28
Riku Sarlin <riku.sarlin@gmail.com>
2021-10-17 21:29:55 +0300

    Exercise 11.11 added health check and version endpoints

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
