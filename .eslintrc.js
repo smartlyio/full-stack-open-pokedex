7cadc370ea76baecfab87ea3662b46a4b810818c
=recedivies <ahmadhiprananta@gmail.com>
2021-10-19 21:44:57 +0700

    e2e test for individual pokemon

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
     es6: true,
     "jest/globals": true,
     node: true,
+    jest: true,
+    "cypress/globals": true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
@@ -13,7 +15,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "jest"],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
