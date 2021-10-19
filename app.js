c09836624dfd9cf783d81aac71099a5c63d8082a
litemars <maxmassi12@gmail.com>
2021-10-19 11:53:08 +0300

    wrong place

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 
 app.use(express.static('dist'))
 
