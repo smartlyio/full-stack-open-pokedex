c0139783dada51e0ea37fb978a14571da8074652
=recedivies <ahmadhiprananta@gmail.com>
2021-10-20 18:45:24 +0700

    Update Version

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.get("/health", (req, res) => {
   res.send("ok");
 });
 
-app.get("/version", (req, res) => {
-  res.send("1"); // change this string to ensure a new version deployed
-});
-
 app.listen(PORT, () => {
   console.log("server started on port 5000");
 });
