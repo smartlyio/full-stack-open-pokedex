91b71f28d9d7d7edbe137bcecabfa4cb82d5b643
Pavel <imran.pavel@gmail.com>
2021-10-29 09:51:18 +0300

    11.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    'node': true
+    'node': true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
