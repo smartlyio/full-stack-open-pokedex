204eea0bb62e335605606a0e89976e179562477c
willydownhouse <ville.l.alatalo@gmail.com>
2021-11-15 15:57:03 +0100

    another test to poke_app.js

--- cypress/integration/poke_app.js
+++ cypress/integration/poke_app.js
@@ -6,4 +6,11 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('single pokemon view can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('venusaur').click()
+    cy.contains('venusaur')
+    cy.contains('chlorophyll')
+  })
 })
