abf65fb3a49ed35e5ed6fb5c586b374208acd208
pavel <pashov.pavel@gmail.com>
2021-11-15 13:39:17 +0200

    added health check to heroku deploy workflow

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = express()
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
