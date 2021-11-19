efa47531768b9bd1ece0c6687570a18487e952e6
willydownhouse <ville.l.alatalo@gmail.com>
2021-11-19 10:21:52 +0100

    haloo lyngen #skip

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('6') // change this string to ensure a new version deployed
+  res.send('99') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
