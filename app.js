ccd79f78cdba41593e029f005988e4a310542e20
Pasang Nuru Sherpa <79486506+solu22@users.noreply.github.com>
2021-11-22 02:57:47 +0200

    prod test on main branch

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('Heroku version is working') // change this string to ensure a new version deployed
+  res.send('Prod test') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
