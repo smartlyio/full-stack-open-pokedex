4fcca4cb85b8776405bf81c0f83b7287c695e0db
ojporv <olli.porvari@helsinki.fi>
2021-11-23 09:54:08 +0200

    11.9 tehty

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,7 +1,13 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+describe('Pokedex', () => {
+  it('front page can be opened', () => {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('certain pokemon page can be navigated to', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
