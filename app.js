c06df5e46d2a307a333cdc06f8dbafd7a181a149
ojporv <olli.porvari@helsinki.fi>
2021-11-24 10:59:55 +0200

    11.16 testailua

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('4') // change this string to ensure a new version deployed
+  res.send('5') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
