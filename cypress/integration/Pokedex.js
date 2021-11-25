190164422b1019c93cb1ff73b32c03ea79a78e6f
CA <51471003+curtag@users.noreply.github.com>
2021-11-25 14:42:33 +0700

    Exercise 10.29 : add cypress test navigate to specific pokemon page

--- cypress/integration/Pokedex.js
+++ cypress/integration/Pokedex.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('can navigate from main page to specific pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('Hidden ability')
+    cy.contains('overgrow')
+  })
 })
