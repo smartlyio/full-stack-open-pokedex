f11e91de6389cab3cb2662f76e12ae36cf54e51e
Antti Turunen <asturune@hotmail.com>
2021-11-25 16:58:34 +0200

    second test added

--- cypress/integration/poke_sample_test.js
+++ cypress/integration/poke_sample_test.js
@@ -5,4 +5,10 @@ describe('Pokedex', function () {
         cy.contains('ivysaur')
         cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
     })
+    it('can be navigated to single Pokemon', function () {
+        cy.visit('http://localhost:5000')
+        cy.contains('ivysaur').click()
+        cy.contains('ivysaur')
+
+    })
 })
