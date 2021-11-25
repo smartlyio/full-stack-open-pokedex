8daa8128ee1c60bfffb01b7686965bd789e5a1d9
Antti Turunen <asturune@hotmail.com>
2021-11-25 11:23:42 +0200

    testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,38 +21,30 @@ module.exports = {
   "rules": {
     "indent": [
       "error",
-      2
-    ],
-    "no-console": "off",
-    "no-restricted-syntax": [
+        2
+      ],
+      
+    "linebreak-style": [
       "error",
-      {
-        "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
-        "message": "Unexpected property on console object was called"
-      }
-    ]
-  },
-  "linebreak-style": [
-    "error",
-    "unix"
-  ],
-  "quotes": [
-    "error",
-    "single"
-  ],
-  "semi": [
-    "error",
-    "never"
-  ],
-  "eqeqeq": "error",
-  "no-trailing-spaces": "error",
-  "object-curly-spacing": [
-    "error", "always"
-  ],
-  "arrow-spacing": [
-    "error", { "before": true, "after": true }
-  ],
-  "no-console": "error",
-  "react/prop-types": 0
-}
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "eqeqeq": "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": [
+      "error", "always"
+    ],
+    "arrow-spacing": [
+      "error", { "before": true, "after": true }
+    ],
+    "no-console": 0,
+    "react/prop-types": 0
+  }
 }
