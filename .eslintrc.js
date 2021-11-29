9d8eb2e035a034fb23f0e24a9c44eaa5009ce369
Luis Navarro <luisfnvrr@gmail.com>
2021-11-28 21:27:21 -0300

    11.9 🚧 adds e2e tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     es6: true,
     "jest/globals": true,
     node: true,
+    "cypress/globals": true
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
@@ -19,7 +20,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "jest"],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
