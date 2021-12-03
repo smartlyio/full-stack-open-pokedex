99aceccac4de8397d5a592b2bffae1386e6d1d67
Aapo Leppänen <aapo.leppanen@gmail.com>
2021-12-03 19:30:12 +0200

    exercises 11.2-11.9

--- cypress/integration/main-test/App.test.js
+++ cypress/integration/main-test/App.test.js
@@ -6,4 +6,9 @@ describe("Pokedex", function () {
 			"Pokémon and Pokémon character names are trademarks of Nintendo."
 		);
 	});
+	it("a page for a pokemon can be opened", function () {
+		cy.visit("http://localhost:8080");
+		cy.get('a[href*="/pokemon/raichu"]').click();
+		cy.contains("static");
+	});
 });
