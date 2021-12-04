d0ea4d7d74c16956c4ccc3dd2026028e3b4297cb
Aapo Leppänen <aapo.leppanen@gmail.com>
2021-12-05 01:30:42 +0200

    added comments

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ app.listen(PORT, () => {
 	console.log("server started on port 5000");
 });
 
+//health check for the deployment pipeline
 app.get("/health", (req, res) => {
 	res.send("ok");
 });
 
+//version number simple req, res server response
 app.get("/version", (req, res) => {
 	res.send("1"); //corresponding to the current version number deployed
 });
