ab543865a96d15c237595b4517eba298b9e9e7ef
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-12-09 15:25:18 +0200

    e2e test added

--- cypress/integration/poke_app.js
+++ cypress/integration/poke_app.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('Navigation from homepage to individual pokemon page works', function() {
+    cy.contains('venusaur').click()
+    cy.contains('venusaur')
+    cy.contains('speed')
+  })
 })
