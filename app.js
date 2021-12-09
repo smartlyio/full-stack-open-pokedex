f42175a39b11286209c5eb7d3b704154ffcf62a9
aaroluo <aarol@iki.fi>
2021-12-09 22:39:36 +0200

    health check

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
