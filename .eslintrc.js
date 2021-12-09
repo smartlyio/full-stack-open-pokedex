361a14eb4945d027df7b7cc624536371e8bb019d
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-12-09 13:47:23 +0200

    fix2

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    'commonjs': true,
+    'es2021': true,
+    'node': true
   },
   "extends": [
     "eslint:recommended",
