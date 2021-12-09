3eada7ed9387629ce2e64928f1e90960822bf538
aaroluo <aarol@iki.fi>
2021-12-09 21:47:35 +0200

    fix page 404s

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
