efbd97364d457412149206a11bfc761fd03966be
Ville Koskela <vkoskela@gmail.com>
2021-12-13 20:23:45 +0200

    11.9 done

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -5,4 +5,9 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('navigate to pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
