f1cd245c6df897e9f3840f9f9188662803cec2c0
Ville Koskela <ville.koskela@kv1.fi>
2021-12-17 11:03:55 +0000

    incr version in source

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('6') // change this string to ensure a new version deployed
+  res.send('7') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
