afceef2fbad8a36db7c6115a4cdd86a4f55eee7a
Elias Herranen <elias.herranen@gmail.com>
2021-12-26 22:00:02 +0200

    tests fixed

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -15,7 +17,6 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
