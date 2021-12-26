23aa66c91fbfe008983b7c71ee1ce995b8b83dc7
Elias Herranen <elias.herranen@gmail.com>
2021-12-26 22:58:06 +0200

    tests fixed

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express()
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
