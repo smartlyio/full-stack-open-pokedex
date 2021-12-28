3a0d80c7764d75228690d8a9e5687df7d7451d63
Vrezerino <parkpatrick1@gmail.com>
2021-12-28 19:22:14 +0200

    fixing an import issue

--- cypress/integration/pokemontest.js
+++ cypress/integration/pokemontest.js
@@ -2,7 +2,8 @@ import { PORT } from '../../utils';
 
 describe('Pokedex', function() {
 	it('front page can be opened', function() {
-		cy.visit(`http://localhost:${PORT}`);
+		//cy.visit(`http://localhost:${PORT}`);
+		cy.visit('http://localhost:3003');
 		cy.contains('ivysaur');
 		cy.contains('nidoran-f');
 		cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.');
