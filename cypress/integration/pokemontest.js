0d9fb005f39f52eba767abcdb339b09d11847052
Vrezerino <parkpatrick1@gmail.com>
2021-12-28 19:27:23 +0200

    cypress left hanging on startup, fixing

--- cypress/integration/pokemontest.js
+++ cypress/integration/pokemontest.js
@@ -1,9 +1,8 @@
-//import { PORT } from '../../utils';
+import { PORT } from '../../utils';
 
 describe('Pokedex', function() {
 	it('front page can be opened', function() {
-		//cy.visit(`http://localhost:${PORT}`);
-		cy.visit('http://localhost:3003');
+		cy.visit(`http://localhost:${PORT}`);
 		cy.contains('ivysaur');
 		cy.contains('nidoran-f');
 		cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.');
