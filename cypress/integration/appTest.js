b6a4ec3333bec0eb819f1c34c09b175c07035a47
luoyumo <yumo.luo@hotmail.com>
2021-12-30 18:00:45 +0200

    fix testing and one more e2e test

--- cypress/integration/appTest.js
+++ cypress/integration/appTest.js
@@ -5,4 +5,11 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate from main page to the page of a particular pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
