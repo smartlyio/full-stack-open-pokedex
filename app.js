d2c2c4e7ae7c4fd19b29028b604b2dfc574efe78
luoyumo <yumo.luo@hotmail.com>
2021-12-30 17:55:16 +0200

    fix linting

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000; // eslint-disable-line
+/*global process*/
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
