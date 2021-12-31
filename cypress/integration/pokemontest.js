78542a2ef994636ee62dde488619cd8469c32a82
Vrezerino <parkpatrick1@gmail.com>
2021-12-31 16:00:32 +0200

    trying different config

--- cypress/integration/pokemontest.js
+++ cypress/integration/pokemontest.js
@@ -3,7 +3,7 @@
 describe('Pokedex', function () {
 	it('front page can be opened', function () {
 		//cy.visit(`http://localhost:${PORT}`);
-		cy.visit('http://localhost:8080');
+		cy.visit('http://localhost:3003');
 		cy.contains('ivysaur');
 		cy.contains('nidoran-f');
 		cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.');
