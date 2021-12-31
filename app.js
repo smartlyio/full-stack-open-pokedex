3268fee64156902799b0884234481d369330c537
Vrezerino <parkpatrick1@gmail.com>
2021-12-31 17:00:21 +0200

    moving on with heroku

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ app.use(express.static('dist'));
 app.listen(PORT, () => {
 	console.log(`server started on port ${PORT}`);
 });
+
+app.get('/health', (_req, res) => {
+	res.send('ok');
+});
+
+app.get('version', (_req, res) => {
+	res.send('1');
+});
