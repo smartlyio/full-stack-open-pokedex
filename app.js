eaff9487ceac9e8eb0035de5e2b6f95f46ce6b5e
Vrezerino <parkpatrick1@gmail.com>
2022-01-01 18:19:30 +0200

    new branch

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 const PORT = require('./utils').PORT;
-//
+
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
 	console.log(`server started on port ${PORT}`);
 });
 
+app.get('/test', (_req, res) => {
+	res.send('test ok');
+});
+
 app.get('/health', (_req, res) => {
 	res.send('ok');
 });
+
 
 app.get('version', (_req, res) => {
 	res.send('1');
