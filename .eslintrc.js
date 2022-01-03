bb14ea5ee9990463caf0ba24caf58b487e5f530a
Đỗ Đức Tâm <nguyenhanh1074@gmail.com>
2022-01-03 20:00:55 +0700

    11.6 Fix the code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   env: {
+    commonjs: true,
+    node: true,
     browser: true,
     es6: true,
     "jest/globals": true,
@@ -15,7 +17,10 @@ module.exports = {
   plugins: ["react", "jest"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "windows"],
+    "linebreak-style": [
+      "error",
+      process.platform === "win32" ? "windows" : "unix",
+    ],
     quotes: ["error", "double"],
     semi: ["error", "always"],
     eqeqeq: "error",
