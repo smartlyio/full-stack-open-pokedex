2d7bdc9d12daea161e967336686e0689ee3fa1f6
Benjamintdk <bc.chew.hong.ee@gmail.com>
2022-01-04 10:47:44 +0800

    corrected e2e build and port directings

--- cypress/integration/poke_app.js
+++ cypress/integration/poke_app.js
@@ -1,13 +1,13 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   it('can navigate from main page to ivysaur', function() {
-    cy.visit('http://localhost:8080')
-    cy.visit('http://localhost:8080/pokemon/ivysaur')
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
     cy.contains('overgrow')
   })
