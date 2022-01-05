e99b824fe532eec31c75616004ada0d7c556b4bc
Cavendishh <kavanderjanne@gmail.com>
2022-01-05 12:17:46 +0200

    Add healthcheck after deployment

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// To check if application is running and healthy
+app.get('/healthcheck', (req, res) => {
+  res.send('ok')
+})
+
+// Version of the app for health checks
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
