c42424d89097b7f47608d9e25bcd5ead3bdbe9a6
Emil Dimitrov <edmtrv@gmail.com>
2022-01-10 19:34:45 +0200

    Add e2e test for a particular pokemon

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate from the main mage to a particular pokemon', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('Home')
+    cy.contains('ivysaur')
+  })
 })
