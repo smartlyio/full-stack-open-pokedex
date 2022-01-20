1081fb14ead8547f84bba2e0af8b4c1a436a0f9b
noorarytila <rytilanoora@gmail.com>
2022-01-20 20:56:38 +0200

    11.9 added cypress test to check navigating to pokemon page

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('speed')
+    cy.url().should('include', 'pokemon/ivysaur')
+  })
 })
