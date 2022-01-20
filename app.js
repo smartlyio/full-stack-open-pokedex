2a3c487714bf3dcd66c61623d1d08efd299f3e7d
noorarytila <rytilanoora@gmail.com>
2022-01-20 21:59:29 +0200

    Procfile fix

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  console.log('server started on port 5000')
-})
-
 app.get('/health', (req, res) => {
   res.send('ok')
 })
@@ -17,3 +13,8 @@ app.get('/health', (req, res) => {
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
+
+app.listen(PORT, () => {
+  console.log('server started on port 5000')
+})
+
