95474aff97721b88511184b6c3d63582e66f209f
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-22 14:57:10 -0800

    11.9

---
+++ cypress/integration/pokedex_test.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be navigated to', function() {
+    // eslint-disable-next-line no-console
+    console.log('test started running')
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
