a672ea4cc75c0a27503fc6ecee2a9f8ef48e70b5
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-22 16:40:59 -0800

    11.9 - Add new script

--- cypress/integration/pokedex_test.js
+++ cypress/integration/pokedex_test.js
@@ -7,8 +7,6 @@ describe('Pokedex', function() {
   })
 
   it('pokemon page can be navigated to', function() {
-    // eslint-disable-next-line no-console
-    console.log('test started running')
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
