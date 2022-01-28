be26a3d28b8fd295dc5f5a980658f7291172ec2f
Flavio Garrido <ing.flaviogarrido@gmail.com>
2022-01-28 07:56:45 -0300

    Ex11.9 Test e2e new test case

--- cypress/integration/01-pokedex.js
+++ cypress/integration/01-pokedex.js
@@ -5,4 +5,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('could access pokemon information', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('charmander').click()
+    cy.contains('solar power')
+  })
 })
