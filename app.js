681f69096190658ac641471becd0d50bc9532716
fuka-innovations <fuad.kalhori@gmail.com>
2022-01-30 19:08:15 +0200

    11.11

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const PORT = process.env.PORT || 4000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line
-  console.log('server started on port 4000')
+  console.log(`server started on port ${PORT}`)
 })
