1b08d2302eca061e2a0423ecf48cbf6da6b4bd7b
Mai Tran <maitran.ln99@gmail.com>
2022-02-01 17:09:49 +0200

    Remove unnecessary console.log

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -34,6 +34,7 @@ const PokemonPage = ({ previous, next }) => {
     (ability) => ability.is_hidden === true
   );
 
+  // eslint-disable-next-line no-console
   console.log("hiddenAbility=", hiddenAbility);
   return (
     <>
