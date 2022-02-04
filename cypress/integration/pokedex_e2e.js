ba0904cee79f99f95c7d6ad4c48fc91fc6603fc7
seenicroot <sngmanhim@hotmail.com>
2022-02-03 21:24:49 -0500

    Add pokemon page cypress test

--- cypress/integration/pokedex_e2e.js
+++ cypress/integration/pokedex_e2e.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be opened', function() {
+    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.contains('chlorophyll')
+    cy.contains('ivysaur')
+  })
 })
