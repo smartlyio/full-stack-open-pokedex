dc8b0022595a80e45258772724d2031fda78ec4d
seenicroot <sngmanhim@hotmail.com>
2022-02-03 21:45:14 -0500

    Fix pokemon page test

--- cypress/integration/pokedex_e2e.js
+++ cypress/integration/pokedex_e2e.js
@@ -5,8 +5,9 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('pokemon page can be opened', function() {
-    cy.visit('http://localhost:5000/pokemon/ivysaur')
-    cy.contains('chlorophyll')
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
     cy.contains('ivysaur')
+    cy.contains('chlorophyll')
   })
 })
