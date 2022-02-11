2f6f778abfade3426865a5a83cc0d15c27300e43
wds <tom.green83@protonmail.com>
2022-02-11 21:59:59 +0000

    Add cypress test

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     "es6": true,
     "jest/globals": true,
     "node": true,
+    "cypress/globals": true,
   },
   "extends": [
     "eslint:recommended",
@@ -17,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
