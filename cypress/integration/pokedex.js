a7487e67a691e3bc290ea1a56e136e5d1300471a
Alex McKendry <a.mckendry2@gmail.com>
2022-02-11 12:25:45 -0500

    added another e2e test

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,9 @@ describe('Pokedex', function(){
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
   })
+  it('clicking pokemon navigates to its page', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
