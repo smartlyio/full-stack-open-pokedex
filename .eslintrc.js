f6b1a4a4f75eb46097e2b2c38315dd1915c2af15
Mats Granholm <granholm.mats@gmail.com>
2022-02-12 16:54:03 +0200

    Set no-console to off in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,7 +44,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
