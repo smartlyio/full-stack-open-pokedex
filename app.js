46e6f03b57b346f84e877e141cca6718febbe85d
Toni Mäki-Leppilampi <basisti@gmail.com>
2022-02-14 12:47:11 +0200

    11.9-1

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
-const process = require('dotenv')
+// const process =
+// require('dotenv').config()
 
 // Heroku dynamically sets a port
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
