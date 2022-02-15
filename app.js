74843cbf883181a2fc37959a585ce147cde13424
Toni Mäki-Leppilampi <basisti@gmail.com>
2022-02-15 08:23:06 +0200

    heroku deploy

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-// const process =
-// require('dotenv').config()
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
@@ -11,5 +9,5 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`Server running on port ${PORT}`)
 })
