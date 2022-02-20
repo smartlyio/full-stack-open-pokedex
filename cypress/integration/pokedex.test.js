3c2bdb5a4ee37bfd5aea30f059fc23bf326e9b59
Rafael Cano <rafael.cano@ucsp.edu.pe>
2022-02-20 14:26:31 -0500

    11.9 done.

---
+++ cypress/integration/pokedex.test.js
@@ -0,0 +1,16 @@
+describe("Pokedex", function () {
+  it("front page can be opened", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur");
+    cy.contains(
+      "Pokémon and Pokémon character names are trademarks of Nintendo."
+    );
+  });
+});
+describe("can go to particular pokemon", function () {
+  it("can go to ivysaur", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur").click();
+    cy.contains("chlorophyll");
+  });
+});
