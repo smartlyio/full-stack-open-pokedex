6549082a841ac941c443636ef8835a594c67c572
Rafael Cano <rafael.cano@ucsp.edu.pe>
2022-02-20 22:48:02 -0500

    11.11 done

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const process = require('process')
 const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
-
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 app.listen(PORT, () => {
   console.log('server started on port 8080')
 })
