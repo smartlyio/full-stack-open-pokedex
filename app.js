2a60f491bf67bfa0b38ff10096d6bd884559d430
Rafael Cano <rafael.cano@ucsp.edu.pe>
2022-02-21 15:07:18 -0500

    Pull request code with errors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('dist'))
 app.get('/health', (req, res) => {
   res.send('ok')
 })
-app.get('/version', (req, res) => {
+pp.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 app.listen(PORT, () => {
