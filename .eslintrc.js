40feeda01ad0630415a30f49f118137e0b321482
Matti Luukkainen <mluukkai@iki.fi>
2022-03-02 16:16:18 +0200

    lint betterment

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'jest'],
+  plugins: ['react', 'jest', 'cypress'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
