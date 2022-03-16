73b191dd0176d209b00f407e7c12864bcb93982e
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-03-16 13:47:43 +0100

    11.12

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
+  res.send('1')
+})
+
+app.get('/health', (req, res) => {
   res.send('ok')
 })
 
