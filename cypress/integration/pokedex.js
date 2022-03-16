e10d23e82fd7592252b8050364a06e26500d213d
showone7 <li.andr1771@gmail.com>
2022-03-16 20:30:12 +0300

    u

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,11 +1,13 @@
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('page of a particular Pokemon', function () {
-    cy.visit('http://localhost:8080/pokemon/pikachu')
-    cy.contains('lightning rod')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
