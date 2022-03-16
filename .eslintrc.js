813a7998f8a2b7fef9088124371b9276c68522f3
Joseph Odunsi <odunsiolakunbi@gmail.com>
2022-03-16 11:45:04 +0100

    fix scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es6: true,
     "jest/globals": true,
     node: true,
+    "cypress/globals": true,
   },
   extends: [
     "eslint:recommended",
@@ -17,7 +18,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module",
   },
-  plugins: ["react", "jest", "eslint-plugin-cypress"],
+  plugins: ["react", "jest", "cypress"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
