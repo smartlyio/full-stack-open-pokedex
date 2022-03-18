a656b6a208afc61887e1a9cefc20ed2cecadbf24
k-tuomas <karatuomas@gmail.com>
2022-03-18 16:41:12 +0200

    11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,10 +25,10 @@ module.exports = {
       "error",
         2
       ],
-    // "linebreak-style": [
-    //   "error",
-    //   "unix"
-    // ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
     "quotes": [
       "error",
       "single"
