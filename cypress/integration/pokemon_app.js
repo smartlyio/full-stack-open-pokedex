111796e45369c5f36f641288d22991f12f185bbc
Sandesh Hyoju <sundesz@gmail.com>
2022-03-22 23:48:15 +0200

    Add new test

--- cypress/integration/pokemon_app.js
+++ cypress/integration/pokemon_app.js
@@ -6,4 +6,9 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.get('.list-item-name').contains('ivysaur').click().contains('ivysaur')
+  })
 })
