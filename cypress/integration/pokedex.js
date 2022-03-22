1216523aef2bf5318121ca60ce51c58ebf3fc87c
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-03-22 00:14:18 -0400

    write first e2e test

---
+++ cypress/integration/pokedex.js
@@ -0,0 +1,14 @@
+// pokedex.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
