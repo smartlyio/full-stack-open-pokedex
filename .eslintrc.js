767bdbf4584041d8b2df103f6dea133a52978590
(Bea)*2 <77938697+Beatrice-Beacco@users.noreply.github.com>
2022-03-28 22:39:19 +0200

    forse adesso è fixato

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,8 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
+    //quotes: ["error", "single"],
+    //semi: ["error", "never"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
