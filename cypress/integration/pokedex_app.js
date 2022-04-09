d67aa015c73370ed355ee05556c1b525bba37965
pablo-maff <pablo.maff.076@gmail.com>
2022-04-09 18:10:00 +0200

    Typo in yaml

--- cypress/integration/pokedex_app.js
+++ cypress/integration/pokedex_app.js
@@ -1,6 +1,8 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5000')
+  })
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
