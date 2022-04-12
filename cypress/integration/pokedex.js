81c8d8267595bc8e0060e5c358cacc1483681408
tvaskisalo <61991314+tvaskisalo@users.noreply.github.com>
2022-04-12 13:33:59 +0300

    added new e2e tests

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -6,3 +6,11 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokemon info', function() {
+  it('pokemon info can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('[href="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+  })
+})
