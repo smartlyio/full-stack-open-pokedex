1b990198ec7cc443db38412ba810ec11a7e83fd0
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-04-23 18:48:34 +0200

    idk

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,23 +25,23 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('login', ({ username, password }) => {
-    cy.request('POST', 'http://localhost:3003/api/login', {
-        username, password
-    }).then(({ body }) => {
-        localStorage.setItem('loggedBlogUser', JSON.stringify(body))
-        cy.visit('http://localhost:3000')
-    })
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username, password
+  }).then(({ body }) => {
+    localStorage.setItem('loggedBlogUser', JSON.stringify(body))
+    cy.visit('http://localhost:3000')
+  })
 })
 
-Cypress.Commands.add('addBlog', ({ title, author, url, likes}) => {
-    cy.request({
-        method: 'POST', 
-        url: 'http://localhost:3003/api/blogs', 
-        body: {title, author, url, likes},
-        headers: {
-            'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedBlogUser')).token}`
-        }
-    })
-    
-    cy.visit('http://localhost:3000')
+Cypress.Commands.add('addBlog', ({ title, author, url, likes }) => {
+  cy.request({
+    method: 'POST',
+    url: 'http://localhost:3003/api/blogs',
+    body: { title, author, url, likes },
+    headers: {
+      'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedBlogUser')).token}`
+    }
+  })
+
+  cy.visit('http://localhost:3000')
 })
