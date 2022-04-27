b6cbdb170c5e8c7f2467bf3d9a819891a50fe40c
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-04-27 15:19:58 +0000

    ex 11.9

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
+  console.log(pokemonList)
   if (isLoading) {
     return <LoadingSpinner />
   }
