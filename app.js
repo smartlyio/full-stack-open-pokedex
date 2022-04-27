eae86944fb173f3f213ddd413a6afdc3515fbec7
TomasSanchezNegrette <tsancheznegrette@frba.utn.edu.ar>
2022-04-26 22:18:19 -0300

    added helathcheck

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+const { version } = require('./package-lock.json')
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(version)
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
