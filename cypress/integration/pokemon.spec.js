e751dc6c1a88248d240ffa87130146a59318b5b1
Hossein movahed <hossein.movahed@monese.com>
2022-04-29 10:21:29 +0300

    another way

--- cypress/integration/pokemon.spec.js
+++ cypress/integration/pokemon.spec.js
@@ -1,7 +1,8 @@
+import { cy } from 'cypress';
 
 describe('Pokedex', function () {
     it('front page can be opened', function () {
-        cy.visit('http://localhost:5000')
+        // cy.visit('http://localhost:5000')
         cy.contains('Ivysaur')
         cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
     })
