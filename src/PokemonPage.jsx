e2f5f85e82b2d0186a2876bdf28d6f16eabe3a53
Graham <graham.hollingsworth@outlook.com>
2022-05-01 06:57:07 -0400

    fixing linting 3

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -27,7 +27,7 @@ const PokemonPage = ({ previous, next }) => {
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
   // eslint-disable-line no-use-before-define
-  console.log('hiddenAbility=', hiddenAbility) 
+  console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
       <div className="links">
