3ba58ac908d029a166bdd8b782726bf28a237e0b
unknown <akashsharma.zak@gmail.com>
2022-05-01 18:14:05 +0530

    end to end testing

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = 5000
 
 app.use(express.static('dist'))
 
+app.get("/hello", (req, res) => {
+  res.status(200).send("Hello i am akash")
+})
+
 app.listen(PORT, () => {
-  // console.log('server started on port 5000')
+  console.log('server started on port 5000')
 })
