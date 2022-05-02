b11c1b9e45e1d216810e9af2f8c7afa94d615e6c
Graham <graham.hollingsworth@outlook.com>
2022-05-02 17:27:05 -0400

    Adusted PORT and removed console.log

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const PORT = 5000 //process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-line no-use-before-define
-  console.log('server started on port 5000')
+  //console.log('server started on port 5000')
 })
