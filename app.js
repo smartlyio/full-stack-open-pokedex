c25fd131ba61c133e6ff25ee3a510a590fbb1041
augericke <augericke@gmail.com>
2022-05-10 16:47:52 -0400

    fix rollback issue

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
+const process = require('process')
+
 const app = express()
 
 // Heroku dynamically sets a port
-// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 4000
 
-
 app.use(express.static('dist'))
-app.use(express.json())
 
-app.get('/health', (req, res) => {
+app.get('/health', (_, res) => {
   res.send('ok')
 })
 
-app.listen(PORT, () => {})
-
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started on port 4000')
+})
