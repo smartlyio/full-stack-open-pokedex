7a65a17e4d9ab06735ef630f42f3ecc76be6dfc3
augericke <augericke@gmail.com>
2022-05-10 15:15:41 -0400

    fix linting rules

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 4000;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 4000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  console.log("server started on port 4000");
-});
+app.listen(PORT, () => {})
