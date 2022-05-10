5b5225da6c500a2c6a560d52e39c407a4796f50b
augericke <augericke@gmail.com>
2022-05-10 16:13:27 -0400

    add health check

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express()
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 4000
 
+
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {})
