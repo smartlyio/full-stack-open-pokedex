062a713b27975582d071186b539009c4e0611054
Eugene Koh <eugene.koh.cheewei@gmail.com>
2022-05-14 14:26:22 +0200

    fix lint issues with cy

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   },
   'extends': [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:cypress/recommended'
   ],
   'parserOptions': {
     'ecmaFeatures': {
