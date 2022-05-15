7bb589bfa7b70f4e88ecf5baffc0fc7bc7327eba
KOMEI KINJO <miketyson40166@gmail.com>
2022-05-15 15:03:41 +0900

    Add new test

--- cypress/integration/api.cypress.js
+++ cypress/integration/api.cypress.js
@@ -7,4 +7,11 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be navigated to', () => {
+    cy.visit('http://localhost:5000')
+    cy.get('.list-item:nth-child(2)').click()
+    cy.contains('chlorophyll')
+  })
+
 })
