cc7b8476248808e46ae9caf3b0388add40ba1fd6
KOMEI KINJO <miketyson40166@gmail.com>
2022-05-15 14:15:11 +0900

    Create the first Cypress test

--- cypress/integration/api.cypress.js
+++ cypress/integration/api.cypress.js
@@ -1,5 +1,8 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
+    cy.on('uncaught:exception', error => {
+      console.error(error)
+    })
     cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
