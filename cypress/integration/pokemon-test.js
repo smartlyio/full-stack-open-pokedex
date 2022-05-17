8e2184953b69caffdc6d27c6b03bb5e68ed57fd8
Joni Suovuori <suovuori.joni@gmail.com>
2022-05-17 21:03:46 +0300

    11.9.5

--- cypress/integration/pokemon-test.js
+++ cypress/integration/pokemon-test.js
@@ -5,3 +5,11 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokedex', function() {
+    it('navigation from frontpage to specific pokemon works', function() {
+      cy.visit('http://localhost:5000')
+      cy.contains('diglett').click()
+      cy.contains('sand veil')
+    })
+  })
