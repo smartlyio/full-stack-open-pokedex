9a6bcd83c1580e4cc7e95a3d7167f2929c48d870
dannyboi07 <danielchettiar07@gmail.com>
2022-05-19 17:11:31 +0530

    Ex 11.12 Enabling rollbacks on ci/cd failure

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
+app.get("/health", (req, res) => {
+    res.send("ok");
+});
+app.get("/version", (req, res) => {
+    res.send("1");
+});
 
 app.listen(PORT, () => {
     // eslint-disable-next-line no-console
