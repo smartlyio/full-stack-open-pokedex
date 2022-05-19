53ca26312e390f4c158c1efaefbbb67919762b96
Tri Ho <minhtri191193@gmail.com>
2022-05-19 10:55:40 -0700

    fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   "env": {
+    "node": true,
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
   },
   "extends": [
     "eslint:recommended",
@@ -43,7 +44,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
