446d6572bc0e67ad9752a05f4a6a50e6d0e5a600
Tri Ho <minhtri191193@gmail.com>
2022-05-19 11:42:06 -0700

    e2e test for pokemon home page

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,30 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "browser": true,
-    "es6": true,
-    "jest/globals": true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+    'cypress/globals': true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "plugins": [
-    "react", "jest"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "off",
-    "react/prop-types": 0
-  }
+  plugins: ['react', 'jest', 'cypress'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 'off',
+    'react/prop-types': 0,
+  },
 }
