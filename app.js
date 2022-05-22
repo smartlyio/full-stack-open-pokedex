3c71f480bd0d5bf185f8673c3ea3f724f53f8ebd
samsoul <samsoul@protonmail.com>
2022-05-22 10:25:50 +0200

    testing healthcheck with procfile incorrect command

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const app = express()
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
+const APPVERSION = '1' // change this string to ensure a new version deployed
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(`${APPVERSION}`)
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
