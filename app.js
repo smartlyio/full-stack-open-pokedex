e6dbb2aece03253c86ddbd2e7c8c81f7787b5540
Alins_No1 <alins_no1@xrm6.kyla.fi>
2022-06-05 01:43:57 +0000

    Exercise 11.12 - version not updated and rolling back

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('11.11')
+  res.send('11.12')
 })
 
 app.listen(PORT, () => {
