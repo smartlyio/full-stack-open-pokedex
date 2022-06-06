f8fdd8c72e4bb91fd6aa048d451d5365e6480f34
Antti <hellsten.antti@gmail.com>
2022-06-06 11:32:16 +0300

    fixing procfile and finalizing exer 11.11

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version',(req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
-//
