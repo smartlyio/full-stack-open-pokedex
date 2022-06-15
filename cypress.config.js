7d3b0c636e43cb616bb9a15db04b66377a51be53
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-06-15 23:33:37 +0300

    cypress setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'cypress'
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
-  // e2e: {
-  //   setupNodeEvents(on, config) {
-  //     // implement node event listeners here
-  //   },
-  // },
-})
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
