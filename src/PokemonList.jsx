16e878bc63867aa8ba14b0b935d1f01002a033c3
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-06-15 23:12:38 +0300

    ex 11.8

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PokemonList = ({ pokemonList }) => {
+
   return (
     <div className="list-container">
       {pokemonList.map(({ id, name }) => (
