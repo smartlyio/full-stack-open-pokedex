7e55da4d9cf9f1e698b5c1ec7c4cedbe7dec2af8
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-06-15 23:37:11 +0300

    cypress eslint fixed

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
