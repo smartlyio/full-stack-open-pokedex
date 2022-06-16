0eaa6a5030f31830226128b2bb2abbd5035e8598
mugiwarafx <sangalesbenes@uoc.edu>
2022-06-16 14:10:08 +0200

    adds part 11 exercise 18, notifies build failed

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('this should not be deployed, fix syntax') // change this string to ensure a new version deployed
+  res.send('notify test failed') // change this string to ensure a new version deployed
 })
