141f69601b5cdd7131424053b1d3a0b231a116c2
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-06-20 11:29:23 +0300

    comment added

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
   id: parseInt(url.match(/\/(\d+)\//)[1])
 })))
 
+//just comment
+
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
   if (isLoading) {
@@ -20,8 +22,6 @@ const App = () => {
   if (error) {
     return <ErrorMessage error={error} />
   }
-
-  
 
   return (
     <Router>
