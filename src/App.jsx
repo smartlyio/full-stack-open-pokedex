fa14b5d7f167782ce844789125be0ac3c9ecd3a3
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-06-20 12:03:17 +0300

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 })))
 
 //just comment
+//another comment
 
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
