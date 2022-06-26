b178af08a685fa412e8752cb7ec2ffc0b65c7e39
dft <varahahmo1@gmail.com>
2022-06-26 18:39:30 +0300

    11.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
+
   ],
   "parserOptions": {
     "ecmaFeatures": {
