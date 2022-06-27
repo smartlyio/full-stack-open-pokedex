796d197a9f14c059a72b09195b12e215b72822f2
Ch4v3-98 <ch4v398@gmail.com>
2022-06-27 18:25:07 -0300

    removed console.log rule from eslint and added a node env

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
     'jest/globals': true,
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
@@ -28,7 +29,6 @@ module.exports = {
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 'error',
     'react/prop-types': 0,
   },
 };
