d4ed16b58cf688e61438ca4a0c47d1343540302b
jhiekkap <jari.hiekkapelto@gmail.com>
2022-06-27 21:15:45 +0300

    fix typo

--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -3,7 +3,7 @@ const port = process.env.NODE_ENV === 'development' ? 8080 : 5000
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     console.log('STARTING CYPRESS TEST')
-    cy.visit(`ttp://localhost:${port}`)
+    cy.visit(`http://localhost:${port}`)
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
