7c478da798faa4a6e3fbe8cc233680043e684c4c
jhiekkap <jari.hiekkapelto@gmail.com>
2022-06-27 22:33:50 +0300

    fix error

--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -1,9 +1,7 @@
-const port = process.env.NODE_ENV === 'development' ? 8080 : 5000
-
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     console.log('STARTING CYPRESS TEST')
-    cy.visit(`http://localhost:${port}`)
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
