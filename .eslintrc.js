9bc3f371e662fa3b7d29fe1e8693e1f76501046a
Ch4v3-98 <ch4v398@gmail.com>
2022-06-27 19:21:46 -0300

    added dev dependency for eslint-cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,19 +17,20 @@ module.exports = {
   plugins: ['react', 'jest', 'cypress'],
   rules: {
     indent: ['error', 2],
-    // "linebreak-style": [
-    //   "error",
-    //   "unix"
-    // ],
     quotes: ['error', 'single'],
-    // "semi": [
-    //   "error",
-    //   "never"
-    // ],
     eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
     'react/prop-types': 0,
+    /// OPTIONAL LINTING RULES
+    // "linebreak-style": [
+    //   "error",
+    //   "unix"
+    // ],
+    // "semi": [
+    //   "error",
+    //   "never"
+    // ],
   },
 };
--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,12 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  e2e: {},
 
   component: {
     devServer: {
-      framework: "react",
-      bundler: "webpack",
+      framework: 'react',
+      bundler: 'webpack',
     },
   },
 });
