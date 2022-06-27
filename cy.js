34c889a1204c45c637ff0ee5424708fa74c5478c
jhiekkap <jari.hiekkapelto@gmail.com>
2022-06-27 10:25:00 +0300

    testing

--- cy.js
+++ cy.js
@@ -1,5 +1,6 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
+    console.log('STARTING CYPRESS TEST')
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
