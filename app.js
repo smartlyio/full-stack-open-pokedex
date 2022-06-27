0403ebe5036b69d3df886620dad3b97eff85dca6
Ch4v3-98 <ch4v398@gmail.com>
2022-06-27 20:35:01 -0300

    add healthcheck to heroku deployment

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('dist'));
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log('server started on port 5000');
+});
+
+app.get('/health', (req, res) => {
+  res.send('ok');
 });
