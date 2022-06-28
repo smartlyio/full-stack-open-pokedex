09296d9b82af17dc3c88ef7442f1f57b6c653c36
kaimhall <kaimhall@gmail.com>
2022-06-28 12:04:27 +0300

    11.9

--- cypress/e2e/pokedex_e2e.cy.js
+++ cypress/e2e/pokedex_e2e.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('navigation from link works', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('pichu').click()
+    cy.contains('pichu')
+    cy.contains('lightning rod')
+
+  })
 })
