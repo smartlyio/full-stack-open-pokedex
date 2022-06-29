3d206b79a4369c568780a33fa703c2ee0a004d37
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2022-06-29 02:05:27 -0500

    add tests

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = 8080
+const PORT = 8081
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => console.log('server started on port 8080'))
+app.listen(PORT, () => console.log('server started on port 8081'))
