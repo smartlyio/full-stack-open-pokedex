b3f0dcfc994b35017d04d0914f92ed93827e4633
Mateo Lomas <mateo.lomas@yachaytech.edu.ec>
2022-06-29 10:20:03 -0500

    fix ivisaur

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -3,7 +3,7 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
     cy.visit('http://localhost:8080')
-    cy.contains('ivysau')
+    cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
