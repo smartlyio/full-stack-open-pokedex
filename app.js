11e91f1020e591afa6a187d5d50806f06d46beb4
kaimhall <kaimhall@gmail.com>
2022-07-01 13:41:39 +0300

    testing rollback

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-//app.use('https://pokedox2022.herokuapp.com')
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
