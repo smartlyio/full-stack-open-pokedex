ed0cfd5c97aa86baee412f5cce301d0c2821bcf3
kaimhall <kaimhall@gmail.com>
2022-07-01 13:25:12 +0300

    trying healthcheck

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+//app.use('https://pokedox2022.herokuapp.com')
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
 })
