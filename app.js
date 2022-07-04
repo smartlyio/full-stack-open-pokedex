ed7499c9904b35caabdac22fb284614fcee9d0c0
Aikamoine <atte.laakso@helsinki.fi>
2022-07-04 14:56:02 +0300

    try rollback with failing build

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line no-console
 })
 
-app.get('/health', (req, res) => {
+app.get('/heath', (req, res) => {
   res.send('ok')
 })
 
