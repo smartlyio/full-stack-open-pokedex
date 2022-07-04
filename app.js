c60d64bf00285d4a13631ff527087f8c856660fe
Aikamoine <atte.laakso@helsinki.fi>
2022-07-04 14:26:11 +0300

    added health check to pipeline

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line no-console
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
