502e9bf5d4aa53b6acd0acd59562051b22fd59d3
Aikamoine <atte.laakso@helsinki.fi>
2022-07-04 13:29:09 +0300

    exercise 11.9, just 10 pokemons are loaded to speed stuf up!

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,8 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('single pokemon can be clicked', function () {
+    cy.contains('charmander').click()
+    cy.contains('solar power')
+  })
 })
