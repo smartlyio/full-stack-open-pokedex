c3490fce1c74d1c5427f6bffd5a867c7e5a4acc5
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-07-14 02:52:44 +0300

    health check

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+app.get('/health', (req, res) => res.send('ok'))
+app.get('/version', (req, res) => res.send('1.0.0'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
