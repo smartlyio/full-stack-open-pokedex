ca466abfc5b1039b71c08f96eef418e940249af0
Veikka <32365239+SirVeggie@users.noreply.github.com>
2022-07-14 02:24:50 +0300

    fix stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     'es6': true,
     'node': true,
     'jest': true,
-    'cypress': true
+    'cypress/globals': true
   },
   'extends': [
     'eslint:recommended',
--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     blockHosts: [
       'raw.githubusercontent.com'
     ]
   },
-});
+})
