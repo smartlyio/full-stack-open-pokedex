ce60a67186ac81fad7935967d072190af2687c67
Aldo Martinez <acm.holmes@gmail.com>
2022-07-14 12:10:42 -0500

    added additional cy testing

--- cypress/e2e/0-testing-pokedex/pokedex_app.cy.js
+++ cypress/e2e/0-testing-pokedex/pokedex_app.cy.js
@@ -4,4 +4,25 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('viewing a pokemon and its attributes', function() {
+    cy.visit('http://localhost:3001', { timeout: 100000 })
+    cy.contains('pikachu').click()
+    cy.contains('Previous')
+    cy.contains('Next')
+    cy.contains('Home')
+    cy.contains('speed')
+    cy.contains('special defense')
+  })
+
+  it('viewing previous and next pokemon', function() {
+    cy.visit('http://localhost:3001', { timeout: 100000 })
+    cy.contains('pikachu').click()
+    cy.contains('Previous').click()
+    cy.contains('arbok')
+    cy.contains('Next').click()
+    cy.contains('pikachu')
+    cy.contains('Next').click()
+    cy.contains('raichu')
+  })
 })
