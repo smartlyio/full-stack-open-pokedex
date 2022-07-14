cb53bcae9ea5185b649e4ebf429e7745c9553aab
Aldo Martinez <acm.holmes@gmail.com>
2022-07-14 07:17:23 -0500

    added health check

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
