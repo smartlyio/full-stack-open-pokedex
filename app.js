eeaf56603300c466d054012dd2c40d94592b97c3
Aldo Martinez <acm.holmes@gmail.com>
2022-07-14 08:45:09 -0500

    updating e2e scripts again!!

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.get('/health', (req, res) => {
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
