dab4ccb5b545bedd18518748891d8f3ed6098fe2
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-07-19 14:57:07 +0500

    Update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     timeouts: {
       pageLoadTimeout: 100000
     }
