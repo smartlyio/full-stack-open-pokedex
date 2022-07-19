94ea99e90805a5225830e831e053b52a4a429033
Tzvi-H <16guitar@gmail.com>
2022-07-19 15:58:15 -0600

    completed exercise 11.12 v8

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/healthh', (req, res) => {
+app.get('/health', (req, res) => {
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('5') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
