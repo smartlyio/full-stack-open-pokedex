2fd61502cacc467b09bc564b43f8f264a0cf79f0
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-07-19 15:01:59 +0500

    Update e2e

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     timeouts: {
-      pageLoadTimeout: 100000
-    }
+      pageLoadTimeout: 100000,
+    },
   },
 });
