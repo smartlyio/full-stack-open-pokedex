a71d0eda1ca40779b83305a9c3c42e9586e27768
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-07-19 14:45:59 +0500

    E2E added

---
+++ cypress/e2e/my/note_app.cy.js
@@ -0,0 +1,7 @@
+describe('Note app', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
