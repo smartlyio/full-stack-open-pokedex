726d10495a62bceedb4b602bd470fc16c4213859
Mario Segovia <marioseg_02@hotmail.com>
2022-07-23 19:03:18 +0100

    Fix 9

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,4 +19,16 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  module.exports = (on) => {
+    on('before:browser:launch', (browser = {}, args) => {
+      if (browser.name === 'chrome') { 
+        // ^ make sure this is your browser name, you may 
+        // be using 'canary' or 'chromium' for example, so change it to match!
+        args.push('--proxy-bypass-list=<-loopback>')
+        return args
+      }
+    })
+  }
+
 }
