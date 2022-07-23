caaafbd947b69dd5e84df9812fbc43d0c6f5d4fd
Mario Segovia <marioseg_02@hotmail.com>
2022-07-23 20:22:12 +0100

    Exercises 11.1 - 11.9

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,5 +4,15 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+
+  it('link can be opened', function() {
+    Cypress.config('pageLoadTimeout', 100000)
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    
   })
+
 })
+
