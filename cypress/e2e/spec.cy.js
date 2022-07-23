99e08a54ec7b6f47004df247956bb0f245d9c01b
Mario Segovia <marioseg_02@hotmail.com>
2022-07-23 10:44:15 +0100

    test cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    Cypress.config('pageLoadTimeout', 100000)
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
