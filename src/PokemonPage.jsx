3a3fa050f690a9356892a1e2a4c376c82d1949b9
hy <iamathis@ninja.com>
2022-07-24 22:30:35 +0100

    Fix eslint errors

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -26,7 +26,6 @@ const PokemonPage = ({ previous, next }) => {
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
-  console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
       <div className="links">
