7e6b2eb92ab7b2760abc4f53579c4bec08787b3f
hy <iamathis@ninja.com>
2022-07-24 22:40:30 +0100

    Fix more eslint errors

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const app = express()
-import process from "process"
+import process from 'process'
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  
-})
+app.listen(PORT, () => {})
