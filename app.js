43977810bbe64a14e78c6975f9192d2fb04ceb80
hy <iamathis@ninja.com>
2022-07-25 10:56:19 +0100

    Fix different node version cypress

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-app.get('/', (req, res) => {
-    res.send('000')
-  })
-
 app.listen(PORT, () => {
 
 })
--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    pageLoadTimeout:60000,
-    "defaultCommandTimeout": 60000,
+    pageLoadTimeout: 60000,
+    defaultCommandTimeout: 60000,
   },
-
-})
+});
