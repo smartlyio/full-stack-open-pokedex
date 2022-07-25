dad92f7e4503ac5ad9f130e9f12969755537e4ee
hy <iamathis@ninja.com>
2022-07-25 11:00:18 +0100

    Fix different node version cypress

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+app.get('/', (req, res) => {
+    res.status('200')
+  })
+
 app.listen(PORT, () => {
 
 })
