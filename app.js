7d3c0756c3a8c015490a676bf043bc9a6363f615
hy <iamathis@ninja.com>
2022-07-25 11:51:49 +0100

    Fix different node version cypress

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const process = require('process')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const process = require("process");
+const cors = require("cors");
+const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
-app.use(express.static('dist'))
-app.use(cors())
+const PORT = process.env.PORT || 5000;
+app.use(express.static("dist"));
+app.use(cors());
 app.get("/", (req, res) => {
-  res.send('success')
-})
+  res.send("success");
+});
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
 
-app.listen(PORT, () => {})
+app.listen(PORT, () => {});
