30d2141cd5d98300ec6fcd19089c03f260370f91
hy <iamathis@ninja.com>
2022-07-25 10:22:50 +0100

    Fix different node version cypress

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     pageLoadTimeout:60000,
-    "defaultCommandTimeout": 20000,
+    "defaultCommandTimeout": 60000,
   },
 
 })
