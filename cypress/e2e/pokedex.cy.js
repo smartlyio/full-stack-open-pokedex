2fe2914d932d3977b2c6645d0751ebd9db4aadaa
Filip <filip.eller@aalto.fi>
2022-07-26 22:55:37 +0300

    Complete 11.9, simple end to end -tests

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,9 +1,17 @@
 describe("Pokedex", function () {
-  it("front page can be opened", function () {
+  beforeEach(function () {
     cy.visit("http://localhost:5000");
+  });
+
+  it("front page can be opened", function () {
     cy.contains(/ivysaur/i);
     cy.contains(
       /Pokémon and Pokémon character names are trademarks of Nintendo./i
     );
+  });
+
+  it("pokemon page can be navigated to", function () {
+    cy.contains(/ivysaur/i).click();
+    cy.contains(/chlorophyll/i);
   });
 });
