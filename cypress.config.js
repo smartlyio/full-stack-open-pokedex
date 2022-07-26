07d22a8d2ae06a5e15ed651335e16215192ce708
Filip <filip.eller@aalto.fi>
2022-07-26 23:04:32 +0300

    Complete 11.9, simple end to end -tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
+/* eslint-disable no-unused-vars */
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-  pageLoadTimeout: 100000
-});
+  pageLoadTimeout: 100000,
+})
--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,17 +1,17 @@
-describe("Pokedex", function () {
+describe('Pokedex', function () {
   beforeEach(function () {
-    cy.visit("http://localhost:5000");
-  });
+    cy.visit('http://localhost:5000')
+  })
 
-  it("front page can be opened", function () {
-    cy.contains(/ivysaur/i);
+  it('front page can be opened', function () {
+    cy.contains(/ivysaur/i)
     cy.contains(
       /Pokémon and Pokémon character names are trademarks of Nintendo./i
-    );
-  });
+    )
+  })
 
-  it("pokemon page can be navigated to", function () {
-    cy.contains(/ivysaur/i).click();
-    cy.contains(/chlorophyll/i);
-  });
-});
+  it('pokemon page can be navigated to', function () {
+    cy.contains(/ivysaur/i).click()
+    cy.contains(/chlorophyll/i)
+  })
+})
