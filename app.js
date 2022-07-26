fce75fbdad499da4fe2420088a7b5fdd61464020
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-07-26 16:17:12 +0300

    Add Heroku health check

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const pjson = require('./package.json')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
@@ -9,4 +10,11 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send(pjson.version)
 })
