414cfcf9d7badda21a226cddf680815086cc7655
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-07-26 15:22:55 +0300

    11.9

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-undef */
 
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('invidulal page can be opened', function () {
+    cy.contains('charmander').click()
+    cy.contains('blaze')
+    cy.contains('solar power')
   })
 })
