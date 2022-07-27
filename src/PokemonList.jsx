fd6178be15ee7cdafaa77465014e053b69a9ab32
Filip <filip.eller@aalto.fi>
2022-07-27 11:30:07 +0300

    Complete 11.10, deploying your application to Heroku.

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -5,12 +5,15 @@ const PokemonList = ({ pokemonList }) => {
   return (
     <div className="list-container">
       {pokemonList.map(({ id, name }) => (
-        <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
-          <div
-            className="list-item-name"
-          >
-            {name}
-          </div>
+        <Link
+          key={id}
+          to={`/pokemon/${name}`}
+          className="list-item"
+          style={{
+            backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})`,
+          }}
+        >
+          <div className="list-item-name">{name}</div>
         </Link>
       ))}
     </div>
