c87baf0f15d58bef1c6b1a967592233c187865b4
Mario Segovia <marioseg_02@hotmail.com>
2022-08-03 18:50:05 +0100

    Test 3 Deployment

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,18 +1,18 @@
+/* eslint-disable no-undef */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    Cypress.config('pageLoadTimeout', 100000)
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    Cypress.config('pageLoadTimeout', 100000);
+    cy.visit('http://localhost:5000');
+    cy.contains('ivysaur');
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.');
   }),
 
   it('link can be opened', function() {
-    Cypress.config('pageLoadTimeout', 100000)
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur').click()
-    cy.contains('chlorophyll')
-    
-  })
+    Cypress.config('pageLoadTimeout', 100000);
+    cy.visit('http://localhost:5000');
+    cy.contains('ivysaur').click();
+    cy.contains('chlorophyll');
+  });
 
-})
+});
 
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,13 +22,13 @@ module.exports = (on, config) => {
 
   module.exports = (on) => {
     on('before:browser:launch', (browser = {}, args) => {
-      if (browser.name === 'chrome') { 
-        // ^ make sure this is your browser name, you may 
+      if (browser.name === 'chrome') {
+        // ^ make sure this is your browser name, you may
         // be using 'canary' or 'chromium' for example, so change it to match!
-        args.push('--proxy-bypass-list=<-loopback>')
-        return args
+        args.push('--proxy-bypass-list=<-loopback>');
+        return args;
       }
-    })
-  }
+    });
+  };
 
-}
+};
