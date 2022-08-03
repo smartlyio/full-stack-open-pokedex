a22c34e19586e16bdb753cce2b5a79086aa5c157
Mohkula <matias.himanen@helsinki.fi>
2022-08-03 12:21:32 +0300

    exercise 11.9

--- cypress/e2e/frontPage.cy.js
+++ cypress/e2e/frontPage.cy.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
 		cy.contains('ivysaur')
 		cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
 	})
+	it('navigation works', function() {
+		cy.visit('http://localhost:5000/pokemon/ivysaur')
+		
+		cy.contains('chlorophyll')
+	})
 })
