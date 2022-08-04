94794a57f8c3bd29341cbc3f90c66a288f1e3b94
Mohkula <matias.himanen@helsinki.fi>
2022-08-04 14:12:41 +0300

    exercise 11.9

--- cypress/integration/frontPage.spec.js
+++ cypress/integration/frontPage.spec.js
@@ -1,8 +1,9 @@
 describe('Pokedex', function() {
-	beforeEach(function() {
-		cy.visit('http://localhost:5000')
-	})
+	
+
 	it('front page can be opened', function() {
+		cy.visit('http://localhost:5000')
+
 		cy.contains('ivysaur')
 		cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
 	})
@@ -10,6 +11,8 @@ describe('Pokedex', function() {
 	
 	it('navigation works', function() {
 		
+		cy.visit('http://localhost:5000')
+
 
 		cy.get('[href="/pokemon/charmander"]').click()
 		
