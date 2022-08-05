d857de12611881b18c0c55f1ab616d4cf020b125
Mohkula <matias.himanen@helsinki.fi>
2022-08-05 13:12:15 +0300

    exercise 11.11

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const { version } = require('./package.json')
+
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
@@ -10,4 +12,12 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
 	// eslint-disable-next-line no-console
 	console.log('server started on port 5000')
+})
+
+app.get('/health', (req, res) => {
+	res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+	res.send(version)
 })
