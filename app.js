f8063d8e224d2ecd40d0982f55add82b9e4096bb
DawidBal <baluszynskidawid@gmail.com>
2022-08-09 21:14:17 +0200

    he heee

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
