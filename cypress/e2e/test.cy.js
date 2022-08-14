864d0b88d83cb35036470ad6ff50960daead4756
tomasz-piwowarski <piwtom99@gmail.com>
2022-08-14 18:27:13 +0200

    Exercises 11.9

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,15 +1,15 @@
 describe('Pokedex', function () {
+  it('can navigate to a page of a particular pokemon', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+
   it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
-  })
-
-  it('can navigate to a page of a particular pokemon', function () {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur').click()
-    cy.contains('chlorophyll')
   })
 })
