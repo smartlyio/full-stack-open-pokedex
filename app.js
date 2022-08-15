0e70a70bc46d486b9f084f2b997408661e20d98b
LiamLi2020 <619568942@qq.com>
2022-08-15 20:33:41 +0300

    11.10 2

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line no-console
 })
