4072b1279c6916a9ae887281380294f8c2b77330
LiamLi2020 <619568942@qq.com>
2022-08-16 15:24:22 +0300

    11.16 try11

--- app.js
+++ app.js
@@ -10,15 +10,6 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
-app.get('/health2', (req, res) => {
-  res.send('ok2')
-})
-
 app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line no-console
 })
-
-//here is the change made for 11.13
-//here is the change made for 11.13 again
-//Just Add some change to check whether it changes something
-//Just Add more change to check whether it changes something
