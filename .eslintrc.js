8a587c11a11cc2f9f6ff708fd7371f776c6b2920
irinh <irene_lee@live.com.sg>
2022-08-22 22:24:44 +0800

    Resolved eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -45,6 +46,7 @@ module.exports = {
       "error", { "before": true, "after": true }
     ],
     "no-console": "off",
-    "react/prop-types": 0
+    "react/prop-types": 0,
+    "no-unused-vars": "off"
   }
 }
