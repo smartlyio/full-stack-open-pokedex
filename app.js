cee2ca821f0370a3b8b89344e5f81289f715b1e4
irinh <irene_lee@live.com.sg>
2022-08-24 21:43:51 +0800

    Added healthcheck

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get("/version", (req, res) => {
+  res.send("1")
+})
+
+app.get("/health", (req, res) => {
+  res.send("ok")
+})
+
+app.post("/version", (req, res) => {
+  res.send("2")
+})
+
+
 app.use(express.static("dist"))
 
 app.listen(PORT, () => {
