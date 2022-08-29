920a72b9b6b6a8c340d397eb7e680e92f9e00d40
Antti Halmetoja <anttihalmetoja@gmail.com>
2022-08-29 10:21:13 +0300

    fix conf

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    "pageLoadTimeout": 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
