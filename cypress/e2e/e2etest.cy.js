720fd9b1b6eb97d49a7c0007c754df11981c46e5
Ali Abu Salam <aliadadehmav@gmail.com>
2022-08-31 11:49:20 +0700

    Fix bug on e2e test

--- cypress/e2e/e2etest.cy.js
+++ cypress/e2e/e2etest.cy.js
@@ -2,6 +2,6 @@ describe('Pokedex', function(){
   it('front page can be opened', function(){
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
-    cy.contains('Pokemon and Pokemon character names are trademark of Nintendo.')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
