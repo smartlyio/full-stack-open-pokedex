b13d281438e46fb2fa495c88ee5811e9fdb8ed40
Evgenii Smirnov <evgenii.smirnov@eficode.com>
2022-08-31 16:45:16 +0300

    more cypress

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,9 +1,17 @@
 /*global cy*/
-
+/*eslint no-undef: "error"*/
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+
+describe('Pokedex', function() {
+  it('navigation to individual pokemon page works', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('butterfree').click()
+    cy.contains('tinted lens')
   })
 })
