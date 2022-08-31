d63f364617ff0c78f32cc216bce13bdecb6a434b
Evgenii Smirnov <evgenii.smirnov@eficode.com>
2022-08-31 16:49:36 +0300

    page load timeout cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    'pageLoadTimeout': 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
