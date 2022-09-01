f97e9190f28a8bd38f89692a6aac013b59c5afb4
Evgenii Smirnov <evgenii.smirnov@eficode.com>
2022-09-01 11:32:45 +0300

    health check and rollback

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// healthcheck
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
