d6f99b93256975ea25abcba8ea6dd2d36b914ad3
Ali Abu Salam <aliadadehmav@gmail.com>
2022-09-02 19:46:23 +0700

    add new endpoint for healthcheck after deployment and another dummy endpoint for checking whether there's an actual change in code

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (_, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) //eslint-disable-line
 })
