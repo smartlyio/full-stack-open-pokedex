44226e39036e29689ceb4d31ee8e2696a4db3804
Ehivan11 <ehivan11@gmail.com>
2022-09-03 19:37:30 -0300

    Add healthcheck an do rollback if not working

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,7 +1,7 @@
 /* global cy */
 
 describe('Pokedex', function() {
-  it.skip('front page can be opened', function() {
+  it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('kakuna')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
