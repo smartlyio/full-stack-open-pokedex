9f42d566c2acdec769dd32728067b7de49441e12
Jorge Balibrea <jorge.balibrea1@gmail.com>
2022-09-04 15:36:04 +0200

    add e2e pokemon navigate to

--- cypress/integration/pokedex.cy.js
+++ cypress/integration/pokedex.cy.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigate to', function(){
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
