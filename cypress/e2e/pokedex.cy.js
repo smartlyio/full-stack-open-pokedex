9fbb905c62bf425063492220c98e09b61342fcdb
Uvaldo Zumaya <uvaldo.zumaya@spaceforce.mil>
2022-09-04 08:10:47 -0600

    Added new test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('Certain pokemon can be selected', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
