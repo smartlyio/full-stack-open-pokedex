edb559377729a063b67bbd5e608326dea0db74e2
Jorge Balibrea <jorge.balibrea1@gmail.com>
2022-09-04 13:13:17 +0200

    Added workflow hello world

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,18 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true,
   },
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended"
   ],
+  "settings":{
+    "react":{
+      "version": "detect",
+    },
+  },
   "parserOptions": {
     "ecmaFeatures": {
       "jsx": true
