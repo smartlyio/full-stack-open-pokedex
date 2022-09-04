a03e83637b029843f01e6f5cdcce7169bbe8745c
Jorge Balibrea <jorge.balibrea1@gmail.com>
2022-09-04 17:26:07 +0200

    added healthcheck rollback

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const { version } = require('./package.json')
+
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(version) // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
