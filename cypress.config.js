8d914f1ef80a7578f3702465cf64c396ba63bde8
asnabryg <8mixu8@gmail.com>
2022-09-07 16:39:30 +0300

    rest 11.9

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,5 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+  e2e: {},
+})
--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,10 +1,16 @@
 
 describe('Pokedex', function() {
-  it('fron page can be opened', function() {
+  it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('tyranitar').click()
+    cy.contains('sand stream')
   })
 })
