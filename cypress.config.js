5a027147d4dc68d9f394eeb0d6a1c484af2cfac2
asnabryg <8mixu8@gmail.com>
2022-09-07 16:57:58 +0300

    fixed cy test

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,12 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {},
-})
+  retries: {
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0,
+  },
+});
--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,5 +1,6 @@
 
 describe('Pokedex', function() {
+  
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
