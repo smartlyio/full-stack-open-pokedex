a44c9d4e6ff88fb9fbba594eeba4ce571fe2841f
rushilshakya <rushil.shakya@gmail.com>
2022-09-13 22:13:06 +0545

    chore: disable eslint for cy

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    "node": true,
-    "jest/globals": true,
-    "cypress/globals": true
+    "node": true
   },
   "extends": [
     "eslint:recommended",
