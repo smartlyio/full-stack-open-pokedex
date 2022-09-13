55a67b9e1897a13eb56df1251e74f3a2dd212237
rushilshakya <rushil.shakya@gmail.com>
2022-09-13 22:04:21 +0545

    fix: update eslint env for cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     "es6": true,
     "jest/globals": true,
     "node": true,
-    "cypress": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
