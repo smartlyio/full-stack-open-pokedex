5a620750a9163c03a1df5d486a8a6c593307ae87
rushilshakya <rushil.shakya@gmail.com>
2022-09-14 11:00:14 +0545

    feat: add cypress test for 11.9

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('can navigate to ivysaur from front page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
