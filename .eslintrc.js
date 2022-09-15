0348b8c5cbfa139be7f5defa95488fa3f7b6a2e8
Serena Black <sunsetsin1.sy@gmail.com>
2022-09-15 14:13:30 +0545

    cypress globals set to true in eslintrc line 7

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     "jest/globals": true,
     node: true,
-    'cypress/globals': false
+    'cypress/globals': true
   },
   extends: ["eslint:recommended",
   // {
