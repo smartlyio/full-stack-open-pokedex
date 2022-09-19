2b5b554d24df153e0611f4cd629ce55ee4357a60
Noriel <N/A>
2022-09-19 08:47:20 +0200

    P11C - Ex 11.12, Working shell script

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+res=$(curl -s https://young-frost-5915.fly.dev/health)
 
+if [ "$res" == "ok" ]; then
+  echo "Succeeded curl to /health"
+  exit 0
+  fi
+
+echo "Failed curl to /health"
 # 1: Failed, 0: Succeeded.
-exit 1 
+exit 1
