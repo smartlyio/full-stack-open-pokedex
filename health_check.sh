e24300832ba5a220d192c3a27de80f1174cf7fdc
Noriel <N/A>
2022-09-19 09:05:52 +0200

    P11C - Ex 11.12, Remove script from fly.toml

--- health_check.sh
+++ health_check.sh
@@ -2,13 +2,11 @@
 
 res=$(curl -s https://young-frost-5915.fly.dev/health)
 
-echo "$res"
+if [ "$res" == "ok" ]; then
+  echo "Succeeded curl to /health"
+  exit 0
+  fi
 
-# if [ "$res" == "ok" ]; then
-#   echo "Succeeded curl to /health"
-#   exit 0
-#   fi
-
-# echo "Failed curl to /health"
+echo "Failed curl to /health"
 # 1: Failed, 0: Succeeded.
 exit 0
