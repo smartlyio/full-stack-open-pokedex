32e3832921f41a582680ae7cce07bc51548766e8
Niklas Mettälä <njmett@utu.fi>
2022-09-21 00:43:49 +0300

    11.9 added a nav test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('navigate to a pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('vulpix').click()
+    cy.contains('drought')
+  })
 })
