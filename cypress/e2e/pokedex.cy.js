bb1e8baaff6874f2c8b7da8013758b520ceaa581
Niklas Mettälä <njmett@utu.fi>
2022-09-21 00:33:26 +0300

    11.9 init

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
