332328839ca9b2831d4c4a6124a39dcb62ec5938
Daniel Redly <dredly@gmail.com>
2022-09-21 21:48:42 +0100

    Ex11.11

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'BIG TIME ERROR'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
