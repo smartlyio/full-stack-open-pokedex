a0a299b3d94ac3bcfa03215d794d5428bca46ada
@alexandre174 <aupli16@freeuni.edu.ge>
2022-09-22 05:45:46 +0400

    exercise 11.16ver12

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 
 const ErrorMessage = ({ error }) => (
-  <div data-testid="error">An error occured ouou: dance {error.toString()}</div>
+  <div data-testid="error">An error occured: {error.toString()}</div>
 )
 
 export default ErrorMessage
