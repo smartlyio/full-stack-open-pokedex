f8af93a57ac764726ee414ed3b1feb3b59a720c5
@alexandre174 <aupli16@freeuni.edu.ge>
2022-09-22 05:37:02 +0400

    skip exercise 11.16ver9

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable */
+
 import React from 'react'
 
 const ErrorMessage = ({ error }) => (
-  <div data-testid="error">An error occured: {error.toString()}</div>
+  <div data-testid="error">An error occured ouou: {error.toString()}</div>
 )
 
 export default ErrorMessage
