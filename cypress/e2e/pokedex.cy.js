0d4de90c5b7416afadf48bbf2687305c4730b840
Elmo Vahvaselkä <elmo.vahvaselka@hotmail.com>
2022-09-23 16:11:08 +0300

    11.9 Simple end to end -tests
    New e2e test for opening Charizerd

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,15 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+describe('Pokedex', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('Charizard can be opened', function () {
+    cy.contains('charizard').click()
+    cy.contains('solar power')
   })
 })
