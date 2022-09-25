247dfedf6e65b84b93c2867d7abd4bde6379ba18
Jade Ye <w0430394@nscc.ca>
2022-09-25 13:59:32 -0300

    Finish 11.9: add one more e2e test.

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -7,4 +7,9 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
