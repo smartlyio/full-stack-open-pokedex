cecbcc9589f0df35aea4e209396b2d991e4eef72
Elmo Vahvaselkä <elmo.vahvaselka@hotmail.com>
2022-09-25 11:40:21 +0300

    Testing that pull request doesn't update versioning

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('11.15.0') // change this string to ensure a new version deployed
+  res.send('0.0.1') // change this string to ensure a new version deployed
 })
