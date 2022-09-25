9c6ccfdc98a3aadd72d0e9e94cb5bf3dea889dbf
(Bea)*2 <77938697+Beatrice-Beacco@users.noreply.github.com>
2022-09-25 18:22:42 +0200

    cypress port

--- cypress/integration/homepage-test.js
+++ cypress/integration/homepage-test.js
@@ -1,6 +1,10 @@
+import process from "node:process";
+const PORT = 5000;
+const CURRENT_PORT = process.env.PORT || PORT;
+
 describe("Pokedex", function () {
   it("front page can be opened", function () {
-    cy.visit("http://localhost:5000");
+    cy.visit("http://localhost:" + CURRENT_PORT);
     cy.contains("ivysaur");
     cy.contains(
       "Pokémon and Pokémon character names are trademarks of Nintendo."
