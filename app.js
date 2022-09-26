10157ae7df4a11cd61c397fe3f915789400ce101
Katariina Mahkonen <katariina.mahkonen@silo.ai>
2022-09-26 13:28:29 +0300

    fixing lint errors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-// eslint-disable-next-line no-console
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
