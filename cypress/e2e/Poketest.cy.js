65a87faa01f4d51f15b5ccc710881628f391e401
Katariina Mahkonen <katariina.mahkonen@silo.ai>
2022-09-26 18:03:15 +0300

    cyppress test Navitest

--- cypress/e2e/Poketest.cy.js
+++ cypress/e2e/Poketest.cy.js
@@ -1,7 +1,23 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+})
+
+describe('Navitest', function () {
+  it('can navigate from front page to ivysaur', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+    cy.get('a[href*="ivysaur"]').click()
+    cy.get('div[class*="pokemon-page"]')
+      .find('div[class="pokemon-name"]')
+      .contains('ivysaur')
   })
 })
