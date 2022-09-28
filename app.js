d76370ba3d8ec4b0d9fc92a83929dde4a83e92f5
Daniel Redly <dredly@gmail.com>
2022-09-28 09:41:12 +0100

    Added fake error

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw new Error('fake error')
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
