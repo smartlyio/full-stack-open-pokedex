e0b688de711bcf7b9652416885117de75a6bdc3c
Daniel Redly <dredly@gmail.com>
2022-09-28 08:58:33 +0100

    General kenobi

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get('/marco', (req, res) => {
   res.send('polo!!')
 })
 
+app.get('/hello there', (req, res) => {
+  res.send('General Kenobi')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5001')
