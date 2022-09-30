17667e107c8d36936a0dc920d0d71dc1f435e23a
Katariina Mahkonen <katariina.mahkonen@silo.ai>
2022-09-30 14:05:27 +0300

    hope it works

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
+// Heroku/Fly.io dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
