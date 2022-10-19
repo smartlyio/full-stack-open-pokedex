7cb6a8b538ffbb78d6088250f2def23c349fe816
otdot <otto.mursula@edu.bc.fi>
2022-10-19 12:35:10 +0300

    fixed lint issues

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const config = require("./config");
 const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5001;
+const PORT = config.PORT || 5001;
 
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
-  console.log("server started on port 5001");
+  /* eslint-disable no-console */
+  console.log(`server started on port  ${PORT}`);
+  /* eslint-enable no-console */
 });
--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const ErrorMessage = ({ error }) => (
   <div data-testid="error">An error occured: {error.toString()}</div>
-)
+);
 
-export default ErrorMessage
+export default ErrorMessage;
