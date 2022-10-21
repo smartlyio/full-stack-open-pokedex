e4a6c551b5b4d4bda68eb2cc0578e5e939ccd97d
1michaelohayon <1michaelohayon@gmail.com>
2022-10-21 15:10:11 +0300

    v

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
