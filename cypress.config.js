2330228623e6584905e0ce9efdd8291f5f49d819
ltmj <lauri.jaakkonen@tuni.fi>
2022-10-21 12:53:36 +0300

    cypress linting errors fixed hopefully

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const { node } = require('webpack');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      node
+      config
     },
   },
 });
