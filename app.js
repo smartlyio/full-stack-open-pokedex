646f6f69fb0dc6de918cec18455b98fd1b5e5fe2
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2022-10-21 13:07:33 +0530

    latest commit

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 
+
 app.listen(PORT, () => {
   //console.log('server started on port 5000')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
