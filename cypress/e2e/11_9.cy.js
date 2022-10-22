ce6cd571424eadb1a1ce35b7834fa09e75c3a990
ltmj <lauri.jaakkonen@tuni.fi>
2022-10-22 13:23:51 +0300

    11.9 done

--- cypress/e2e/11_9.cy.js
+++ cypress/e2e/11_9.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('charmander').click()
+    cy.contains('charmander')
+    cy.contains('solar power')
+    cy.contains('blaze')
+  })
 })
