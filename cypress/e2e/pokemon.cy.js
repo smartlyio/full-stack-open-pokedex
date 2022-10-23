00376f3b2474f2d5b416ec02e04369456290af44
utkuzun <utkuzun50@gmail.com>
2022-10-23 14:37:27 +0300

    lint and cypress action fix

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -6,4 +6,14 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('can open a pokemos"s page', function () {
+    // cy.server()
+    // cy.route('GET', '/api/v2/pokemon/ivysaur').as('pokePage')
+
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    // cy.wait('@pokePage')
+    cy.contains('chlorophyll')
+  })
 })
