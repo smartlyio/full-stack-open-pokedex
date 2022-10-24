76ae78c2f7182f8c58c807a7afdf5db73af8becb
Rahul Desar <desarrahul@gmail.com>
2022-10-25 00:41:16 +0545

    custom endoint (health) checking script added

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+NAME=$(curl https://rahul-pokedex.fly.dev/health)
+if [ "$NAME" == "ok" ]; then 
+  echo "HEALTH ENDPOINT ONLINE"
+else
+  echo "CANNOT REACH HEALTH ENDPOINT"
+fi
 
-exit 1 #exit status 1 means that the script "fails"
+exit 1 #exit status 1 means that the script "fails"https://rahul-pokedex.fly.dev/health
