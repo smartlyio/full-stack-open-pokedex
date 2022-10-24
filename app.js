d6b30a377cd53c585fd19c4c14cf472f92997dab
Rahul Desar <desarrahul@gmail.com>
2022-10-24 20:40:17 +0545

    tesing /health

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1.2') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log('server started on port 5000')
