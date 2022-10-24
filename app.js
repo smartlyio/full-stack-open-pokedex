d82c5bc8487e5083369adfbd07bc560fa5dbeda8
Rahul Desar <desarrahul@gmail.com>
2022-10-24 17:07:07 +0545

    testing lint action with some errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
