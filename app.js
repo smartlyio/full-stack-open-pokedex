f42c4ff9ac66898f66300d0e9b3142bede6bcfce
ltmj <lauri.jaakkonen@tuni.fi>
2022-10-25 12:11:15 +0300

    version 2 but intentionally broken

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('2')
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
