0bd5b6f1b5044f0a59a149cf7af7e1eb962df701
LaukkaE <erkkalaukkanen@gmail.com>
2022-10-27 00:27:34 +0300

    add cypress

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
