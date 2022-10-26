f1a1ad9d1aae97c6bfae1dd4bc92da93bf324c6c
LaukkaE <erkkalaukkanen@gmail.com>
2022-10-27 00:38:38 +0300

    add e2e test for navigating in pokedex

--- cypress/e2e/e2e_test.cy.js
+++ cypress/e2e/e2e_test.cy.js
@@ -1,8 +1,17 @@
+//LOCAALISTI CYPRESS AUKI : NPX CYPRESS OPEN
+
 /* eslint-disable no-undef */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+
+describe('navigation works', () => {
+  it('clicking a pokemon opens their info', () => {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
