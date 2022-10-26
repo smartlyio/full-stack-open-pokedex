56f56423e2ae853ac89bbd569f807b887c1a3dbd
vikas singh <vikas.singh@edu.turkuamk.fi>
2022-10-26 08:41:23 +0300

    adding e2e testing 11.9

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -21,7 +21,7 @@ import './commands'
 
 describe('Pokedex', () => {
     it('front page can be opened', () => {
-      cy.visit('http://localhost:8080')
+      cy.visit('http://localhost:5000')
       cy.contains('ivysaur')
       cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
     })
