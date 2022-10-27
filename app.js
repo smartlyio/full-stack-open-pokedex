b5458615fdf158ced4de2871c3ff2f37e7e76c86
LaukkaE <erkkalaukkanen@gmail.com>
2022-10-27 23:46:56 +0300

    add healthcheck and rollback

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
+
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
