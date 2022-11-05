ec5baddabddadfa1ff751cc593cb2d767059b298
Alexander <josenavasiv@gmail.com>
2022-11-05 15:20:34 -0700

    Fixed linting3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
 	env: {
+		browser: true,
+		es6: true,
+		'jest/globals': true,
 		commonjs: true,
-		es2021: true,
 		node: true,
 	},
 	extends: ['eslint:recommended', 'plugin:react/recommended'],
