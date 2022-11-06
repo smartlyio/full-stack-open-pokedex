8865b167995051bd76eafe002ec7f0d8d97aaa1e
Alexander <josenavasiv@gmail.com>
2022-11-05 17:40:07 -0700

    changed rock color

--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -3,5 +3,7 @@ describe('Pokedex', function () {
 		cy.visit('http://localhost:5000/');
 		cy.contains('ivysaur');
 		cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.');
+		
 	});
+	
 });
