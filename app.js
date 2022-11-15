c0c17071acf959529a05f016838b5fa7ac063d94
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-15 09:42:45 +0200

    Exercise 11.10

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express()
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 8080
+const port = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
