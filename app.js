58c5d22aefcda85ee47ae6029f33e66123f25526
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-15 12:29:50 +0200

    Exercise 11.11

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 8080')
