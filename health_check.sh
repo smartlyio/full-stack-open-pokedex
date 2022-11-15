a11e4ad715a987a4318c1e2cdc9b7c2dbc5add06
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-15 16:24:43 +0200

    Exercise 11.12

--- health_check.sh
+++ health_check.sh
@@ -3,7 +3,7 @@
 
 echo "Hello from shell script"
 
-content=$(curl -sLI https:stefanpokedex.fly.dev/health)
+content=$(curl -s https://stefanpokedex.fly.dev/health)
 
 if [ "$content" == "ok" ] 
 then
