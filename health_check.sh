8588fcb25a47c6462a69f57e4dd9e6b890a6fe3b
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-15 16:06:28 +0200

    Exercise 11.12

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+# exit status 0 means that the script "succeeds"
 
 echo "Hello from shell script"
 
-exit 0  # exit status 0 means that the script "succeeds"
+content=$(curl -sLI https:stefanpokedex.fly.dev/health)
+
+if [ "$res" == "ok" ] 
+then
+    echo "/healt returned ok"
+    exit 0
+else
+    echo "/healt didn't return ok"
+    exit 1
+fi 
