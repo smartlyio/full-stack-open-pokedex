6e551e69ab9c8623e5fd5bbe19d26ce40ead8817
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-11-15 13:38:59 +0200

    Exercise 11.11

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ const port = process.env.PORT || 8080
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log('/health')
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log('/version')
   res.send('3') // change this string to ensure a new version deployed
 })
 
