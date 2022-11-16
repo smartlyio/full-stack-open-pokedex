57f8980d3be43e39f8a1f61a95d7e5b0d59d14b6
baxterjimuk <81945889+baxterjimuk@users.noreply.github.com>
2022-11-16 15:44:28 +0800

    deploy #1

--- app.js
+++ app.js
@@ -8,5 +8,13 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
 })
