2d520eada5c116995ab7785956b26f13e235dafb
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-11-18 19:34:27 +0200

    ex 11.9

---
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
