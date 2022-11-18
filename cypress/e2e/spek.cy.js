14d35832b953dc3a46678cd3c628412cba797895
Rory Buckle <rorytbuckle@gmail.com>
2022-11-18 16:20:57 -0500

    added navigation test

--- cypress/e2e/spek.cy.js
+++ cypress/e2e/spek.cy.js
@@ -6,4 +6,11 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('Pokemon can be navigated to', function () {
+    cy.visit('http://localhost:5000')
+    cy.get('a').contains('ivysaur').click()
+    cy.location('pathname').should('eq', '/pokemon/ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
