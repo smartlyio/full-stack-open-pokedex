ae17b7426d5f90f29e608e0dd900b1978b4a6bd4
Rory Buckle <rorytbuckle@gmail.com>
2022-11-18 16:07:48 -0500

    fixed cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-// const { defineConfig } = require('cypress')
+/* eslint-disable no-unused-vars */
+const { defineConfig } = require('cypress')
 
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// })
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
