91c783b2e7c61fe89291a4b815645bd9e8e07b82
VasiliyTemniy <flash_er@mail.ru>
2022-11-18 22:08:03 +0300

    this should work for 11.9

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+// eslint-disable-next-line no-undef
+module.exports = defineConfig({
+  e2e: {
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
