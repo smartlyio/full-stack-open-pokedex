80105719e604c39ffa85a57ee6b601249181a96e
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-11-20 17:57:47 +0200

    pipeline

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
+})
+app.get('/health', (_, res) => {
+  res.send('ok')
 })
--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  }, 
+  },
 })
 /* eslint-enable no-unused-vars */
