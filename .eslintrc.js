1824a0360e501e081b3ac5d0fe01958e45318418
codeaktivist <codeaktivist@gmail.com>
2022-12-05 11:20:36 +0100

    fixed lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "node": true,
     "es6": true,
     "jest/globals": true,
-    "cypress": true
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -18,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
