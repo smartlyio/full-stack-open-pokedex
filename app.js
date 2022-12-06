ca6e8fa76e4c1986fc36750ef42a5f587890d2b3
codeaktivist <codeaktivist@gmail.com>
2022-12-06 09:45:02 +0100

    Stage 11.11
    break the app to test health check

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  throw 'erroRRR...'
+  res.send('ok') // eslint-disable-line no-unreachable
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000') // eslint-disable-line
 })
