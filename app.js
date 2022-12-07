ad9d5e0098a8c3cbbc9ad5e9f937658712790bc1
codeaktivist <codeaktivist@gmail.com>
2022-12-07 23:29:50 +0100

    added periodic health check #minor

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express')
 const app = express()
 
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
-
 app.get('/version', (req, res) => {
   res.send('17')
+})
+
+// switch to "break" the app for periodic checks
+let isWorking = true
+
+app.get('/break', (req, res) => {
+  isWorking = false
+  res.send('you broke the app')
+})
+app.get('/fix', (req, res) => {
+  isWorking = true
+  res.send('you fixed the app')
+})
+
+app.get('/health', (req, res) => {
+  if (isWorking) {
+    res.send('ok')
+  } else {
+    res.status(404)
+  }
 })
 
 app.listen(PORT, () => {
