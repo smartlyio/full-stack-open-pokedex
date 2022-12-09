692a5162cf5628238ef80aca88db8df4d9d05a12
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-12-09 19:01:20 +0100

    add http check to health request script

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+EXPECT="ok"
+RESPONSE=$(curl http://localhost:5000/health)
 
-exit 0  # exit status 0 means that the script "succeeds"
+  echo "$EXPECT"
+  echo $RESPONSE
+
+if [ "$EXPECT" = "$RESPONSE" ]; then
+    echo "Strings are equal."
+    exit 0  # exit status 0 means that the script "succeeds"
+else
+    echo "Strings are not equal."
+    exit 1  # exit status 1 means that the script "fails"
+fi
