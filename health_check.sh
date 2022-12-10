93014522ddcbbdc7a3b93fb41af370f51d8cd882
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-12-10 18:42:56 +0100

    test deployment

--- health_check.sh
+++ health_check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-EXPECT="ok"
-RESPONSE=$(curl http://localhost:5000/health)
+res=$(curl -s https://fso-ml-pokedex.fly.dev/health)
 
-if [ "$EXPECT" = "$RESPONSE" ]; then
-    echo "Strings are equal."
-    exit 0  # exit status 0 means that the script "succeeds"
-else
-    echo "Strings are not equal."
-    exit 1  # exit status 1 means that the script "fails"
-fi
+if [ "$res" == "ok" ]; then
+  echo "Succeeded curl to /health"
+  exit 0
+  fi
+
+echo "Failed curl to /health"
+# 0: OK, 1: Bad.
+exit 1
