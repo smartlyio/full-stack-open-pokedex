da3cfdd1d81b296e06924e831616c64a289d48db
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-12-10 16:22:44 +0100

    test deployment

--- health_check.sh
+++ health_check.sh
@@ -3,9 +3,6 @@
 EXPECT="ok"
 RESPONSE=$(curl http://localhost:5000/health)
 
-  echo "$EXPECT"
-  echo $RESPONSE
-
 if [ "$EXPECT" = "$RESPONSE" ]; then
     echo "Strings are equal."
     exit 0  # exit status 0 means that the script "succeeds"
