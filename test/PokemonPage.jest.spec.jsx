84906754a41ade54e5801d0c76c885348c88d00a
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2022-12-12 19:26:10 +0200

    11.7 test fixes

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -120,6 +120,7 @@ describe('<PokemonPage />', () => {
 
     expect(screen.getByText('Previous')).toHaveAttribute('href', '/pokemon/ditto')
     expect(screen.getByText('Next')).toHaveAttribute('href', '/pokemon/vaporeon')
+
   })
 
   it('should not render previous and next urls if none exist', async () => {
