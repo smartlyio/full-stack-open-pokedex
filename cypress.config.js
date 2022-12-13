91ba1c0183c082017f54496a760a7116ee0983d2
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2022-12-13 20:58:31 +0200

    cypress.config.js changed

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+    baseUrl: 'http://localhost:5000',
+    specPattern: 'e2e/*.cy.js',
+    supportFile: false,
+    pageLoadTimeout: 120000
+  }});
