f34a634c1de5a2e8ac40d862f2d22bbdde866353
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2022-12-14 13:57:47 +0200

    ivysaur test changed

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,8 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
   it('ivysaur page can be opened', function() {
-    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.visit('http://localhost:5000')
+    cy.get('a').eq(1).click()
     cy.contains('ivysaur')
     cy.contains('chlorophyll')
     cy.contains('overgrow')
