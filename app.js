38e9c4d82319db694f3736c43c6ca130c6c4c537
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-12-14 11:17:06 -0700

    11.11

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('2')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 8080')
