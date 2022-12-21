b7bb9b6847c93330949e08d89311a4a24902056a
Julius Goddard <julius.goddard@webforprofessionals.com>
2022-12-21 15:32:50 +0000

    test2

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -27,7 +27,7 @@ const PokemonPage = ({ previous, next }) => {
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
   console.log('hiddenAbility=', hiddenAbility)
-  console.log(next.name)
+
   return (
     <>
       <div className="links">
