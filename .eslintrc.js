53705617da679a3db1c68b94ac45d66c498b71eb
Julius Goddard <julius.goddard@webforprofessionals.com>
2022-12-21 14:35:00 +0000

    lint corrections

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
+    'commonjs': true,
+    'es2021': true,
+    'node': true
   },
   "extends": [
     "eslint:recommended",
@@ -43,7 +43,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
