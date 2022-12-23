354d5188652943e5e70f5ea358a0555ec2836978
Julius Goddard <julius.goddard@webforprofessionals.com>
2022-12-23 11:04:56 +0000

    Experimental Branch

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,9 @@ const App = () => {
       <Switch>
         <Route exact path="/">
           <PokemonList pokemonList={pokemonList} />
+          <div>
+            <p>Hello</p>
+          </div>
         </Route>
         <Route path="/pokemon/:name" render={(routeParams) => {
           const pokemonId = pokemonList.find(({ name }) => name === routeParams.match.params.name).id
