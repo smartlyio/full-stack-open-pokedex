08fdfc0aeadce0ccd6a560b2428de6756df17111
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-12-27 12:12:11 +0100

    completed exercise 11.11

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (_req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
