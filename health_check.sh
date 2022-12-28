37d41ace771d4bdc4424155bf988ab689f3d7022
Alex Allen <99975765+AlexAllen247@users.noreply.github.com>
2022-12-28 08:04:27 +0100

    on exercise 11.12

--- health_check.sh
+++ health_check.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-res=$(curl -s https://little-fire-716.fly.dev/health)
+echo "Hello from shell script"
 
 exit 0  # exit status 0 means that the script "succeeds"
-if [ "$res" == "ok" ]; then
-  echo "Succeeded curl to /health"
-  exit 0
-  fi
-
-echo "Failed curl to /health"
-# 0: OK, 1: Bad.
-exit 1
