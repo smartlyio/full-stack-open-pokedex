2c91a16c6ad4aeb2ba31926f3439ceca299f4237
Topi Kanninen <topi.kanninen@sci.fi>
2022-12-30 19:19:54 +0200

    added more tests

--- cypress/e2e/Pokedex.cy.js
+++ cypress/e2e/Pokedex.cy.js
@@ -5,4 +5,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to certain Pokemon', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('charizard').click()
+    cy.contains('speed').should('exist')
+    cy.contains('solar power').should('exist')
+    cy.get('div[class="pokemon-page pokemon-type-fire"]').should('exist')
+  })
 })
