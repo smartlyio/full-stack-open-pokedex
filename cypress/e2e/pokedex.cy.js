22d47dc92e6c68c38c0c39309583bb0d3ebb5f75
AK <anttikup@metropolia.fi>
2023-01-03 16:04:16 +0200

    Added navigation test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,13 @@
 describe('Pokedex', function() {
-    it('front page can be opened', function() {
-        cy.visit('http://localhost:5000')
-        cy.contains('ivysaur')
-        cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    })
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can navigate to a pokemon', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
