92e4130548254d1b90e4ef6eac79380019d06a15
PupkinLangford <tahellman@gmail.com>
2023-01-05 15:00:59 -0500

    fix e2e

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
@@ -9,7 +9,7 @@ describe('Pokedex', function() {
 
 describe('Pokedex', function() {
   it('pokemon page can be navigated to', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.get('a[href*="/pokemon/ivysaur"]').click()
     cy.contains('chlorophyll')
   })
