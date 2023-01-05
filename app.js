ae58e8d536b133e3689de6ccb9bed510d761d910
PupkinLangford <tahellman@gmail.com>
2023-01-05 15:26:28 -0500

    test broken health check

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 8080
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
+  throw 'error'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
