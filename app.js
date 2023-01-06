1bc1a91543ed4f7aa581efe1bde8eb773dd9954e
alcerar <alcerar@hotmail.com>
2023-01-06 20:10:11 +0100

    add health check

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = express()
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
