b3308d4bcff15f5ceaeff37a6422de226fa0f6d7
YouqinSun <xiaoouvivian.it@gmail.com>
2023-01-07 04:25:12 +0200

    11.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,11 @@ module.exports = {
     "node": true,
     "es6": true,
     "jest/globals": true,
-    // "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
