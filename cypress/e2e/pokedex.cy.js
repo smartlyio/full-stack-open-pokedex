9699e2b9848c3c64429ae84570d5d8471ceae1db
aaartyomsss <59570358+aaartyomsss@users.noreply.github.com>
2023-01-07 15:36:47 +0200

    Add test cases

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,17 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('pokemon details page can be opened', function () {
+    cy.visit('/')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+
+  it('Next pokemon can be opened', function () {
+    cy.visit('/')
+    cy.contains('ivysaur').click()
+    cy.contains('Next').click()
+    cy.contains('venusaur')
+  })
 })
