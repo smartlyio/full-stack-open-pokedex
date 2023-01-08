9a451c02a0a336fd67c02b5bdbaf8908b46c164b
YouqinSun <xiaoouvivian.it@gmail.com>
2023-01-08 02:51:03 +0200

    11.10

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
