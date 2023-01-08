2783cb1a958bd88138a5735e153e72ebf84ec42b
Thomas Alemayehu <thomas2alexmech@gmail.com>
2023-01-08 17:15:55 +0300

    cypress added

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
