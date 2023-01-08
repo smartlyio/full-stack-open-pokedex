391f88b5fd4df8dc5c4b29db93c293519f866e29
YouqinSun <xiaoouvivian.it@gmail.com>
2023-01-08 15:30:50 +0200

    11.12

--- health_check.sh
+++ health_check.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "Hello from shell script"
-echo "trying to install curl with 'apt update & apt install curl'"
-apt update & apt install curl
+#echo "trying to install curl with 'apt update & apt install curl'"
+#apt update & apt install curl
 #CURL='/usr/bin/curl'
 ##URL="/health"
 LOCAL_URL="http://localhost:8080/health"
 URL="https://fullstack-part11.fly.dev/health"
 
-result="$(curl -s $LOCAL_URL)"
+result=$(curl $URL)
 echo "curl result: '$result'"
 
-if [ "$result" == "ok" ]; then
+if [ "${result}" == "ok" ]; then
   echo "health check OK"
   exit 0
 else
