d22a4d3bc535f01eabfd7dbd70ae06701718906f
aaartyomsss <59570358+aaartyomsss@users.noreply.github.com>
2023-01-09 21:23:03 +0200

    Add http health check and throw error

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  throw 'eeeeerrr'
+  // res.send('HEALTH HAS BEEN CHECKED AND IT IS OK')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
