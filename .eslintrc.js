cab08864d7b7e68c08ceb84f0f27ecca4301f074
Austin Wang <yyaustin@bu.edu>
2023-01-10 12:22:33 -0500

    update eslint's quote rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: off,
+    quotes: "off",
     semi: ["error", "never"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
