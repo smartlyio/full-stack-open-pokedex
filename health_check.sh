c7ff3804a0cedb770b6be05889b5057535126b3e
ville <ville.prami@aalto.fi>
2023-01-13 15:30:32 +0200

    11.12

--- health_check.sh
+++ health_check.sh
@@ -3,9 +3,11 @@
 apt-get update -y && apt-get install -y curl
 
 RES=$(curl -L https://my-pokedex.fly.dev/health)
+echo "$RES"
 
 if [ "$RES" = "ok" ]
 then
+    echo "SUCCESS"
     exit 0
 else
     exit 1
