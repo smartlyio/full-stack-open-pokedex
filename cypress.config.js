7a1808878a908c4f2794937ecf8d7c7e86396dc7
jpronkko <jukka.ronkko@iki.fi>
2023-01-17 17:55:52 +0200

    Linting fix

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    /* eslint-disable */
+    setupNodeEvents(on, config) { 
       // implement node event listeners here
     },
   },
