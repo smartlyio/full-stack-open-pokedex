982d768d27aaeb25f08ac14d9c00df58d2e69b2e
Juho Räsänen <juho.rasanen@setuppi.fi>
2023-01-17 09:54:26 +0200

    cypress config file passes eslint now

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
