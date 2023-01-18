0f2a5264eac5dcf7e9dfef694224315711005b4a
jpronkko <jukka.ronkko@iki.fi>
2023-01-18 11:01:39 +0200

    Script health check using curl. Curl install added to Dockerfile.

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+result=$(curl https://fspokedex.fly.dev/health)
+compare_res="ok"
 
-exit 0 # Script "succeeds"
+if [[ "$result" == "$compare_res" ]]; then
+ echo "Got result: $result, similar to $compare_res. Succeeded."
+ exit 0 # Script "succeeds"
+else
+ echo "Got result $result, not similar to $compare_res. Failed."
+ exit 1 # Script "fails"
+fi
 
