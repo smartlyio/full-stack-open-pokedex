c69964fa1e498f2c298471299d3f5a69f3b8f17d
Hyun Chae <daniel.hyun.chae@gmail.com>
2023-01-18 13:54:13 +0100

    Fixed an issue with port

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const app = express();
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
