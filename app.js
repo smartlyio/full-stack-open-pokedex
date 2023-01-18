7b1bc56c742193be5c9669778a155af00e10b570
Hyun Chae <daniel.hyun.chae@gmail.com>
2023-01-18 13:51:36 +0100

    Implemented Exercise 11.10

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
