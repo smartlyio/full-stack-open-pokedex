021568125764620e4a87436d5c853c284817a860
ville <Ville.2000.vhk@gmail.com>
2023-01-20 00:26:43 +0200

    Changed heath_check script

--- health_check.sh
+++ health_check.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-response=$(curl -s https://silent-hill-9183.fly.dev/health)
-if [[ $response == *"ok"* ]]; then
-  exit 0
-else
-  exit 1
-fi
+
+echo "Hello from shell script"
+
+exit 0
