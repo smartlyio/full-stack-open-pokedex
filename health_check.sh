bd989b94e537dbaa03403c0acefac24253329619
ville <Ville.2000.vhk@gmail.com>
2023-01-20 00:05:12 +0200

    Changed heath_check script

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000/health)
-if [ $response -eq 200 ]; then
+response=$(curl -s http://localhost:5000/health)
+if [[ $response == *"ok"* ]]; then
   exit 0
 else
   exit 1
