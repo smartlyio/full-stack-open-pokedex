a1bc00ce6cc8a76760a02ec353eebb181555a881
jpronkko <jukka.ronkko@iki.fi>
2023-01-20 13:23:37 +0200

    Test build by failing health check from app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  //throw 'Health error!'
+  throw 'Health error!'
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
