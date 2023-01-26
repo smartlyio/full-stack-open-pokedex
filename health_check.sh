ecb5ac6226cb4b2a5fd22a94d23f2ace98ee8b96
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2023-01-26 06:52:46 -0300

    fix shell script

--- health_check.sh
+++ health_check.sh
@@ -3,9 +3,8 @@
 VARIAVEL=$(curl -s -D GET https://delicate-morning-8864.fly.dev/health)
 
 if [ $VARIAVEL = "ok" ]; then
+    echo $VARIAVEL
     exit 0  # exit status 0 means that the script "succeeds"
 fi
 
-echo "falhou"
-exit 1
 
