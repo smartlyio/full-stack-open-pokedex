844e60154125b264451ed8204633d24d17e04294
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2023-01-26 06:19:36 -0300

    add conditional health check in shell script

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+VARIAVEL=$(curl -s -D GET http://localhost:8080/health)
 
-exit 0  # exit status 0 means that the script "succeeds"
+if [ $VARIAVEL = "ok" ]; then
+    exit 0  # exit status 0 means that the script "succeeds"
+fi
+
+echo "falhou"
+exit 1
+
