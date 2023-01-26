38fbb2ababaaa66d2c42c154cf351638501bbbb1
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2023-01-26 02:29:40 -0300

    fix deploy

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ app.listen(PORT, () => {
 })
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('should fail') // change this string to ensure a new version deployed
+  res.send('Version V4') // change this string to ensure a new version deployed
 })
