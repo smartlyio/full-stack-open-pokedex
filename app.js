fd000eb7e107a5941eeb6b3fd70789b0e79e70d2
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2023-01-28 10:24:55 -0300

    build status fix

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('Build Action') // change this string to ensure a new version deployed
+  res.send('Fix build status') // change this string to ensure a new version deployed
 })
