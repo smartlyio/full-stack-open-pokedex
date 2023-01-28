eee62b9f466359acdfa6ed0d31605d0c7c7d5eab
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2023-01-28 03:06:50 -0300

    deploy without skip

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('Version V4') // change this string to ensure a new version deployed
+  res.send('Deploy without skip') // change this string to ensure a new version deployed
 })
