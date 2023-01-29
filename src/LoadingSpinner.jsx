a42bd6824b452de697bec34a217eb96d944273ee
Luiz Júnior <34041344+LuizAPJunior@users.noreply.github.com>
2023-01-29 07:48:42 -0300

    add health check action and break the application

--- src/LoadingSpinner.jsx
+++ src/LoadingSpinner.jsx
@@ -4,4 +4,4 @@ const LoadingSpinner = () => (
   <img className="loading-spinner" alt="Loading..." src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" />
 )
 
-export default LoadingSpinner
+export default Loa
