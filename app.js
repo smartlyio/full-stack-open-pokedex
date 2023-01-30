9da199d2c55168ecb569a7ad69990963c54e28f4
safaelmali <tsafaelmali@gmail.com>
2023-01-30 12:28:04 +0300

    Fix eslint errors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+/* eslint-disable no-console */
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
-});
+  console.log('server started on port 5000')
+})
