612f0e74e9b4f589bc9c3c7263820daec271a5f7
Supr0en <erik.ruotsalainen1@gmail.com>
2023-02-02 14:05:02 +0200

    indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     },
     plugins: ['react', 'jest'],
     rules: {
-        indent: ['error', 2],
+        indent: ['error', 4],
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'always'],
