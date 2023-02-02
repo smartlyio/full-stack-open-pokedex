87544aa687f53a51001dc7f72bf3a015f67936a0
CABBY <s2001055@edu.bc.fi>
2023-02-02 13:53:00 +0200

    pokemon can be searched

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
             'Pokémon and Pokémon character names are trademarks of Nintendo.'
         );
     });
+
+    it('pokemon can be searched', function () {
+        cy.visit('http://localhost:8080');
+        cy.contains('ivysaur').click();
+        cy.contains('chlorophyll');
+    });
 });
