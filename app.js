22325674ed62ec306714b393f729746722ef0924
CABBY <s2001055@edu.bc.fi>
2023-02-02 10:01:42 +0200

    tollasta pientä

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // Heroku dynamically sets a port
+// eslint-disable-next-line
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
+    // eslint-disable-next-line
+    console.log('server started on port 5000');
 });
