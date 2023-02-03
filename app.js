3e158fe07cdc2f7adfa8bd716f9717dfd6aa2f87
CABBY <s2001055@edu.bc.fi>
2023-02-03 09:06:16 +0200

    sellaset sinne

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('dist'));
 
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+app.get('/version', (req, res) => {
+    res.send('1');
+});
+
 app.listen(PORT, () => {
     // eslint-disable-next-line
     console.log('server started on port 5000');
