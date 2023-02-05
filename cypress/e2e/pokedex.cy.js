748989c649f57d1860712093f3ad807138db9b31
castrodd <castrodd@gmail.com>
2023-02-05 14:03:56 -0800

    Add second e2e test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('links open subpages', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
