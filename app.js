50026d6ca1f60f2d7f3cb8641f7dd41450b064e0
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-02-13 08:34:27 +0300

    11.5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-// eslint-disable-next-line no-undef
+
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
