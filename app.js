016811b552fd9aa860516c1e9bb5f8c534c962c7
VasiliyTemniy <flash_er@mail.ru>
2023-02-14 14:55:32 +0300

    upd backend for healthcheck for 11.11

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
@@ -10,4 +9,12 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
 })
