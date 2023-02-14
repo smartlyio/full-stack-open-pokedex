c6d4ca9d211b1b7e4ece7a03d79a339768290f44
codeaktivist <codeaktivist@gmail.com>
2023-02-14 09:24:46 +0100

    Added curl to health_check #minor

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+response=$(curl localhost:5000/health)
+echo $response
+
 echo "Hello from shell script"
 echo "Path: $PWD"
+
+if [[ $response -eq 'ok' ]]
+then
+    echo "App is healthy"
+    exit 0
+else
+    echo "App is unhealthy"
+    exit 1
+fi
 
 exit 0 # exit status 1 means that the script "fails"
