7b7cb8018807a496cd565036bddba7d6251f5add
Harris-Ryan <eryanharris1@gmail.com>
2023-02-16 15:21:45 -0700

    retrying failed e2e tests with GH action integration

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+    baseUrl: 'http://localhost:5000',
   },
-});
+})
