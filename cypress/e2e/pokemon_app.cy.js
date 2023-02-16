2216f2be1b01b9bc694bceb24015057659fa9c1f
Harris-Ryan <eryanharris1@gmail.com>
2023-02-16 16:10:40 -0700

    corrected linting error

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -1,17 +1,17 @@
 describe('Pokedex', function() {
-	beforeEach(function() {
-		cy.visit('http://localhost:5000')
-	})
+  beforeEach(function() {
+    cy.visit('http://localhost:5000')
+  })
 
   it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
-	it('Can navigate to individual pokemon page', function() {
-		cy.contains('charizard').click()
-		cy.contains('blaze')
-		cy.contains('solar power')
-		cy.contains('109')
-	})
+  it('Can navigate to individual pokemon page', function() {
+    cy.contains('charizard').click()
+    cy.contains('blaze')
+    cy.contains('solar power')
+    cy.contains('109')
+  })
 })
