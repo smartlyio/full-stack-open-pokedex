e6e0c34b1e4cb6d4c4fd4daec55dfb72327c2917
Harris-Ryan <eryanharris1@gmail.com>
2023-02-16 18:37:46 -0700

    testing fly.io health checks

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('4')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started at: http://localhost:5000')
