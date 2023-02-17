234534872e75558a79b7f49079e1f9f5fddd5aab
Harris-Ryan <eryanharris1@gmail.com>
2023-02-16 18:53:03 -0700

    test deployment with intentional faliure

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('4')
+  res.send('5')
 })
 
 app.listen(PORT, () => {
