b93b48dddeab1df0e1831f0afe71b6b313957a09
Harris-Ryan <eryanharris1@gmail.com>
2023-02-16 19:16:28 -0700

    full release

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
+  // throw 'error...'
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('5')
+  res.send('8')
 })
 
 app.listen(PORT, () => {
