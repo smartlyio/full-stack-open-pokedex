1b3dd7b7fc20c4e6f984845860122f460f786431
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-02-20 22:27:49 +0300

    Update health_check.sh

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+res=$(curl -s https://divine-sound-4603.fly.dev/health)
 
-exit 0  # exit status 0 means that the script "succeeds"
+if [ "$res" == "ok" ]; then
+  echo "Succeeded curl to /health"
+  exit 0
+  fi
+
+echo "Failed curl to /health"
+# 0: OK, 1: Bad.
+exit 1
