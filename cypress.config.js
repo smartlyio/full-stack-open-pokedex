eeecab7be36bc160a8c5eb59f5a6864bc4c41813
Dillan <dillank@gmail.com>
2023-02-21 21:43:58 +1300

    fix lint errors

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(_on, _config) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
   },
