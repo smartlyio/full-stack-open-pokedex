d25d5a065259888a7a7c91c7504e12f3678d3201
TheSimplifiedApp <72549339+TheSimplifiedApp@users.noreply.github.com>
2023-02-22 19:00:42 +0800

    Cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   "env": {
     "browser": true,
-    "node" : true,
+    "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -17,13 +18,13 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
       "error",
-        2
-      ],
+      2
+    ],
     "linebreak-style": [
       "error",
       "unix"
