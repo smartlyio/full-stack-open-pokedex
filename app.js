3c828799b6707b34a3ae1212ff1ab128177dd96c
villagrat <elsainetedelautrec@gmail.com>
2023-02-23 16:22:51 -0300

    part11 exercise 11.11 - fixed our basic broken HTTP health check

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 // A very basic endpoint to check app responds to HTTP requests
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 app.get('/version', (req, res) => {
