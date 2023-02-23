e5152f639c884a41ded4571362269b1a31f2e859
villagrat <elsainetedelautrec@gmail.com>
2023-02-23 16:16:26 -0300

    part11 exercise 11.11 - purposeful deploy Fail, added HTTP health check mechanism

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+// A very basic endpoint to check app responds to HTTP requests
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
