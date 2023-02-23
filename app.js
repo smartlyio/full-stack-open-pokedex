88b6f85a017dacf7cd1ce60bf54d3bb45dba8d2d
villagrat <elsainetedelautrec@gmail.com>
2023-02-23 00:02:19 -0300

    part11 exercise 11.19 - First Fly.io deploy test with Github Actions

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+// Deploy to Fly.io requires the following port
+const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
