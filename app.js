5a5b1ec69da8b86377f15580e9a4068ca2ea857b
villagrat <elsainetedelautrec@gmail.com>
2023-02-23 14:29:49 -0300

    part11 exercise 11.10 - changed port to 8080, fixed fly.toml file config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Deploy to Fly.io requires the following port
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.listen(PORT, () => {
