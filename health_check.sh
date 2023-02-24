eeba923aea916a66699cb7c7c0be7be102079bcc
villagrat <elsainetedelautrec@gmail.com>
2023-02-24 06:28:26 -0300

    part11 exercise 11.12 - final fix for bash, assuming curl is installed on VM

--- health_check.sh
+++ health_check.sh
@@ -2,7 +2,7 @@
 
 echo "The health check script is starting on the Fly.io VM"
 expected_response="ok"
-response=(curl --silent https://villagrat-pokedex-cicd.fly.dev/health)
+response=$(curl --silent https://villagrat-pokedex-cicd.fly.dev/health)
 if [[ "$expected_response" == "${response[0]}" ]]; then
   exit 0 # bash success status code
 else
