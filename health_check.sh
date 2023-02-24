c1729d6cf1a2d600fed1bdb2824f9e3720d68bf0
villagrat <elsainetedelautrec@gmail.com>
2023-02-24 06:37:10 -0300

    part11 exercise 11.12 - final fix for bash, not assuming curl comes preinstalled in VM

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "The health check script is starting on the Fly.io VM"
+apt-get update && apt install -y curl
 expected_response="ok"
 response=$(curl --silent https://villagrat-pokedex-cicd.fly.dev/health)
 if [[ "$expected_response" == "${response[0]}" ]]; then
