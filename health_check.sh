3d2e0025348961286f9f82087b693aa77d142276
villagrat <elsainetedelautrec@gmail.com>
2023-02-24 05:25:15 -0300

    part11 exercise 11.12 - check if script health check is the failing culprit

--- health_check.sh
+++ health_check.sh
@@ -4,7 +4,6 @@ echo "The health check script is starting on the Fly.io VM"
 apt-get update && apt-get -y install curl
 expected_response="ok"
 response=(curl --silent https://villagrat-pokedex-cicd.fly.dev/health)
-exit 0
-# if [[ "$expected_response" == "$response" ]]
-# then exit 0 # bash success status code
-# else exit 1 # bash failure status code
+if [[ "$expected_response" == "$response" ]]
+then exit 0 # bash success status code
+else exit 1 # bash failure status code
