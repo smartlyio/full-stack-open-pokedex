bb09f70eaf55973740c85072e3ee5f673fd3f2e1
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2023-02-26 12:33:03 -0600

    exercise 11.11 Health check and rollback

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const { version } = require('./package.json')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  const string = `ok - version: ${version}`
+  res.send(string)
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
