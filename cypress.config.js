7ce697609456615a1d584e74e82b159668238e37
Allison Tielking <atielkin@alumni.stanford.edu>
2023-03-01 14:06:01 -0500

    no support file

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
