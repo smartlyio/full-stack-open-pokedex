9b477d4a765f4b538cc3d45e7709914afcd5339b
Allison Tielking <atielkin@alumni.stanford.edu>
2023-03-01 20:33:22 -0500

    try catching broken health

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ console.log(PORT)
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
