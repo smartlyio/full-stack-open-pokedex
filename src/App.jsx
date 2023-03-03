5b9d9fa7141f77e5add50cf99e2d880aa1ec8e4d
Antonio <pietroluongo.antonio@gmail.com>
2023-03-03 17:28:51 +0100

    eslint

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,10 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <PokemonList pokemonList={pokemonList} />
         </Route>
-        <Route path="/pokemon/:name" render={(routeParams) => {
+        <Route path='/pokemon/:name' render={(routeParams) => {
           const pokemonId = pokemonList.find(({ name }) => name === routeParams.match.params.name).id
           const previous = pokemonList.find(({ id }) => id === pokemonId - 1)
           const next = pokemonList.find(({ id }) => id === pokemonId + 1)
