ad06c256b6e605b0a9cc1045d65472f7b31f2f4d
Michael Pacleb <michael.pacleb@gmail.com>
2023-03-05 16:06:33 -0500

    Exer#11.6 - Trial for Deployment Pipeline CI.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 5001");
-});
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5001')
+})
