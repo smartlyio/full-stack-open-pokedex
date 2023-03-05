3564c595ab3e1c02408e81fa26511b25913dea12
Achyut <actimsina@gmail.com>
2023-03-05 21:22:02 +0545

    feature 1 added

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' })
+})
+
 app.listen(PORT, () => {
   //eslint-disable-next-line no-console
   console.log('server started on port 5000')
