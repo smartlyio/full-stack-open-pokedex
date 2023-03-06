32d987869fdac281e249452747211b1232618a35
Michael Pacleb <michael.pacleb@gmail.com>
2023-03-06 10:47:42 -0500

    Exer#11.9v2 - Cypress initial test, front page can be opened.

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require("cypress");
+/* eslint-disable no-unused-vars */
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
@@ -6,4 +7,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-});
+})
