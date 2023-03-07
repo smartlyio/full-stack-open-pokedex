3503d0927758c45b9b305824b0262189fd05d52d
Michael Pacleb <michael.pacleb@gmail.com>
2023-03-07 16:58:38 -0500

    Exer#11.11v2 - Simulating an error on health check.

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('3')
+  res.send('v3')
 })
 
 app.listen(PORT, () => {
