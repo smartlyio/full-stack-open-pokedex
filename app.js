6f15bde7f0d19850b33afb7385ea377aa103c665
Michael Pacleb <michael.pacleb@gmail.com>
2023-03-07 16:32:16 -0500

    Exer#11.11v1 - Health check using services.http_checks.

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('3')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5001')
