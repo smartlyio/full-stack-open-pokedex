2e0aa2bb8cd24f2262ac4b7c116c6ca2f1a9ac0a
ivose <ivoseeba@gmail.com>
2023-03-12 18:47:52 +0200

    Task 11.19

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const { version } = require('./package.json')
+
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
@@ -11,7 +13,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send(version) // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
