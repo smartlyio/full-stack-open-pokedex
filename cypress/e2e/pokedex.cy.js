9902c2c225794f028cb7be4b583a3201a8512131
eeva.alanko <eeva.alanko@siili.com>
2023-03-17 14:03:47 +0200

    11.9 add cypress test for navigation

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,13 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:5001')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pages can be navigated', function() {
+    cy.visit('http://localhost:5001')
+    cy.get('[href="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
   })
 })
