6a39b50b6faf76b8b80f34791b1ba4aff75fa802
eeva.alanko <eeva.alanko@siili.com>
2023-03-17 17:37:41 +0200

    11.11 health check

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-unreachable
+  res.send('health ok')
+})
+
+app.get('/version', (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log(req)
+  res.send('TODO') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5001')
