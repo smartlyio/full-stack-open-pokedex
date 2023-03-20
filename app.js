e3fd5f175f58de51363d205acdaf000620d16a10
ivose <ivoseeba@gmail.com>
2023-03-20 10:09:39 +0200

    fixes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"))
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
