1fe64902842252f3ae86a592937f86fe2f785ad1
teofou <teofou13@gmail.com>
2023-03-21 20:37:05 +0200

    Revert "third cypress"
    This reverts commit d9f63c473dc81a28b1589b189328479336ebd96e.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,7 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:cypress/recommended"
+    "plugin:react/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
