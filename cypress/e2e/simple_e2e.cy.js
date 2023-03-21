7acec0e997e74c81ceec74be0c72e5aab9fb00e8
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-03-21 19:45:19 +0700

    add another e2e test

--- cypress/e2e/simple_e2e.cy.js
+++ cypress/e2e/simple_e2e.cy.js
@@ -5,4 +5,13 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('squirtle').click()
+
+    cy.get('.pokemon-info').children().should('have.length', 3)
+    cy.get('.pokemon-name').should('include.text', 'squirtle')
+    cy.get('[data-testid="stats"]').should('be.visible')
+    cy.get('.pokemon-abilities').should('be.visible')
+  })
 })
