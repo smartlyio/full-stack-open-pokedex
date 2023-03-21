967e5a10b4d97ab53a763ef843620f9be13a1eaf
teofou <teofou13@gmail.com>
2023-03-21 21:19:37 +0200

    20

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    'node': true
+    'node': true,
+    ELECTRON_EXTRA_LAUNCH_ARGS: '--disable-gpu'
   },
   "extends": [
     "eslint:recommended",
