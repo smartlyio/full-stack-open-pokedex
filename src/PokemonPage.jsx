40ffb860033af5aa570937f7fcce3e44509dd3e7
Theodoros Fountzoulas <teofou13@gmail.com>
2023-03-21 10:37:08 +0200

    Update PokemonPage.jsx

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -26,6 +26,7 @@ const PokemonPage = ({ previous, next }) => {
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
+  // eslint-disable-next-line no-use-before-define
   console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
