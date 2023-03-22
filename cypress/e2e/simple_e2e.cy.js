b022b35f3da810771bbe8de4185133187aaae903
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-03-22 17:25:08 +0700

    change e2e test step to wait on port 8080 instead of 5000
    fix typo
    
    fix tpo in the flyctl deploy comamnd

--- cypress/e2e/simple_e2e.cy.js
+++ cypress/e2e/simple_e2e.cy.js
@@ -1,12 +1,12 @@
 
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('pokemon page can be navigated to', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('squirtle').click()
 
     cy.get('.pokemon-info').children().should('have.length', 3)
