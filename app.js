503df82d662722d9a8a24e4f8a3ea8669d6cc84b
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-03-22 19:44:29 +0700

    add health_check endpoint to backend & http_checks section to fly config

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (_req, res) => {
+  res.statusCode('200').end()
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 8080')
