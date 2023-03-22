7b8e61dba70749ce41b4832b89f40b0aeb1eecbd
Marmik Bhatt <marmikbhatt@Marmiks-MacBook-Pro.local>
2023-03-22 19:17:22 -0400

    Update pipeline.yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("cypress");
+/*const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
@@ -7,3 +7,4 @@ module.exports = defineConfig({
     },
   },
 });
+*/
