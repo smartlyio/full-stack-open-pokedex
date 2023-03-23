ce473703c5f6b721e03f8d0794f768b7f9084221
Marmik Bhatt <marmikbhatt@Marmiks-MacBook-Pro.local>
2023-03-23 11:11:44 -0400

    Update pipeline.yml

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -12,4 +12,11 @@ describe("Pokedex", function () {
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
+
+  it("pokemon page can be navigated to", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur").click();
+    cy.contains("chlorophyll");
+  });
+  
 });
