4fc241153e062c8347d3c35b4e56deb3844cf82e
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-03-24 10:07:25 +0200

    health check test

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express')
+
 const app = express()
+app.use(express.json())
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log('Health is OK!')
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log('version 1')
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+// This middleware must be last after the other endpoints
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
