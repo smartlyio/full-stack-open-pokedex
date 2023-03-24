47a809e14f457bc33bea656459675fcec960be5a
Kaarle Järvinen <kaarle46@gmail.com>
2023-03-24 18:47:24 +0200

    Added a new e2e test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('A card can be inspected', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
