bc15d58e103150a354a4a0d829fbb2b91718d697
bgu152 <erikrammschmidt@gmail.com>
2023-03-29 16:17:54 +0300

    11.11.10

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 app.get('/version', (req, res) => {
---
+++ health_check.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello from shell script"
+
+exit 0 # exit status 1 means that the script "fails"
