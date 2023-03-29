ad411786e5a5d3dda449e378010fbbdd4cd7b6a4
bgu152 <erikrammschmidt@gmail.com>
2023-03-29 15:43:53 +0300

    11.10.8

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 app.get('/version', (req, res) => {
