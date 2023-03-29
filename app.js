c3024373a1d66101c7186620831ec622fbd76900
bgu152 <erikrammschmidt@gmail.com>
2023-03-29 15:36:39 +0300

    11.10.7

--- app.js
+++ app.js
@@ -13,3 +13,6 @@ app.listen(PORT, () => {
 app.get('/health', (req, res) => {
   res.send('ok')
 })
+app.get('/version', (req, res) => {
+  res.send('7') // change this string to ensure a new version deployed
+})
