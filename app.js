75bc03e40559f0fc6370e29f6f16a61503dba3e1
Juha Karhusaari <juha.karhusaari@knowit.fi>
2023-03-30 13:36:44 +0300

    add /version endpoint

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/health', (req, res) => {
-  throw Error('jee jee')
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
-// ! JATKA  => 11.11 Health check and rollback
+app.get('/version', (req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
+})
 app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log(`server started on port ${PORT}`)
