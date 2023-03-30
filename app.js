a7b15e4951fd204faf66e982ae627eec543c4856
Juha Karhusaari <juha.karhusaari@knowit.fi>
2023-03-30 13:02:09 +0300

    test healthcheck with causing error

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/health', (req, res) => {
+  throw Error('jee jee')
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 // ! JATKA  => 11.11 Health check and rollback
