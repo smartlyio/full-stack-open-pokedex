edb87721904a9366c644803359ecc7eb54709656
bgu152 <erikrammschmidt@gmail.com>
2023-03-30 13:03:35 +0300

    11.12 Please work now

--- health_check.sh
+++ health_check.sh
@@ -2,4 +2,11 @@
 
 echo "Hello from shell script"
 
-exit 0 # exit status 1 means that the script "fails"
+http_code=$(curl -LI https://fullstack-pokestuff.fly.dev/health -o /dev/null -w '%{http_code}\n' -s)
+if [ ${http_code} -eq 200 ]; then
+    echo "bash health check ok" 
+    exit 0
+else
+    echo "bash health check NOT ok" 
+    echo 1
+fi
