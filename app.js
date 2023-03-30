605a8a497e2c517cc7db203003ed6cefd338a880
Juha Karhusaari <juha.karhusaari@knowit.fi>
2023-03-30 12:40:30 +0300

    add custom healthcheck

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+// ! JATKA  => 11.11 Health check and rollback
 app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log(`server started on port ${PORT}`)
