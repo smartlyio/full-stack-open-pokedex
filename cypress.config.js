7e4b28b4d34d327862e8c7a38a99c059c71c18cd
Michael Zhang <michael.zhang818@gmail.com>
2023-03-31 15:26:07 +0700

    fix cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:5000',
+    video: false,
   },
 })
--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('front page can be opened', () => {
-    cy.visit('http://localhost:5000')
+    cy.visit('/', { timeout: 5000 }) //use baseUrl in cypress.config.js
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
