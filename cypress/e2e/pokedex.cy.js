f2a032dd6208cccd15bc734fe860513fa0922690
Michael Zhang <michael.zhang818@gmail.com>
2023-03-31 16:52:42 +0700

    add cypress step

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,9 +1,15 @@
-describe('template spec', () => {
+describe('pokedex', () => {
+  beforeEach(() => cy.visit('/', { timeout: 5000 }))
+
   it('front page can be opened', () => {
-    cy.visit('/', { timeout: 5000 }) //use baseUrl in cypress.config.js
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+
+  it('pokemon page can be navigated to', () => {
+    cy.get('a[href*="ivysaur"]').click()
+    cy.contains('chlorophyll')
   })
 })
