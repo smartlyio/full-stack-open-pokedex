e77760ca0a2ed33682a412e92d1e80e031590ae9
Kimmo Muli <kimmomuli@gmail.com>
2023-03-31 16:25:18 +0300

    11.9 ready

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,18 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  describe('Pokedex', () => {
+    it('front page can be opened', () => {
+      cy.visit('http://localhost:5000')
+      cy.contains('ivysaur')
+      cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    })
+  
+    it('navigate to ivysaur page',() => {
+      cy.visit('http://localhost:5000')
+      cy.contains('ivysaur').click()
+      cy.contains('chlorophyll')
+    })
+  })
+})
