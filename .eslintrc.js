4e1bfeb792b3c963f7c231c13fb7a5d045900e2e
Michael Zhang <michael.zhang818@gmail.com>
2023-03-31 11:14:54 +0700

    fix test

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: ['react', 'jest'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['never', 'unix'],
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
