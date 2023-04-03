b2f41d0e980c9c761a841bba178ac2b9181bc402
bgu152 <erikrammschmidt@gmail.com>
2023-04-03 20:19:06 +0300

    testing notifications

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
 app.get('/health', (req, res) => {
--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -1,17 +1,15 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
-  it('pokemon page can be navigated to', function() {
+  it('pokemon page can be navigated to', function () {
     cy.visit('http://localhost:5000')
     cy.get('a.list-item')
-    .contains('caterpie')
-    .click();
-
-    cy.wait(2000);
+      .contains('caterpie')
+      .click()
     cy.contains('caterpie')
     cy.contains('speed')
     cy.contains(45)
