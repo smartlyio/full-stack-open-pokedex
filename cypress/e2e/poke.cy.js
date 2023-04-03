a2f2d6b3766809d99aba2f23f2efa994cc077383
bgu152 <erikrammschmidt@gmail.com>
2023-04-03 20:00:42 +0300

    testing error message to discord

--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -4,4 +4,18 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('a.list-item')
+    .contains('caterpie')
+    .click();
+
+    cy.wait(2000);
+    cy.contains('caterpie')
+    cy.contains('speed')
+    cy.contains(45)
+    cy.contains('Hidden ability')
+    cy.contains('shield dust')
+  })
 })
