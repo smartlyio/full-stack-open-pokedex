c8b13253d3be2ec968fde72625fb04b4ed717d8a
Youngrok Kim <yngrk.kim@gmail.com>
2023-04-03 16:40:33 +0200

    fixed linting errors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 3000");
-});
+  // eslint-disable-next-line no-console
+  console.log('server started on port 3000')
+})
