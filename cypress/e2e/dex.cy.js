c78bb7896c14a4c2c99137ca4ab6e5b97bf09cdb
Youngrok Kim <yngrk.kim@gmail.com>
2023-04-04 12:52:13 +0200

    setup fly deployment to workflow

--- cypress/e2e/dex.cy.js
+++ cypress/e2e/dex.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
@@ -8,7 +8,7 @@ describe('Pokedex', function () {
   })
 
   it('can navigate to pokemon specific page', function () {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('charmander').click()
     cy.url().should('include', '/charmander')
     cy.contains('solar power')
