88890d572773adacdad6b27713532bf65f8f54cc
Youngrok Kim <yngrk.kim@gmail.com>
2023-04-04 14:08:06 +0200

    fix error

--- app.js
+++ app.js
@@ -12,11 +12,9 @@ app.listen(PORT, () => {
 })
 
 app.get('/health', (req, res) => {
-  throw 'error'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
