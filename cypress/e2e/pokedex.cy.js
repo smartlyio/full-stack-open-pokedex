a925defe6cd39cd32cd047e71b841dd55101ac90
simon greenwood <simonrobertgreenwood@gmail.com>
2023-04-06 17:38:43 +0100

    done with the new test, 11.9 done

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  it('front page can be opened', () => {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+  it('pokemon page can be navigated to', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
