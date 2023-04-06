7343c176a07edde06ede8965e3c679e4099c255b
Youngrok Kim <yngrk.kim@gmail.com>
2023-04-06 12:12:56 +0200

    change version number

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
