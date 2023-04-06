dcb7520e7688010199c02120ca6aa2af53c62a2f
simon greenwood <simonrobertgreenwood@gmail.com>
2023-04-06 16:16:51 +0100

    fixed the eslint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import { useApi } from "./useApi"
-import LoadingSpinner from "./LoadingSpinner"
-import ErrorMessage from "./ErrorMessage"
-import PokemonPage from "./PokemonPage"
-import PokemonList from "./PokemonList"
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { useApi } from './useApi'
+import LoadingSpinner from './LoadingSpinner'
+import ErrorMessage from './ErrorMessage'
+import PokemonPage from './PokemonPage'
+import PokemonList from './PokemonList'
 
 const mapResults = ({ results }) =>
   results.map(({ url, name }) => ({
@@ -18,7 +18,7 @@ const App = () => {
     data: pokemonList,
     error,
     isLoading,
-  } = useApi("https://pokeapi.co/api/v2/pokemon/?limit=50", mapResults)
+  } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
   if (isLoading) {
     return <LoadingSpinner />
   }
