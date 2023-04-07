4b2ae94fb9bf83e326a74abb51624ee28765b340
ZeroMinus13 <zerominus13@gmail.com>
2023-04-07 11:50:55 +0500

    adding eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    quotes: [],
+    quotes: 'off',
     semi: ['error', 'never'],
     eqeqeq: 'error',
     'no-trailing-spaces': 'error',
