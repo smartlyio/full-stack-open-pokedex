f9936d220da45f2acc1431a2a5b14b14a68c9dda
ZeroMinus13 <zerominus13@gmail.com>
2023-04-07 11:33:17 +0500

    testing git actions

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
-const mapResults = (({ results }) => results.map(({ url, name }) => ({
-  url,
-  name,
-  id: parseInt(url.match(/\/(\d+)\//)[1])
-})))
+const mapResults = ({ results }) =>
+  results.map(({ url, name }) => ({
+    url,
+    name,
+    id: parseInt(url.match(/\/(\d+)\//)[1]),
+  }))
 
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
@@ -24,15 +25,18 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <PokemonList pokemonList={pokemonList} />
         </Route>
-        <Route path="/pokemon/:name" render={(routeParams) => {
-          const pokemonId = pokemonList.find(({ name }) => name === routeParams.match.params.name).id
-          const previous = pokemonList.find(({ id }) => id === pokemonId - 1)
-          const next = pokemonList.find(({ id }) => id === pokemonId + 1)
-          return <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
-        }} />
+        <Route
+          path='/pokemon/:name'
+          render={(routeParams) => {
+            const pokemonId = pokemonList.find(({ name }) => name === routeParams.match.params.name).id
+            const previous = pokemonList.find(({ id }) => id === pokemonId - 1)
+            const next = pokemonList.find(({ id }) => id === pokemonId + 1)
+            return <PokemonPage pokemonList={pokemonList} previous={previous} next={next} />
+          }}
+        />
       </Switch>
     </Router>
   )
