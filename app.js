a3fa44a26c03d8eed05c65f89e374bf436bb2e16
simon greenwood <simonrobertgreenwood@gmail.com>
2023-04-07 09:26:40 +0100

    done with 11.11 and adding the health check to the express app

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
 app.get('/version', (req, res) => {
   res.send('2') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
