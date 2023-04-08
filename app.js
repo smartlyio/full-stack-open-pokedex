58973aa79b0c72d32b9abd0322b430f27acec5d7
abdoemr11 <abdoemr11@gmail.com>
2023-04-08 14:44:29 +0200

    added health check point updated the pipeline

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
