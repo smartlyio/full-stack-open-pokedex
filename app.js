3c05e3769905541c7ce22064f6922263a372c158
timothyjapolinario <timothyjapolinario@gmail.com>
2023-04-12 16:33:45 +0800

    Update render token variable

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 app.get("/version", (req, res) => {
-  res.send("10");
+  res.send("11");
 });
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
