51a3eb168182f9db3670146d248713466eefcfd8
timothyjapolinario <timothyjapolinario@gmail.com>
2023-04-12 14:42:55 +0800

    Add deploy to render step on pipeline

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
-
+app.get("/version", (req, res) => {
+  res.send("1");
+});
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
