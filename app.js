2b05a5df6a2b14c8ce0ea58272c45b21e4e98930
dynamo <hietanenelias@gmail.com>
2023-04-13 17:09:25 +0200

    exercise 11.11 - pushing the working version

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
