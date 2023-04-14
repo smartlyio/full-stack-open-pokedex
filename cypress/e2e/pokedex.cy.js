69b0f27e18884362ad521c3a46775416dc07317a
Ahmed Kasu <ankassu@gmail.com>
2023-04-14 09:57:24 +0300

    Exercise 11.9, added main page to a paticular page navigation test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,9 +1,16 @@
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  }),
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  }),
+
+  it('pockemon page can be navigated to', function () {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
