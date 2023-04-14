f7e131df36c06988c8ceafc7584659452efeb1aa
Susanne Mäkelä <susamakela@Susanne-MacBook-Pro.local>
2023-04-14 12:43:01 +0300

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // eslint-disable-next-line
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
