16c1f319dc2f7c8b38dcc6d101970d1b8d39e104
Ahmed Kasu <ankassu@gmail.com>
2023-04-14 17:52:39 +0300

    Health check and rollback fail check

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
+
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
