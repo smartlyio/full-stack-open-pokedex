87bb41cf583707e810e9eea6491e150b99068daa
susamac <57934899+susamac@users.noreply.github.com>
2023-04-14 10:47:14 +0300

    Update PokemonPage.jsx

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -25,7 +25,6 @@ const PokemonPage = ({ previous, next }) => {
   })).reverse()
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
-  
   // eslint-disable-next-line
   console.log('hiddenAbility=', hiddenAbility)
   return (
