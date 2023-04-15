2330bbd64d8689b044944278fd8688729b1e7070
Susanne Mäkelä <susamakela@Susanne-MacBook-Pro.local>
2023-04-15 12:14:50 +0300

    teht 11.12

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
@@ -9,4 +8,8 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log('server started on port 5000')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
