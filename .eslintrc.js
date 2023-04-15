aec8aacc6991ca68b3c0aa203dd305147a1eba4c
Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>
2023-04-15 17:13:41 -0400

    change eslint
    Signed-off-by: Quang.Ng.2003 <78969220+quangnguyen9723@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   "env": {
     "browser": true,
+    "node": true,
     "es6": true,
     "jest/globals": true
   },
@@ -27,14 +28,14 @@ module.exports = {
       "error",
       "unix"
     ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
+    // "quotes": [
+    //   "error",
+    //   "single"
+    // ],
+    // "semi": [
+    //   "error",
+    //   "never"
+    // ],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": [
@@ -43,7 +44,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    // "no-console": "error",
     "react/prop-types": 0
   }
 }
