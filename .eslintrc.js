ec4f3fc3be9c7a6cebef4c5ea74e983fe7b3bcce
Ahmed Abdulrahman <ahmed751995@gmail.com>
2023-04-20 14:39:07 +0200

    setup cypress test

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     'jest/globals': true,
     commonjs: true,
     'node': true,
+    "cypress/globals": true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
@@ -14,7 +15,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'jest'],
+  plugins: ['react', 'jest', 'cypress'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
