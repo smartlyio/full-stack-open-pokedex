a53dc48ddd7b5228d13ec4a59fc39a509bd1192b
Lauri Säteri <laukka22@gmail.com>
2023-04-21 14:26:54 +0300

    task 11.9

---
+++ cypress/e2e/pokemon.cy.js
@@ -0,0 +1,15 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+
+  it('PokemonPage shows correspongin info - ivysaur', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
+})
