6055063d036722941a481449ea6a5b4c59c7889b
Ahmed Abdulrahman <ahmed751995@gmail.com>
2023-04-26 17:31:56 +0200

    fix linting

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || "8080"
+const PORT = process.env.PORT || '8080'
 
 app.use(express.static('dist'))
 
