64efc1f55b2b48d51648e62a40cfedffdf2fb6d0
Lauri Säteri <laukka22@gmail.com>
2023-04-27 12:17:45 +0300

    task 11.12

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const app = express()
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
+app.get('/health', (req,res) => {
+  res.send('ok')
+})
+
+
+
 app.use(express.static('dist'))
+
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
