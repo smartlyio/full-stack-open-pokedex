6c0446f57679f73a4f25c61e2d332d83c94814d0
ahlsvedc <77110652+ahlsvedc@users.noreply.github.com>
2023-04-28 16:17:54 +0300

    fly.io deployment added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express()
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
-// const PORT = process.env.PORT || 5000
-const PORT = 5000
+const PORT = process.env.PORT || 5000
+// const PORT = 5000
 
 app.use(express.static('dist'))
 
