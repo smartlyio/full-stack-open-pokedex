02d4c0071a137b95ceba824b9fa13d73792d84b0
ahlsvedc <77110652+ahlsvedc@users.noreply.github.com>
2023-04-28 18:53:51 +0300

    better helath check

--- health_check.sh
+++ health_check.sh
@@ -2,4 +2,12 @@
 
 echo "Hello from shell script"
 
-exit 0 # exit status 1 means that the script "fails"
+response=$(curl -s https://pokedex-fullstack.fly.dev/health)
+if [ "$response" = "ok" ]; then
+    echo "Pokedex is up and running"
+    exit 0
+else
+    echo "Pokedex is down"
+    exit 1
+fi
+# exit 0 # exit status 1 means that the script "fails"
