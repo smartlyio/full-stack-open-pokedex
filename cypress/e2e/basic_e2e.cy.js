314ae952a98c91c21d648db7386aa99972d905e2
ahlsvedc <77110652+ahlsvedc@users.noreply.github.com>
2023-04-28 12:26:11 +0300

    e2e test navigate to page

--- cypress/e2e/basic_e2e.cy.js
+++ cypress/e2e/basic_e2e.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate to ivysaur page', function() {
+    cy.visit('http://localhost:5000');
+    cy.contains('ivysaur').click();
+    cy.contains('chlorophyll');
+  });
 })
