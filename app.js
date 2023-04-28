4ceadcd57349fc54b582c1a546f250705991ba8f
ahlsvedc <77110652+ahlsvedc@users.noreply.github.com>
2023-04-28 16:39:21 +0300

    health check test 1

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 
+
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 // const PORT = 5000
 
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
