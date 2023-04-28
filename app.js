7278893e9c52ef15f309fba837b693a359939e58
ahlsvedc <77110652+ahlsvedc@users.noreply.github.com>
2023-04-28 16:51:43 +0300

    revert failing health check

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
+  // throw 'error...'
+  // // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
