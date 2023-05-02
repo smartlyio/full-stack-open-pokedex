3b10220c5bd2604e2769a147cc03b87f2506efa0
ahlsvedc <77110652+ahlsvedc@users.noreply.github.com>
2023-05-02 13:52:59 +0300

    11.13 Pull request

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const PokemonList = ({ pokemonList }) => {
   return (
     <div className="list-container">
-      PULL REQUEST TEXT 0
+      PULL REQUEST TEXT 1
       {pokemonList.map(({ id, name }) => (
         <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
           <div
