db1ee0b8b0e20a38ea9f606e9c0f5aee6bab371b
Jenniina <jenniina@jenniina.fi>
2023-05-05 18:30:06 +0300

    edit port

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,11 +1,11 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('can navigate to pokemon page', function () {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.get('a[href="/pokemon/ivysaur"]').click()
     cy.contains('ivysaur')
     cy.contains('chlorophyll')
