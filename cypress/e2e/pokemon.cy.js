c68feca947b4ac832e7f0fa2450991a59242047b
Jenniina <jenniina@jenniina.fi>
2023-05-05 18:07:48 +0300

    edit cypress

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,7 +1,13 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:8081')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('can navigate to pokemon page', function () {
+    cy.visit('http://localhost:8080')
+    cy.get('a[href="/pokemon/ivysaur"]').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
   })
 })
