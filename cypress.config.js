7740ba7f110939ec0d18aaa34e34aa888bf047ed
Jenniina <jenniina@jenniina.fi>
2023-05-05 18:23:15 +0300

    add // eslint-disable-next-line no-unused-vars

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
