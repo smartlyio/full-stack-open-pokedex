aa755e6787f41294b2d78e3b8980ab91af90e23c
Jenniina <jenniina@jenniina.fi>
2023-05-06 13:50:06 +0300

    add health path

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// eslint-disable-next-line no-unused-vars
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
