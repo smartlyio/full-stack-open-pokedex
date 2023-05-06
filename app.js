7b9002cc0ec7e150c5a42dc6f2713ea8de886b61
Jenniina <jenniina@jenniina.fi>
2023-05-06 14:12:01 +0300

    11.12 Health check finished

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ app.use(express.static('dist'))
 
 // eslint-disable-next-line no-unused-vars
 app.get('/health', (_req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
+
+// // // eslint-disable-next-line no-unused-vars
+// // app.get('/health', (_req, res) => {
+// //   throw 'error...'
+// //   // eslint-disable-next-line no-unreachable
+// //   res.send('ok')
+// // })
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
