a7d704e28fa0e39f6f8b07d2dce1b280936d16d4
lorgik <sib.lorgik@gmail.com>
2023-05-07 13:22:37 +0300

    fix

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -1,7 +1,13 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
