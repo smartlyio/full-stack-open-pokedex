054c27fa77479fd122bf1d04e5d9f7a39207cace
PG <psyfry@protonmail.com>
2023-05-08 10:28:52 -0700

    linted and console removed

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const process = require('process')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3003
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
-});
+  // console.log(`server started on port ${PORT}`);
+})
