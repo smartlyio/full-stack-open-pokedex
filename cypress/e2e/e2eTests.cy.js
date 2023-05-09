2aea8d6e8583879f8699e6f49036b01e4fbb7aff
dwzm0 <dwzm00gmail.com>
2023-05-09 22:22:26 +0300

    e2e second test

--- cypress/e2e/e2eTests.cy.js
+++ cypress/e2e/e2eTests.cy.js
@@ -5,4 +5,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href="/pokemon/butterfree"]').click()
+    cy.contains('butterfree')
+  })
 })
