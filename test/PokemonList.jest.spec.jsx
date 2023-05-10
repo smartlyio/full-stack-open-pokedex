63dd60d41225cad980f1d0b68cc2d76521cfef51
lorgik <sib.lorgik@gmail.com>
2023-05-10 12:45:30 +0300

    fix

--- test/PokemonList.jest.spec.jsx
+++ test/PokemonList.jest.spec.jsx
@@ -22,7 +22,7 @@ describe('<PokemonList />', () => {
         <PokemonList pokemonList={pokemonList} />
       </BrowserRouter>
     )
-    expect(screen.getByText('bulsaur')).toBeVisible()
+    expect(screen.getByText('bulbasaur')).toBeVisible()
     expect(screen.getByText('eevee')).toBeVisible()
   })
 })
