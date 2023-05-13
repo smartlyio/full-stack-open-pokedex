8588b4350193112090b159dd90bd45dba84bfd2b
Risto Leivo <ristoml@metropolia.fi>
2023-05-13 20:04:19 +0300

    add error v3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
