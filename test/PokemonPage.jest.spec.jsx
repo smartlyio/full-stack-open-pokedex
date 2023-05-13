af2e202b6641ec3787dff29717e71c1be28129df
cpp233 <962582834@qq.com>
2023-05-13 19:51:43 +0800

    homework 17.7-2

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -122,8 +122,10 @@ describe('<PokemonPage />', () => {
       'href',
       '/pokemon/ditto'
     );
-    // expect(screen.getByText('Next')).toHaveAttribute('href', '/pokemon/vaporeon')
-    expect(screen.getByText('Next')).toHaveAttribute('href', '/pokemon/ditto');
+    expect(screen.getByText('Next')).toHaveAttribute(
+      'href',
+      '/pokemon/vaporeon'
+    );
   });
 
   it('should not render previous and next urls if none exist', async () => {
