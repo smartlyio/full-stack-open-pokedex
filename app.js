1c544d97103de02bbedcb0b19282ca4b179b48dd
Risto Leivo <ristoml@metropolia.fi>
2023-05-13 19:25:47 +0300

    render deployment

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log('server started on port 5000')
 })
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
---
+++ build_step.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Build script"
+
+# add the commands here
+
+npm install
+webpack --mode production
