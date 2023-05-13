0a3d6e3f15edb371957577012e9508913d600b33
Risto Leivo <ristoml@metropolia.fi>
2023-05-13 20:50:45 +0300

    fix another  error

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.get('/version', (req, res) => {
   res.send('4') // change this string to ensure a new version deployed
 })
 
-app.get('/health', (req, res) => { 
+app.get('/health', (req, res) => {
   res.send('ok')
 })
