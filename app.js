b8d6da4ec984d1624a5cee7e0cdf832d96914118
PG <psyfry@protonmail.com>
2023-05-16 10:59:07 -0700

    Revert  Experimental broken deployment back to working state

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
+// Render dynamically sets a port
 const PORT = process.env.PORT || 3003
 
 app.use(express.static('dist'))
 
 app.get('/healthy', (req, res) => {
-  throw Error('Simulated breakdown in deployment pipeline at healthy ping')
-  // eslint-disable-next-line no-unreachable
   res.send('healthy')
 })
 app.get('/version', (req, res) => {
