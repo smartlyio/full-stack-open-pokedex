935f38b39c17834c74f2bad4517d9e9f52330950
PG <psyfry@protonmail.com>
2023-05-16 09:38:35 -0700

    bug fix deploy pipeline

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const PORT = process.env.PORT || 3003
 
 app.use(express.static('dist'))
 
+app.get('/healthy', (req, res) => {
+  res.send('healthy')
+})
+app.get('/version', (req, res) => {
+  res.send('1.0.2')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   // console.log(`server started on port ${PORT}`)
