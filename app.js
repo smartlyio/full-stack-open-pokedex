52ac226ca68114d27e7c05882c5c34817c16853b
evusr1 <127464885+evusr1@users.noreply.github.com>
2023-05-23 23:33:04 -0700

    fix health check!

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
