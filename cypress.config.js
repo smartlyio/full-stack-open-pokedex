b5470e007dd51f6e1f94eae50bc73f140b8521ce
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-05-30 01:04:21 +0300

    fix cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // eslint-disable-next-line
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:5000',
   },
 })
