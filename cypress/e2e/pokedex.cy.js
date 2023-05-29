06ad267cfe7f9c06555a9dbd2f31a6c4ff952aea
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-05-30 02:40:37 +0300

    add another e2e test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('navigation to pokemon page succees', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
