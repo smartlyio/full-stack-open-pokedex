ad58241a416052f06cbc5b47e05cb0574c200576
Doubleneck <antti.vuorenmaa@kolumbus.fi>
2023-05-30 01:24:26 +0300

    fix port 5000

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
-
+//const PORT = process.env.PORT || 5000
+const PORT = 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000',
+    // eslint-disable-next-line
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 })
