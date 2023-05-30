7524d48ac2cf274f0bf9a12c2c8886ecc5aa704f
Jorma Kemppainen <j.kemppainen@kolumbus.fi>
2023-05-30 13:48:04 +0300

    test ok heath check

--- app.js
+++ app.js
@@ -7,15 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
+  // throw 'error...'
+
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
-/*
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
-*/
 
 app.get('/version', (req, res) => {
   res.send('4') // change this string to ensure a new version deployed
