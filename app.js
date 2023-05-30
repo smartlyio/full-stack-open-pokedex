4f9abb9bff274f3d27a624d08b5a4cbc1ff0727f
Jorma Kemppainen <j.kemppainen@kolumbus.fi>
2023-05-30 18:22:50 +0300

    finish feature 1

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('6') // change this string to ensure a new version deployed
+  res.send('7') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
