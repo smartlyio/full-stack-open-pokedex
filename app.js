f7c713555a2ff07b366089e51e6853272411af6a
Jorma Kemppainen <j.kemppainen@kolumbus.fi>
2023-05-30 13:39:44 +0300

    testing health check error

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
+/*
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+*/
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
