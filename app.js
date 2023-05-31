9b8ee83d5612569083cce6f14fd475cd15b47023
Jorma Kemppainen <j.kemppainen@kolumbus.fi>
2023-05-31 18:03:35 +0300

    Exercises 11.1.-11.17 done #major

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('19') // change this string to ensure a new version deployed
+  res.send('20') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
