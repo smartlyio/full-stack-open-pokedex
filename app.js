9b3c48ec2692bfd499aa5e40b4a0806f3e878094
Enrique Verea <kikeverea@gmail.com>
2023-06-02 10:19:35 +0200

    Exercise 11.11: health check and rollback
    Fly is not rolling back on health check fail. Custom script added

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', () => {
-  // res.send('ok')
-  throw 'error...'
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployedgit re
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
