1ced001e808ddec9b06b5cb7d76597f42caac33d
Emma Laitinen <emlaitine@gmail.com>
2023-06-03 20:50:30 +0300

    update configs

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
-  // res.send('ok')
+  res.send('ok')
 })
 
 app.get('/version', (req, res) => {
