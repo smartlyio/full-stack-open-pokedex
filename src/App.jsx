920fea50e15f221020b8e7c0e44a8d5b2d915f68
sahandsn <sanaeisahand@gmail.com>
2023-06-03 15:30:34 +0330

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
+// testing
 const mapResults = ({ results }) =>
   results.map(({ url, name }) => ({
     url,
