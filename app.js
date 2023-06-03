8917022f49e98e9b01047bf684f39a58211f8a17
sahandsn <sanaeisahand@gmail.com>
2023-06-04 01:15:33 +0330

    11.12 completed

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', () => {
-  throw 'error...'
+app.get('/health', (_req, res) => {
+  res.status(200).send('ok')
 })
 
 app.listen(PORT, () => {
