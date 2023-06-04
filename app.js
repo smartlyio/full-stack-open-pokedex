d2f2a7d40a59501c73b36a6e50f105ff49df3541
LRNZH <fullmentalfloss@gmail.com>
2023-06-04 19:21:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('2') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
