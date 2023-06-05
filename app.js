237726e426d1c4e1352c62587f50257a25744765
LRNZH <fullmentalfloss@gmail.com>
2023-06-05 08:25:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use('/',express.static('/dist'))
+app.use('/',express.static('dist'))
 
 app.get('/version', (req, res) => {
   res.send('2') // change this string to ensure a new version deployed
