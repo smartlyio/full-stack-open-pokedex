fe457cc3a532cde0e76cdfb5f8a1ab21f905980a
maeeri <marianne.eerikainen@gmail.com>
2023-06-07 15:53:53 +0300

    11.9.4

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
   },
-});
+})
