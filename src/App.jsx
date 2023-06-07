5b2a72fc8a185e437ccadb6a3c8e213ef718b50c
sahand-sn <133222495+sahand-sn@users.noreply.github.com>
2023-06-07 16:20:25 +0330

    11.14

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
 
+// testing pipeline trigger
 const mapResults = ({ results }) =>
   results.map(({ url, name }) => ({
     url,
