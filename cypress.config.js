bae6304e7fb8dfafc7d24ee3fdb33fc78685a977
maeeri <marianne.eerikainen@gmail.com>
2023-06-07 16:26:14 +0300

    11.9.8

--- cypress.config.js
+++ cypress.config.js
@@ -6,14 +6,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, _config) {
       // implement node event listeners here
       on('before:browser:launch', (browser = {}, launchOptions) => {
-        console.log(launchOptions.args)
-    
-        if (browser.name == 'chrome') {
+        if (browser.name === 'chrome') {
           launchOptions.args.push('--disable-gpu')
         }
-    
         return launchOptions
-      }),
-    },
+      })
+    }
   },
 })
