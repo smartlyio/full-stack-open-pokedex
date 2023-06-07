6abe565c8c2b5a6bc331a41a4bea48a1857db7a9
maeeri <marianne.eerikainen@gmail.com>
2023-06-07 16:23:54 +0300

    11.9.7

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,17 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // eslint-disable-next-line no-unused-vars
-    setupNodeEvents(_on, _config) {
+    setupNodeEvents(on, _config) {
       // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        console.log(launchOptions.args)
+    
+        if (browser.name == 'chrome') {
+          launchOptions.args.push('--disable-gpu')
+        }
+    
+        return launchOptions
+      }),
     },
   },
 })
