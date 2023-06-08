88cc3c89349237d454825ac1b6ae5f45ac700b14
maeeri <marianne.eerikainen@gmail.com>
2023-06-08 12:05:10 +0300

    11.11

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/version', (_req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (_req, res) => {
+  // throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
