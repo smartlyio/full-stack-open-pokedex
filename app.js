d4f519cc2f413a86c01921d8d3a24ee8a1ff57cb
maeeri <marianne.eerikainen@gmail.com>
2023-06-08 13:33:10 +0300

    11.14.branch(2)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('5') // change this string to ensure a new version deployed
+  res.send('6') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (_req, res) => {
