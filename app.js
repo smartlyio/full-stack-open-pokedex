a2001dbfc4e277e6322fb436aef5e544c0465c58
maeeri <marianne.eerikainen@gmail.com>
2023-06-08 12:23:58 +0300

    11.11.4

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (_req, res) => {
-  throw 'error...'
+  // throw 'error...'
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
