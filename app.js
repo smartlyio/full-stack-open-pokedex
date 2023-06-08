8ca97816392a63617d7271b90160453a29e018e0
maeeri <marianne.eerikainen@gmail.com>
2023-06-08 12:57:16 +0300

    11.11.7

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+// eslint-disable-next-line no-console
+console.log(process.env.PORT)
+
 app.use(express.static('dist'))
 
 app.get('/version', (_req, res) => {
-  res.send('4') // change this string to ensure a new version deployed
+  res.send('5') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (_req, res) => {
