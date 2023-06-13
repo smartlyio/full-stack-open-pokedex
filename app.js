f7e5375fe08875b4d3012d8e8072ac1e0ee45f6d
Tommi Koivula <tommi.koivula93@gmail.com>
2023-06-13 15:13:24 +0300

    break health check

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw 'error'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
@@ -16,5 +18,5 @@ app.get('/version', (req, res) => {
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log('server started on port ', PORT)
 })
