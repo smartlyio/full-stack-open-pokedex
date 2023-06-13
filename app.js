e669a66b4efaf7416f03350f1be404351c7863f3
Tommi Koivula <tommi.koivula93@gmail.com>
2023-06-13 15:18:20 +0300

    revert to working app

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('v11')
+  res.send('working v11')
 })
 
 app.listen(PORT, () => {
