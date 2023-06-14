b1fc8b8c54769b2a8453a9f8567e03151574de5b
Enissimu <120343318+Enissimu@users.noreply.github.com>
2023-06-14 18:32:59 +0300

    fixed issues .d

--- app.js
+++ app.js
@@ -8,14 +8,8 @@ app.use(express.static('dist'))
 
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
-
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// })
 
 app.get('/version', (req, res) => {
   res.send('2') // change this string to ensure a new version deployed
