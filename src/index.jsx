6213e16eb4dcca8db39ac85c024015b3276d49e6
Ahmed Kasu <ankassu@gmail.com>
2023-06-14 12:50:55 +0300

    exercise11.6 fix the code

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
-
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById('app'))
