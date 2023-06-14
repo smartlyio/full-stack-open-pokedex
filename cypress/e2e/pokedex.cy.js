f7cd43b20ce5a455fca3d272dc5a064bf00839ef
Enissimu <120343318+Enissimu@users.noreply.github.com>
2023-06-14 14:58:29 +0300

    port fixed 11.9

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function() {
 
-  beforeEach(function() {    cy.visit('http://localhost:8080')  })
+  beforeEach(function() {    cy.visit('http://localhost:5000')  })
   it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
