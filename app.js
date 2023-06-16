962b4a9a4bede0c68dffb1cb3579d6eeda106878
Ayatullah Khameni <ayat2486@gmail.com>
2023-06-16 19:31:50 +0600

    open dist build file

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
+
+
+app.get('/version', function(req, res){
+  console.log('req_body', req.body)
+  res.send(1)
+})
+
+app.get('/health', function(req, res){
+  console.log('req', req.body)
+  res.send('Ok!')
+})
