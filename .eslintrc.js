ae64c3c073d281df0f2c182622fd55d8d40e3a1c
Tuuli Toivanen-Gripentrog <tuuli.k.toivanen@gmail.com>
2023-06-18 20:52:06 +0300

    Ex. 11.6 Fix linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   "env": {
+    "node": true,
     "browser": true,
     "es6": true,
     "jest/globals": true
@@ -43,7 +44,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
