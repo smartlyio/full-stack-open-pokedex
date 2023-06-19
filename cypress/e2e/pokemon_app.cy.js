7f291d2254b1222666ba40c94bf3f5021aefd8e3
Tuuli Toivanen-Gripentrog <tuuli.k.toivanen@gmail.com>
2023-06-19 23:01:05 +0300

    Ex. 11.9 Add another e2e test

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('Pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href*="charmander"]').click()
+    cy.contains('solar power')
+    cy.contains('blaze')
+  })
 })
