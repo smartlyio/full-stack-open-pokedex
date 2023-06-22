ee2871eb11eb9f801a62b4a3cab794c11d7651ae
JoseA <joseantanaper@gmail.com>
2023-06-22 19:31:24 +0200

    11.9

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // implement node event listeners here
-    },
+    }
   },
-});
+})
