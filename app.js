45ba0a4fcb0d95124488cc7fd32bf23ec7b1e713
JoseA <joseantanaper@gmail.com>
2023-06-22 20:20:47 +0200

    11.9

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port ' + PORT)
+  // console.log('server started on port ' + PORT)
 })
