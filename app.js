226372d7357fbfe93668845a0b8d2d014a9e5eab
JoseA <joseantanaper@gmail.com>
2023-06-23 17:16:08 +0200

    11.10

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/', (req, res) => {
+  res.send('Welcome')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server started on port ${PORT}`)
