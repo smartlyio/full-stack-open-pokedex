41a8cdbc3205701dadb432a34b1e4b9bd07d593d
kimyu <kimyu@hanasiu.local>
2023-06-27 14:46:40 +0900

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   "env": {
-    "node": true,
+    "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
