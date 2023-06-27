3f75a939339666fc3fdd6c6f07bc2d9dbefc45ca
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2023-06-27 20:12:12 +0300

    Push first workflow

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
-});
+  // eslint-disable-next-line
+  console.log(`server started on port ${PORT}`)
+})
