62b2fe4787fdc704f59b138cfd61538ebed59c28
kimyu <kimyu@hanasiu.local>
2023-06-27 17:02:10 +0900

    webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        rules: ['style-loader', 'css-loader'],
+        loader: ['style-loader', 'css-loader'],
       }
     ],
   },
