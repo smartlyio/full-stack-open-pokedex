3c7e83b45a8c240da879aa080e5e6375afe7c0f4
Väinö Rantanen <vaino01@hotmail.fi>
2023-06-28 19:04:10 +0300

    11.6 korjattu env oleen windows

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es2021': true,
-    'node': true
-},
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jest/globals": true
+  },
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended"
@@ -25,7 +25,7 @@ module.exports = {
       ],
     "linebreak-style": [
       "error",
-      "unix"
+      "windows"
     ],
     "quotes": [
       "error",
