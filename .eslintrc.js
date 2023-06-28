ccc5979bc43974f63d9942e25e422947bb8a33f7
Väinö Rantanen <vaino01@hotmail.fi>
2023-06-28 18:56:36 +0300

    11.6 vaihdettu lint env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest/globals": true
-  },
+  'env': {
+    'commonjs': true,
+    'es2021': true,
+    'node': true
+},
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended"
