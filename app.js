644cfc3b4ceeddb0c64cc45e7f3b33b90c8e7695
kimyu <kimyu@hanasiu.local>
2023-06-30 15:23:17 +0900

    package.json

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 8001')
+  console.log('server started on port 3000')
 })
