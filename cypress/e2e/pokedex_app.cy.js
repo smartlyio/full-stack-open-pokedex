57adbce3b389058e85a19c91d85eee4004ba1da9
hobi9 <hans25@live.it>
2023-06-30 22:09:07 +0200

    added a new cypress test

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,9 +1,18 @@
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  beforeEach(() => {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+
+  it('can navigate from the main page to the page of a particular Pokemon', function () {
+    cy.contains('ivysaur').click()
+
+    cy.contains('chlorophyll')
   })
 })
