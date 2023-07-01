e3b563ed432b6648afd63b31aa84b33c96421dc2
hobi9 <hans25@live.it>
2023-07-01 21:02:26 +0200

    testing deployment failure

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  throw 'err'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
