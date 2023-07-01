b72e97e088c271accc39eaa746c76578e994c3ab
hobi9 <hans25@live.it>
2023-07-01 21:21:05 +0200

    trying deployment with healtch check

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'err'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
