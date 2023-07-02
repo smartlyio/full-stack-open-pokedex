394d78e6898d9d59067d03df5c1d7966bb2a2916
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-02 16:41:43 +0300

    11.10 kokeiltu lisätä html tiedoston lisääminen

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
 
 // Heroku dynamically sets a port
@@ -6,6 +8,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
