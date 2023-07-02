1f4ebab926dbdd9d305b8db9d5641e79b094cb80
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2023-07-02 21:54:44 +0300

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -10,13 +10,3 @@ module.exports = defineConfig({
     },
   },
 })
-
-module.exports = (on, config) => {
-  on('before:browser:launch', (browser = {}, launchOptions) => {
-    console.log(launchOptions.args)
-    if (browser.name == 'chrome') {
-      launchOptions.args.push('--disable-gpu')
-    }
-    return launchOptions
-  }),
-}
