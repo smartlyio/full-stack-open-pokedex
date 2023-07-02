0e85b08c37cc277fd823a87e8f7d6ac31b378f07
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-02 16:43:29 +0300

    tyylivirheet korjattu

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
 
 const app = express()
 
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(PORT, () => {
