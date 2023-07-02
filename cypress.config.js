68b919870782f81446b1f5384d303a1d45e6d0db
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2023-07-02 16:18:04 +0300

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3001',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
