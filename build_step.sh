961a901e7884e1c21239a2e686c1e509573a0068
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-03 17:50:17 +0300

    lisätty build_step tiedostoon linttaus ja testit

--- build_step.sh
+++ build_step.sh
@@ -5,5 +5,14 @@ echo "Build script"
 # Install dependencies
 npm install
 
+# Linttaus
+npm run eslint
+
 # Build the project
 npm run build
+
+# Testit
+npm run test
+
+# e2e testit
+npm run test:e2e
