51ea94d1a9e926977caa8351249b6483bc23f75c
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2023-07-03 14:54:29 +0300

    Add health check & rollback on failure

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const PORT = process.env.PORT || 3001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  // eslint-disable-next-line no-undef
+  res.send({ version: process.env.npm_package_version })
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
