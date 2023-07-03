04a7302988f37cc540f3ed201762cbcd6f705f57
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-03 21:33:33 +0300

    11.12 stimuloidaan rikkinäistä deployausta

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('6') // change this string to ensure a new version deployed
+  res.send('7') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  res.send('this should be ok')
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
