da4f54c5c8f0639e6467339cdea15e8b0fad1396
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-03 18:07:08 +0300

    korjattu scriptitiedosto

--- build_step.sh
+++ build_step.sh
@@ -11,11 +11,5 @@ npm run eslint
 # Build the project
 npm run build
 
-# Start the application in the background
-npm start &
-
-# Wait for the application to be accessible
-sleep 5
-
-# Run the e2e tests
-npm run test:e2e
+# Testit
+npm run test
