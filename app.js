fffd23a0c29d35263de9b1919581e6d1da01f516
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-03 21:24:25 +0300

    korjattu path /health

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('4') // change this string to ensure a new version deployed
+  res.send('5') // change this string to ensure a new version deployed
 })
 
-app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
