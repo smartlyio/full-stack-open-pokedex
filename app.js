6aa454d83da7d37c3f8033066adb1e8857472be5
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-03 21:44:29 +0300

    tehty 11.12 asti, muutettu app.js toimivaksi

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('7') // change this string to ensure a new version deployed
+  res.send('8') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
