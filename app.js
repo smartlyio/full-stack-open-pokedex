93a1ad90c5f67e46202babd600652f0774398d96
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-03 17:04:26 +0300

    kokeiltu korjata index.html syöttö

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const path = require('path')
 
 const app = express()
 
@@ -8,10 +7,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
