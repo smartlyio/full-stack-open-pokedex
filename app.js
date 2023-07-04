fcbe63567705c394c9e923591835323ea6b8fdf4
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-04 17:50:50 +0300

    11.15 lisätty tag release

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('9, lisätty if statementti') // change this string to ensure a new version deployed
+  res.send('10, lisätty tag_release') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
