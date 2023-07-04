18d8c1c569b1f09c297793d9a93b6a2afecc3a4f
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-07-04 23:31:59 +0300

    Added test for pokemon page

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5005')
+    cy.get('a').contains('charizard').click()
+    cy.contains('Hidden ability')
+    cy.contains('blaze')
+  })
 })
