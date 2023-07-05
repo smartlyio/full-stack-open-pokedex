162324217f5b5f5318da854251d89389f9390568
gcarugati <carugatiguido@gmail.com>
2023-07-05 04:24:14 -0300

    ci/cd test

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import LoadingSpinner from "./LoadingSpinner";
 import { useApi } from "./useApi";
 import PokemonAbility from "./PokemonAbility";
