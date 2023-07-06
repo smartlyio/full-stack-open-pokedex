25e60541b08b0fe74a8e6e0e31a60b89d5fd0f02
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-07-07 00:25:31 +0300

    Health Check

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/version', (req, res) => {
   res.send('1')
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5005')
