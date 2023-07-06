01dbc66f3ccf9f4f7fa354e41e7f6fd539acc31f
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-07-06 23:04:37 +0300

    Pipeline test

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5005
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5005')
