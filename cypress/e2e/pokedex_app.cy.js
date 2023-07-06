ffd349fec8998364a115e42f40309014c332329e
Väinö Rantanen <vaino01@hotmail.fi>
2023-07-06 17:50:21 +0300

    11.18 korjattu testit

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,12 +1,12 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     cy.visit('http://localhost:5000')
-    cy.contains('moiivysaur')
+    cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('can navigate to a specific Pokemon page', function() {
     cy.visit('http://localhost:5000')
-    cy.contains('moiivysaur').click() // Click on the link for the desired Pokemon
+    cy.contains('ivysaur').click() // Click on the link for the desired Pokemon
     cy.contains('chlorophyll') // Verify that the page contains the string 'chlorophyll'
   })
 })
