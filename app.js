2d1692bf527e670a44411c81aa1978596cdcc244
Tuuli Toivanen-Gripentrog <tuuli.k.toivanen@gmail.com>
2023-07-08 14:12:58 +0300

    Fix deployment

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
+  // throw 'error...'
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('7') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
