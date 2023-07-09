bb93d8100e4e85460acda43d6aa143effa7fdfbe
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2023-07-09 16:56:24 +0200

    cypress

--- cypress/e2e/first.cy.js
+++ cypress/e2e/first.cy.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 describe("Pokedex", function () {
   it("front page can be opened", function () {
-    cy.visit("http://localhost:4100");
+    cy.visit("http://localhost:8081");
     cy.contains("ivysaur");
     cy.contains(
       "Pokémon and Pokémon character names are trademarks of Nintendo."
