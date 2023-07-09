919e6ce6dff11795b592c1125b6844cb04a3cc5a
Meqdad <Meqdad_@outlook.com>
2023-07-09 13:08:52 +0300

    11.12 -healthCheck

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.static('dist'))
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
