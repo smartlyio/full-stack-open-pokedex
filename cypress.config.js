12a0e6d25182e06580d3445a1e4af75a9844f2a6
Meqdad <Meqdad_@outlook.com>
2023-07-09 10:51:02 +0300

    part11b  -11.9 -e2e:cypress

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  video: false,
+  e2e: {
+    // eslint-disable-next-line no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'http://localhost:5000',
+  },
+})
---
+++ cypress/e2e/poki.cy.js
@@ -0,0 +1,14 @@
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('')
+    cy.contains('ivysaur')
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+  it('pokemon page can be navigated to', function () {
+    cy.visit('')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
