5316667af0e65da98cf6a47bfcfaa4457cb7e471
Tuuli Toivanen-Gripentrog <tuuli.k.toivanen@gmail.com>
2023-07-10 21:38:52 +0300

    Ex.11.17 Add protection to master branch

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('19 - NEW') // change this string to ensure a new version deployed
+  res.send('20') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
