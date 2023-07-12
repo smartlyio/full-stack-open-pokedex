2b5ce8de797921973c87bfba67ba0d5488462a35
Tuuli Toivanen-Gripentrog <tuuli.k.toivanen@gmail.com>
2023-07-12 13:44:40 +0300

    Add conditions to Discord notification

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('21') // change this string to ensure a new version deployed
+  res.send('22') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
