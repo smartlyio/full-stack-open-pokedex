80c405f4b5324870a68ecd558d18e3bbbbf417d1
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-07-13 07:35:59 -0400

    lint cypressconfig

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,7 +1,10 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
+    // eslint-disable-next-line no-undef
     cy.visit('http://localhost:5001')
+    // eslint-disable-next-line no-undef
     cy.contains('ivysaur')
+    // eslint-disable-next-line no-undef
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
