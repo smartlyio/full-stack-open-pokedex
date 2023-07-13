96e25df7c7a8540202bed825790bbc2ded010e65
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-07-13 08:16:28 -0400

    Github Action bugfix

--- cypress.config.js
+++ cypress.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
   },
 })
--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,18 +1,12 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    // eslint-disable-next-line no-undef
     cy.visit('http://localhost:5001')
-    // eslint-disable-next-line no-undef
     cy.contains('ivysaur')
-    // eslint-disable-next-line no-undef
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('Right Pokemon content when navigate', function () {
-    // eslint-disable-next-line no-undef
     cy.visit('http://localhost:5001')
-    // eslint-disable-next-line no-undef
     cy.contains('ivysaur')
-    // eslint-disable-next-line no-undef
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
