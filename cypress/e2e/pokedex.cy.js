4b0f5e2c2e52489fa74ac676a1e975193d10d0f0
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-07-13 07:45:24 -0400

    GitHub Action with Cypress

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -7,4 +7,12 @@ describe('Pokedex', function () {
     // eslint-disable-next-line no-undef
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('Right Pokemon content when navigate', function () {
+    // eslint-disable-next-line no-undef
+    cy.visit('http://localhost:5001')
+    // eslint-disable-next-line no-undef
+    cy.contains('ivysaur')
+    // eslint-disable-next-line no-undef
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
 })
