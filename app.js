66f8b471e34d5f824516020c2078f65505a2a39e
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-07-13 09:11:55 -0400

    modify port

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5001
+const port = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`server started on port ${PORT}`)
+  console.log(`server started on port ${port}`)
 })
