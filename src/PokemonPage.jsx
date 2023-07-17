74cc062901eb20dd7f97a7f526553498649c00c2
mutlr <rmutlu01@hotmail.com>
2023-07-18 02:01:04 +0300

    11.7

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -8,6 +8,7 @@ import ErrorMessage from './ErrorMessage'
 const formatName = (nameWithDash) => nameWithDash.replace('-', ' ')
 
 const PokemonPage = ({ previous, next }) => {
+
   const { name } = useParams()
   const { data: pokemon, error, isLoading } = useApi(`https://pokeapi.co/api/v2/pokemon/${name}`)
 
@@ -27,12 +28,18 @@ const PokemonPage = ({ previous, next }) => {
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
   // eslint-disable-next-line no-console
   console.log('hiddenAbility=', hiddenAbility)
+  // eslint-disable-next-line no-console
+  console.log('Previous: ', previous)
+  // eslint-disable-next-line no-console
+  console.log('Next: ', next)
+  //const previousName = previous ? previous.name : pokemon.name
+  //const nextName = next ? next.name : pokemon.name
   return (
     <>
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
