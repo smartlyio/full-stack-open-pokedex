52e245c05423278b362f654eee610b2394168d58
mutlr <rmutlu01@hotmail.com>
2023-07-18 02:45:43 +0300

    11.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,12 @@ module.exports = {
     "browser": true,
     "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -17,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
