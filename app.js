363912af1bd233de87bb7807e76b2bf02b74f022
mutlr <rmutlu01@hotmail.com>
2023-07-18 17:22:37 +0300

    11.12 should not deploy

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('11') // change this string to ensure a new version deployed
+  res.send('15') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
-  //throw res.status(404).json(error: 'broken')
+  throw res.status(404).json({ error: 'broken' })
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
