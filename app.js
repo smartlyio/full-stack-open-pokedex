a352b52a30e0b852aeabf76939d643b19e4e79cf
mutlr <rmutlu01@hotmail.com>
2023-07-18 15:32:48 +0300

    11.12

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/version', (req, res) => {
   res.send('3') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
