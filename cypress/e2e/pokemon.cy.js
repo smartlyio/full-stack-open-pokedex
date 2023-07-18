9d6a5e7c07fccf0caa87cbe09d6159578b0ad9e6
mutlr <rmutlu01@hotmail.com>
2023-07-18 03:00:18 +0300

    11.9

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,11 +1,12 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5000')
+  })
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('Navigation works', () => {
-    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('kakuna').click()
     cy.contains('ivysaur').should('not.exist')
