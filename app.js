d3e13aa117eafe550de69867a50b62d40b324c7e
Jesse Kankaisto <jesse.kankaisto@gmail.com>
2023-07-19 17:26:55 +0300

    pipeline eslint fix

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-// Conditionally load dotenv in development mode
+
 if (process.env.NODE_ENV === 'development') {
-  const dotenv = require('dotenv');
-  dotenv.config();
+  const dotenv = require('dotenv')
+  dotenv.config()
 }
 
-const PORT = process.env.PORT || 3000;
-app.use(express.static('dist'));
+const PORT = process.env.PORT || 3000
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
