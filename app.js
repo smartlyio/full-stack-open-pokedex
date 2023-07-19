7b0368516f05ba2616f572c5ec3b35b8060b595e
Jesse Kankaisto <jesse.kankaisto@gmail.com>
2023-07-19 18:38:56 +0300

    so it doesnt work...

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ if (process.env.NODE_ENV === 'development') {
 
 const PORT = process.env.PORT || 3000
 app.use(express.static('dist'))
-
+/*
 app.get('/health', (req, res) => {
   res.send('ok')
 })
-
+*/
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
