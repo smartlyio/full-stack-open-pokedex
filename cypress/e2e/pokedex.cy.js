5b43b5e05dd65b330628726ff9ad7f1cf2627ecf
Bruno Sousa <brunobarrosdesousa@outlook.com>
2023-07-21 18:25:52 -0300

    adding  test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', () => {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can visit ivysaur', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
