7cb73ccdc32e92fff969cb573b7c61f9497787fe
Bruno Sousa <brunobarrosdesousa@outlook.com>
2023-07-21 21:59:20 -0300

    testing pipeline

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
