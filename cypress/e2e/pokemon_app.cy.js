5193d10f753aaa76fd2f904b9bab64378bb62333
AdamsFontil <adamsfuntil@gmail.com>
2023-07-23 07:54:16 -0400

    change port for e2e test to 5001

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:5001')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('pokemon page can be navigated to', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:5001')
     cy.contains('pidgey').click()
     cy.contains('big pecks')
     cy.contains('keen eye')
