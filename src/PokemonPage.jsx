9320e8ef16903f738973fe6a66cfc71cdf08171d
ttajakka <tuomas.tajakka@gmail.com>
2023-07-25 21:28:40 +0300

    add 11.2, 11.3

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -18,6 +18,7 @@ const PokemonPage = ({ previous, next }) => {
     return <ErrorMessage error={error} />
   }
 
+
   const { type } = pokemon.types.find((type) => type.slot === 1)
   const stats = pokemon.stats.map((stat) => ({
     name: formatName(stat.stat.name),
