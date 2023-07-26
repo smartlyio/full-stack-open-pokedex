77c7fa7b9e4b9443f0c8b487c6654f25aea50d0a
Erkka Tuhkanen <erkka.tuhkanen@hotmail.com>
2023-07-26 15:09:46 +0300

    small tweak

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
-import LoadingSpinner from './LoadingSpinner'
-import { useApi } from './useApi'
-import PokemonAbility from './PokemonAbility'
 import ErrorMessage from './ErrorMessage'
+import LoadingSpinner from './LoadingSpinner'
+import PokemonAbility from './PokemonAbility'
+import { useApi } from './useApi'
 
 const formatName = (nameWithDash) => nameWithDash.replace('-', ' ')
 
@@ -32,7 +32,7 @@ const PokemonPage = ({ previous, next }) => {
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
