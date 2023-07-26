ad846c16dc73207319869b480498553f98bc7dc5
Erkka Tuhkanen <erkka.tuhkanen@hotmail.com>
2023-07-26 17:48:43 +0300

    eslint cypress plugin

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,15 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  describe('after opening a single pokemon', function() {
+    beforeEach(function() {
+      cy.visit('http://localhost:5001')
+      cy.contains('ivysaur').click()
+    })
+    it('pokemon page can be navigated to', function() {
+      cy.contains('ivysaur')
+      cy.contains('chlorophyll')
+    })
+  }
+  )
 })
