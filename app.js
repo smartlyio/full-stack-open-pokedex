70f372fb922d8f0cef309cf30aeddd0a2fe6402c
Erkka Tuhkanen <erkka.tuhkanen@hotmail.com>
2023-07-26 23:16:32 +0300

    tweak

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/version', (req, res) => {
+  res.send('10')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
