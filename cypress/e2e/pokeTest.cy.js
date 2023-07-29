bcc9102b0251a0b3e97a5116084a2fbea8a77734
perttinen <samuli.viitala@gmail.com>
2023-07-29 21:14:28 +0300

    11.9.7

--- cypress/e2e/pokeTest.cy.js
+++ cypress/e2e/pokeTest.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('link from main to PokemonPage works', function() {
+    cy.visit('http://localhost:5000', { timeout: 150000 })
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('venusaur').should('not.exist')
+  })
 })
