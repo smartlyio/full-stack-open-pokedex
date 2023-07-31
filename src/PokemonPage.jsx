872f528ddf3b4c485e330c29f3d533838a45ac83
perttinen <samuli.viitala@gmail.com>
2023-07-31 18:22:19 +0300

    11.10.4

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -31,8 +31,8 @@ const PokemonPage = ({ previous, next }) => {
     <>
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
-        <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${next.name}`}>ext</Link>}
+        <Link to="/">ome</Link>
+        {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
