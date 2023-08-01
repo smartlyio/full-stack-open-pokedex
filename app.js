8968449e6b3f7722e57c9de2dac57ec9267a7941
xx19898 <zomg13912111@gmail.com>
2023-08-01 19:23:37 +0300

    fixed csp

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express')
-const csp = require('express-csp-header')
 const app = express()
 
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header')
 
-app.use(csp({
-  policies: {
-    'default-src': [csp.NONE],
-    'img-src': [csp.SELF],
+app.use(expressCspHeader({
+  directives: {
+    'default-src': [SELF],
+    'script-src': [SELF, INLINE, 'somehost.com'],
+    'style-src': [SELF, 'mystyles.net'],
+    'img-src': ['data:', 'images.com'],
+    'worker-src': [NONE],
+    'block-all-mixed-content': true
   }
 }))
+
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
