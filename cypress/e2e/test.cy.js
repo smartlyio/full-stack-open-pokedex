747083ee931fab79d305fe7e05fe3aa05adf069f
Santeri <santeri.ora@tuni.fi>
2023-08-01 23:41:41 +0300

    made another cypress test

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -5,3 +5,11 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokedex', function () {
+  it('can navigate to a specific Pokemon page', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
