e029d315a94dc0019e9c3174f34baa85e7b6de75
xx19898 <zomg13912111@gmail.com>
2023-08-02 14:08:50 +0300

    testing that deployment happens automatically

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.status(200).send('ok')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
