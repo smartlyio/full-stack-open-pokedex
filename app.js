091aca8ceddf03845a8dd4c52d5baf53150f39d8
xx19898 <zomg13912111@gmail.com>
2023-08-02 21:10:40 +0300

    new commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.status(200).send('ok')
+  res.status(200).send('Everything good')
 })
 
 app.listen(PORT, () => {
