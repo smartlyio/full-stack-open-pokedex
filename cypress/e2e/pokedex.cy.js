6ed0960bf2254f3a483bb39c228702b5b99b70e2
Thomas Aspbury <tomaspbury@icloud.com>
2023-08-03 19:03:22 +0100

    Add Cypress to project

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,7 @@
+describe('Pokedex', function () {
+  it('opens front page', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
