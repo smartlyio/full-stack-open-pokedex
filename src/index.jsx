96f7f5c6d5428aea5d803c2d178fff3220a620fd
seungahpyun <seungahpyun@icloud.com>
2023-08-03 11:42:27 +0200

    part11 11.6

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// eslint-disable-next-line no-undef
+const root = document.getElementById('app')
+ReactDOM.createRoot(root).render(<App />)
