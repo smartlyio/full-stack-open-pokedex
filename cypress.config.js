c46ace00f786ba672b0be48db745c9b9fbd28a5f
seungahpyun <seungahpyun@icloud.com>
2023-08-03 13:24:24 +0200

    part11 11.9 fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,4 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
-  }
-});
+})
