a5af83dd69ed14c19315430a16e4e5052eeafec7
seungahpyun <seungahpyun@icloud.com>
2023-08-03 13:19:56 +0200

    part11 11.9

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-// eslint-disable-next-line no-undef
+
 const root = document.getElementById('app')
+// eslint-disable-next-line no-undef
 ReactDOM.createRoot(root).render(<App />)
