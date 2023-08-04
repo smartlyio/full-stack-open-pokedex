30177219d46e46c239b54ae04f4ea23f37142e17
Thomas Aspbury <tomaspbury@icloud.com>
2023-08-04 17:33:58 +0100

    Add http health checks

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// Dummy route to check for a diff in deployed code
+app.get('/version', (req, res) => {
+  res.send('3')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
