d4b3f98c1f936482ca693c2be00ad78a7a814a86
perttinen <samuli.viitala@gmail.com>
2023-08-04 19:31:10 +0300

    home to ome

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -31,7 +31,7 @@ const PokemonPage = ({ previous, next }) => {
     <>
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
-        <Link to="/">Home</Link>
+        <Link to="/">ome</Link>
         {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
