a878656ff2622ff7cbf2ea2343915400b44fe29c
perttinen <samuli.viitala@gmail.com>
2023-08-05 09:35:48 +0300

    This should be deployed

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -31,7 +31,7 @@ const PokemonPage = ({ previous, next }) => {
     <>
       <div className="links">
         {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
-        <Link to="/">ome</Link>
+        <Link to="/">Home</Link>
         {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
