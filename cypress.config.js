83e3d8a6ce2e553bc20dbd2a83db8fa4be77ad14
Ismail Dewidar <60668385+ismail-97@users.noreply.github.com>
2023-08-05 18:46:05 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
   },
-});
+})
