6d17a7b96702e59381de26ae73f56d2223baae17
Miao <himia301@gmail.com>
2023-08-06 18:02:30 +0300

    11.11 Automatic deployments

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('a new version deployed') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5001')
