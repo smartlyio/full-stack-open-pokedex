1d09765c6e92c3a074d5f21a76d99359706aedff
Ismail Dewidar <60668385+ismail-97@users.noreply.github.com>
2023-08-06 13:30:48 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        launchOptions.args = launchOptions.args.filter(item => item !== "--disable-dev-shm-usage")
-        return launchOptions;
-      });
     },
   },
 })
