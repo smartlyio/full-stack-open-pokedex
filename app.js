23f0d7c5b0c490502ef6d588ef9ee058a8d945ec
Miao <himia301@gmail.com>
2023-08-06 18:24:40 +0300

    11.11.2 Automatic deployments

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('a new version deployed') // change this string to ensure a new version deployed
+  res.send('a new version deployed!') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
