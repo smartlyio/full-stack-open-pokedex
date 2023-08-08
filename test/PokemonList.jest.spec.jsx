c93889a18b8bfa8f8758c065f852f04ac2510c52
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-08-08 16:16:13 -0600

    11.6 fix code format

--- test/PokemonList.jest.spec.jsx
+++ test/PokemonList.jest.spec.jsx
@@ -4,23 +4,25 @@ import { BrowserRouter } from 'react-router-dom'
 import '@testing-library/jest-dom/extend-expect'
 import PokemonList from '../src/PokemonList'
 
-
-const pokemonList = [{
-  url: 'https://pokeapi.co/api/v2/pokemon/1/',
-  name: 'bulbasaur',
-  id: 1
-}, {
-  url: 'https://pokeapi.co/api/v2/pokemon/133/',
-  name: 'eevee',
-  id: 133
-}]
+const pokemonList = [
+  {
+    url: 'https://pokeapi.co/api/v2/pokemon/1/',
+    name: 'bulbasaur',
+    id: 1,
+  },
+  {
+    url: 'https://pokeapi.co/api/v2/pokemon/133/',
+    name: 'eevee',
+    id: 133,
+  },
+]
 
 describe('<PokemonList />', () => {
   it('should render items', () => {
     render(
       <BrowserRouter>
         <PokemonList pokemonList={pokemonList} />
-      </BrowserRouter>
+      </BrowserRouter>,
     )
     expect(screen.getByText('bulbasaur')).toBeVisible()
     expect(screen.getByText('eevee')).toBeVisible()
