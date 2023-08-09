96b39a9d713e7828803cee4add7003a74764adfa
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2023-08-09 15:21:36 -0600

    test

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
-  throw 'error'
-
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
