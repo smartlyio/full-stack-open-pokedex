956e2c555501acb53d33dbbcc5495a5dd9a9f4a6
C.Ziskas <z1skgr@yahoo.gr>
2023-08-10 16:44:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
+// eslint-disable-next-line
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)})
