b0f25404a31bf7e9d8234eb3bc92bf2bdebb41a9
z1skgr <z1skgr@yahoo.gr>
2023-08-11 10:50:49 +0300

    Update sp.cy.js

--- cypress/e2e/sp.cy.js
+++ cypress/e2e/sp.cy.js
@@ -1,8 +1,8 @@
 describe('Pokedex', function() {
-  beforeEach(function() {
-    cy.visit('http://localhost:8081')
-  })
+
+    
   it('front page can be opened', function() {
+    cy.visit('http://localhost:8081')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
