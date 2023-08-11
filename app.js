c5c684f51d69ff1ab7cb0d7b18fe6e5056cb2c1b
z1skgr <z1skgr@yahoo.gr>
2023-08-11 12:36:36 +0300

    updates

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const app = express()
 // eslint-disable-next-line
 const PORT = 8081 || process.env.PORT
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)})
+
+  
