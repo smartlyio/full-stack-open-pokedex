e3f0a3ea742ce351b40563ba94de3254bc19e25f
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-08-13 23:33:47 +0300

    Exercise 11.9 - bug fix to allow lint step to complete

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 })
