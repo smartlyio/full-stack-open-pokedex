4c4d2eca5d3b069009cf439d2df0d99d1d2792a4
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-08-13 23:31:18 +0300

    Exercise 11.9 initial commit to test e2e testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        'jest/globals': true,
-        node: true,
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+    node: true,
+    'cypress/globals': true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    plugins: ['react', 'jest'],
-    rules: {
-        indent: ['error', 2],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'never'],
-        eqeqeq: 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': ['error', 'always'],
-        'arrow-spacing': ['error', { before: true, after: true }],
-        'no-console': 'error',
-        'react/prop-types': 0,
-    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest', 'cypress'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 'error',
+    'react/prop-types': 0,
+  },
 }
