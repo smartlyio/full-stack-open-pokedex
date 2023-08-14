ffb8635e24972dff29b5e59cbd01a8c4715a7ecf
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-08-14 18:15:32 +0300

    Testing if error throws deployment off in actions - 11.11

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
-
 // app.get('/health', (req, res) => {
-//   throw 'error...'
-//   // eslint-disable-next-line no-unreachable
 //   res.send('ok')
 // })
+
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
