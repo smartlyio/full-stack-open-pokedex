0af443de7c606c086d37571986c1c99042a9361d
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-08-14 22:03:41 +0300

    Final submission for 11.11 - rollback not working at the moment

--- app.js
+++ app.js
@@ -6,21 +6,12 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// })
-
-// ! work on below - perhaps use a custome check
-//! perhaps set the header in the check and send the version here
-//! !!!!!!!!!!!1
-app.get('/version', (req, res) => {
-  res.send('14') // change this string to ensure a new version deployed
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
-app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
-  res.send('ok')
+app.get('/version', (req, res) => {
+  res.send('1')
 })
 
 app.listen(PORT, () => {
