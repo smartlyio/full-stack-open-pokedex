e35ff3fa954c96f344e6bbc6d882355112dc91d4
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-08-14 15:33:57 +0300

    Testing deployment 11.11 with checks

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-// Changed to port 3000 as 5000 is used internally by macOS...
-const PORT = process.env.PORT || 3000
+// Changed to port 3003 as 5000 is used internally by macOS...
+const PORT = process.env.PORT || 3003
 
 app.use(express.static('dist'))
 
@@ -12,5 +12,5 @@ app.get('/health', (req, res) => {
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 3000')
+  console.log('server started on port 3003')
 })
