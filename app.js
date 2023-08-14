11e9adefc9f36f0e66ea5264edfffccb8e37a3e0
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-08-14 17:42:22 +0300

    Bug fixes 11.11

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express')
 const app = express()
 
 // Changed to port 3003 as 5000 is used internally by macOS...
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+// app.get('/health', (req, res) => {
+//   res.send('ok')
+// })
+
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 3000')
+  console.log('server started on port 8080')
 })
