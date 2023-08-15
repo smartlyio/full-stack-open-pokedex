bb5663f5bec0cf2bd1fa955f5e34342c47db4896
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-08-15 19:38:19 +0300

    Final submission 11.18

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,14 +1,14 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:1232')
+    cy.visit('http://localhost:3000')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
-  //! revert back to 3000
+
   it('pokemon page can be navigated to', function () {
-    cy.visit('http://localhost:1212')
+    cy.visit('http://localhost:3000')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
