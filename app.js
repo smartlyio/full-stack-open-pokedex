b5552f22a83629870e46e814fd0f2e1cb01c4c1a
Suraj Mishra <mishrasur7@gmail.com>
2023-08-16 23:06:25 +0300

    health check

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('version')
+})
+
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5001')
 })
