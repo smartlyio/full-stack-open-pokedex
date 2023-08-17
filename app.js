a1416ba043e8dd0b4af72d8fde7ef3ea2917936e
Max Mynter <maxmynter@me.com>
2023-08-17 16:24:57 +0200

    Prep for Blender

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
 })
