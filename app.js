9893960110f44796ef2643eca48d98ad878c6652
Jack Lingle <captainjack0818@gmail.com>
2023-08-17 18:01:11 -0400

    add width property to html

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static("dist"));
 
+app.get("/", (req, res) => {
+  // eslint-disable-next-line no-unreachable
+  res.status(400);
+});
 app.listen(PORT, () => {
   console.log("server started on port 5000");
 });
