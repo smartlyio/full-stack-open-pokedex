8a55e2f5f13e8ae840f9ea7b0a1f6966e55f30cb
Max Mynter <maxmynter@me.com>
2023-08-17 14:18:07 +0200

    Ex.11.9

---
+++ cypress/e2e/landingInfo.cy.js
@@ -0,0 +1,15 @@
+describe('Pokedex', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:3000')
+    cy.contains('ivysaur')
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    )
+  })
+
+  it('Can Navigate to individual pokemeon', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
