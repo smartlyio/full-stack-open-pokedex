5b1c2b19daa1230b3da9d371e32d58722210d74e
Max Mynter <maxmynter@me.com>
2023-08-17 17:25:58 +0200

    Add Health CHeck

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, '0.0.0.0', () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
