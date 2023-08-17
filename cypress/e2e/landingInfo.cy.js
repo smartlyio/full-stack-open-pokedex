0475b7a07a7a8202d2cdf7197f2a7ac71854c2eb
Max Mynter <maxmynter@me.com>
2023-08-17 16:54:16 +0200

    adapt cypress testing url to real url

--- cypress/e2e/landingInfo.cy.js
+++ cypress/e2e/landingInfo.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
@@ -8,7 +8,7 @@ describe('Pokedex', function () {
   })
 
   it('Can Navigate to individual pokemeon', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
