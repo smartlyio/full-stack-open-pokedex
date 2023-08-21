33d1476cc17b1eb479aa262585da8247e41f5c97
z1skgr <z1skgr@yahoo.gr>
2023-08-21 13:54:07 +0300

    last

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults({
+  static: './build'
+});
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log('JSON Server is running');
+});
