7ec9eaadfe0843c7eabb86f640d9a3b578767fb9
Josh Lin <joshlin@longanvision.com>
2023-08-21 13:09:45 -0400

    bugfix: add cypress plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
@@ -17,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // eslint-disable-next-line
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     video: false
   },
-});
+})
