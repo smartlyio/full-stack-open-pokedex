4ba07695b9d67a4519a80bf85a6c0d39c58ab81f
saannsaar <sannsaarinen@gmail.com>
2023-08-23 09:46:19 +0300

    11.12.1

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
