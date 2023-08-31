99bf2f1d5aae46263bfad5d3a756ed21289c2d06
charuka deshan <itscharukadeshan@gmail.com>
2023-08-31 13:35:54 +0530

    fix styling problems and add prittier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    'node': true
+
   },
   "extends": [
     "eslint:recommended",
