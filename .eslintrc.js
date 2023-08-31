681430e665e4dc68512e49dbb7e435671888922d
charuka deshan <itscharukadeshan@gmail.com>
2023-08-31 15:54:39 +0530

    update the console test

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,7 +45,8 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": 'off'
+,
     "react/prop-types": 0
   }
 }
