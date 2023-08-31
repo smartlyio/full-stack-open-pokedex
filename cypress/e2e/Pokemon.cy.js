55bc9fc6025aa09abacd473b7e9bc584dd4a3c13
unknown <pietari.m@hotmail.com>
2023-08-31 22:35:21 +0300

    add one card test

--- cypress/e2e/Pokemon.cy.js
+++ cypress/e2e/Pokemon.cy.js
@@ -1,9 +1,15 @@
 describe('Pokedex', function () {
-  it('front page can be opened', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:5000')
+  })
+  it('front page can be opened', function () {
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+  it('can navigate to pokemon from main page', function () {
+    cy.contains('charizard').click()
+    cy.contains('blaze')
   })
 })
