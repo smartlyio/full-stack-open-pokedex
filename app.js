5c56e6995d08a270a290a64a6155e53446441b50
unknown <pietari.m@hotmail.com>
2023-08-31 19:11:12 +0300

    fix express and react routing problem

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('*', (req, res) => {
+  res.sendFile('dist/index.html', { root: '.' })
+})
+
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
