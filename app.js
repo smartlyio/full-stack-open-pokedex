76e745931479a8860466ba95d7352cc85ed64f39
Jonathan Heyno <jona.heyno@gmail.com>
2023-08-31 19:58:59 +0300

    should fail

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ app.listen(PORT, () => {
 })
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('2') // change this string to ensure a new version deployed
+  res.send('3') // change this string to ensure a new version deployed
 })
