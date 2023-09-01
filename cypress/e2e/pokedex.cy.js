c34fceb2b44bc1b7d03d9e2184b78914a9629520
Kristian Hannula <kristian.hannula@proton.me>
2023-09-01 14:02:09 +0300

    Added e2e test that views a single pokemon page

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('raticate').click()
+    cy.contains('raticate')
+    cy.contains('run away')
+    cy.contains('hustle')
+  })
 })
