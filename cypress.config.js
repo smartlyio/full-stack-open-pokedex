6ce63f257492ab058894230a8918f02dc2a3a29d
Nathanael <natsiewks@gmail.com>
2023-09-01 21:11:08 +0800

    correct eslint errors

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
+/* eslint-disable */
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})/* eslint-disable */
