c2e67519d608e7179731d9632572adcb7fd8cc07
charuka deshan <itscharukadeshan@gmail.com>
2023-09-01 19:54:19 +0530

    fix style problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
 /** @format */
 
-import React from "react";
+import React from 'react'
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-import { useApi } from "./useApi";
-import LoadingSpinner from "./LoadingSpinner";
-import ErrorMessage from "./ErrorMessage";
-import PokemonPage from "./PokemonPage";
-import PokemonList from "./PokemonList";
+
+import { useApi } from './useApi'
+import LoadingSpinner from './LoadingSpinner'
+import ErrorMessage from './ErrorMessage'
+import PokemonPage from './PokemonPage'
+import PokemonList from './PokemonList'
 
 const mapResults = ({ results }) =>
   results.map(({ url, name }) => ({
     url,
     name,
     id: parseInt(url.match(/\/(\d+)\//)[1]),
-  }));
+  }))
 
 const App = () => {
   const {
     data: pokemonList,
     error,
     isLoading,
-  } = useApi("https://pokeapi.co/api/v2/pokemon/?limit=50", mapResults);
+  } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
   if (isLoading) {
-    return <LoadingSpinner />;
+    return <LoadingSpinner />
   }
   if (error) {
-    return <ErrorMessage error={error} />;
+    return <ErrorMessage error={error} />
   }
 
   return (
@@ -41,21 +42,21 @@ const App = () => {
           render={(routeParams) => {
             const pokemonId = pokemonList.find(
               ({ name }) => name === routeParams.match.params.name
-            ).id;
-            const previous = pokemonList.find(({ id }) => id === pokemonId - 1);
-            const next = pokemonList.find(({ id }) => id === pokemonId + 1);
+            ).id
+            const previous = pokemonList.find(({ id }) => id === pokemonId - 1)
+            const next = pokemonList.find(({ id }) => id === pokemonId + 1)
             return (
               <PokemonPage
                 pokemonList={pokemonList}
                 previous={previous}
                 next={next}
               />
-            );
+            )
           }}
         />
       </Switch>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
