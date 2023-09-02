11bb6dae4fd9359321bdd52487c5b1cd91a9eb7e
Kristian Hannula <kristian.hannula@proton.me>
2023-09-02 12:40:36 +0300

    Reverting to OK health check

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ app.listen(PORT, () => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('3')
+  res.send('4')
 })
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
