8d88408a468954ca6bb36a84076fd19097c895e7
Janne Tolppanen <janne.ks@gmail.com>
2023-09-03 00:41:17 +0300

    formatting

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const app = express();
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
@@ -7,5 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log('server started on port 5000')
-});
+})
