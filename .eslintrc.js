aa16da90ce37b9fce45ab7f6ec599d81126f75a1
Janne Tolppanen <janne.ks@gmail.com>
2023-09-04 15:55:45 +0300

    removed "cypress/globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,7 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    "node": true, 
-    "cypress/globals": true
+    "node": true
   },
   "extends": [
     "eslint:recommended",
