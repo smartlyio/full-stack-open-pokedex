0c9a635e5443bd3a21f363ecf16317ff42eb20eb
Janne Tolppanen <janne.ks@gmail.com>
2023-09-05 08:37:25 +0300

    11.9 done

--- cypress/e2e/pokedex-e2e.cy.js
+++ cypress/e2e/pokedex-e2e.cy.js
@@ -1,8 +1,14 @@
-/* eslint-disable linebreak-style */
 describe('Pokedex', function() {
+  const PORT = process.env.PORT || 5000
+  const MAINSITE = `http://localhost:${PORT}`
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit(MAINSITE)
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('ivysaur page opens', function() {
+    cy.visit(MAINSITE)
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
