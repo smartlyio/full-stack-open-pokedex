4bc6252fe098019fdfdae9492b0acf4739ee8f05
unknown <pietari.m@hotmail.com>
2023-09-07 00:05:43 +0300

    will health check fail as it supposed

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // throw 'error...'
+  throw 'error...'
   // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('3')
+  res.send('4')
 })
 
 app.get('*', (req, res) => {
