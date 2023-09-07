8f620b731e3cd8726a936b522a1d0a922654ca33
Janne Tolppanen <janne.ks@gmail.com>
2023-09-07 15:48:02 +0300

    health check should work again

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 // Health check
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// })
-
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
+
+// app.get('/health', (req, res) => {
+//   throw 'error...'
+//   // eslint-disable-next-line no-unreachable
+//   res.send('ok')
+// })
 
 app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
