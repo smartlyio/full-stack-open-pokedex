820d2e5056159755fa97f94a7da4f7d7ca50db29
Janne Tolppanen <janne.ks@gmail.com>
2023-09-07 15:37:33 +0300

    health error

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 // Health check
+// app.get('/health', (req, res) => {
+//   res.send('ok')
+// })
+
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
