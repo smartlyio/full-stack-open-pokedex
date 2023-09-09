12bde595a07c3d2d60ca4dc754e90c020dd17f44
Joni Joensuu <joni.joens@gmail.com>
2023-09-09 23:34:44 +0300

    11.11 fix

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('2') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
