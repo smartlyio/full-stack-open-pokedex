cba4c245b5cbca0790cb6a0a0322dafdea9bcb50
huynguyen <103279988+huynguyen2110@users.noreply.github.com>
2023-09-13 17:07:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 
 // Heroku dynamically sets a port
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log("server started on port 5000");
 });
