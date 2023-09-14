0b1c1f3b8ee0e33e14bebe235913fc44c0759d52
Kai <nguyenbahuy2110@gmail.com>
2023-09-14 14:03:18 +0700

    update endpoint test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/version', (req, res) => {
-  res.send('1234') // change this string to ensure a new version deployed
+  res.send('hellooo') // change this string to ensure a new version deployed
 })
 app.use(express.static('dist'))
 
