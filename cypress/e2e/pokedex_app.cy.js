ef017c236ee07f8dc62b26e78d6c414370f6d8dc
Angelica Järvenpää <74900808+angelicaroselie@users.noreply.github.com>
2023-09-17 10:24:38 +0300

    add new test for proper content

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon has proper hidden ability', function() {
+    cy.visit('http://localhost:5001')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
+
+
+
