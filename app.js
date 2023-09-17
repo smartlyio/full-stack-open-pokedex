f73d6aebe9ddae56bbb1b41cf5931bc31a4e9df6
Toni Hukka <berendin@gmail.com>
2023-09-17 23:00:46 +0300

    11.6 fixed lint errors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
