68ef13f8f405d6232669f5ba98704de35f723965
Angelica Järvenpää <74900808+angelicaroselie@users.noreply.github.com>
2023-09-18 05:38:22 +0300

    add custom check

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const { exec } = require('child_process')
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5001
@@ -9,6 +10,18 @@ app.use(express.static('dist'))
 app.get('/health', (req, res) => { // 11.11 - health check
   res.send('ok')
 })
+
+app.get('/custom_health_check', (req, res) => { // 11.12 - custom health check
+  res.send('everything is ok')
+  exec('run ./health_check.sh', (err) => {
+    if (err) {
+      res.send('1')
+    } else {
+      res.send('0')
+    }
+  })
+})
+
 
 app.get('/version', (req, res) => { // 11.11 - version endpoint
   res.send('1') // change this string to ensure a new version deployed
