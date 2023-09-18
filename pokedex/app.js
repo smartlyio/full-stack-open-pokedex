8196cccf961616380a4946f72a3ac0263d25eff1
Hoopaugi <hoopaugi@gmail.com>
2023-09-18 13:49:36 +0300

    Health check endpoint

--- pokedex/app.js
+++ pokedex/app.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
--- pokedex/cypress/e2e/pokedex.cy.js
+++ pokedex/cypress/e2e/pokedex.cy.js
@@ -1,12 +1,12 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   it('Can navigate to an individual pokemons page', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
