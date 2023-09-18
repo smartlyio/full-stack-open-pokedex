06e29fed41fe8b044c4f2991a2badd494d62d3d8
Angelica Järvenpää <74900808+angelicaroselie@users.noreply.github.com>
2023-09-18 05:45:51 +0300

    up

--- app.js
+++ app.js
@@ -8,19 +8,11 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => { // 11.11 - health check
-  res.send('ok')
+  exec('./health_check.sh')
+  throw new Error('health check failed')
 })
 
-app.get('/custom_health_check', (req, res) => { // 11.12 - custom health check
-  res.send('everything is ok')
-  exec('run ./health_check.sh', (err) => {
-    if (err) {
-      res.send('1')
-    } else {
-      res.send('0')
-    }
-  })
-})
+
 
 
 app.get('/version', (req, res) => { // 11.11 - version endpoint
