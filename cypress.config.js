876b960b83c210db2b736b6b1508112c622f2ab8
Angelica Järvenpää <74900808+angelicaroselie@users.noreply.github.com>
2023-09-18 21:30:04 +0300

    fix unused args

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,9 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      console.log('setupNodeEvents')
+      console.log('on', on)
+      console.log('config', config)
     },
   },
 })
