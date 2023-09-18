28b848beb0de085f03c6568e7b6badfdfa674115
Hoopaugi <hoopaugi@gmail.com>
2023-09-18 12:28:28 +0300

    Deployment pipeline e2e tests fix

--- pokedex/cypress/e2e/pokedex.cy.js
+++ pokedex/cypress/e2e/pokedex.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:8080')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
