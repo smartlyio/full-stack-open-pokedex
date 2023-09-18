2c0095b4938c00baa5b8cb46ee4cfbbfe5a929d7
Angelica Järvenpää <74900808+angelicaroselie@users.noreply.github.com>
2023-09-18 04:08:18 +0300

    add health checks

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => { // 11.11 - health check
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => { // 11.11 - version endpoint
+  res.send('1') // change this string to ensure a new version deployed
+}
+)
+
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
