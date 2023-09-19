8973d49357ef6673f0b112940fb771e069d30bc2
Owen Rees <owenrees82@gmail.com>
2023-09-19 12:25:49 +0200

    11.11

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/healthcheck', (req, res) => {
-  throw new Error('error')
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
