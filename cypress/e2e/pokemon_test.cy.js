dc80caa8e06c905b563fee45105841f86a28472d
Toni Hukka <berendin@gmail.com>
2023-09-20 20:33:28 +0300

    11.9 final

--- cypress/e2e/pokemon_test.cy.js
+++ cypress/e2e/pokemon_test.cy.js
@@ -1,7 +1,17 @@
 describe('Pokedex', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:5001')
+  })
+
   it('frontpage can be opened', function () {
     cy.visit('http://localhost:5001')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+
+  it('pokemon page can be navigated to', function() {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
