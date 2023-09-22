f16979d88c74d886e0ddbf9bc70c49c94368bdf3
Tupsu-jy <ylinenjaakko@gmail.com>
2023-09-22 17:26:26 +0300

    new attempt for render

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
