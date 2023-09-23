312b3c0fece8b88ad44c503f9339ae6b3c7b0ebc
macwille <vw.manninen@gmail.com>
2023-09-23 16:31:21 +0300

    use production mode default

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
+  mode: "production",
   entry: "./src/index.jsx",
   output: {
     path: path.resolve(__dirname, 'dist'),
