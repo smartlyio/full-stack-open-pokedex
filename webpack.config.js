d1297c6683ab69064832ba7bcc08393f8556315b
macwille <vw.manninen@gmail.com>
2023-09-23 16:42:15 +0300

    remove mode from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  mode: "production",
   entry: "./src/index.jsx",
   output: {
     path: path.resolve(__dirname, 'dist'),
