8e6114cd8111f36e6177a57d14929f71843f1f8e
“Your <oliver.jurgen90@gmail.com>
2023-09-24 02:58:46 -0400

    change port for pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 // console.log({ PORT });
 
 app.use(express.static("dist"));
