ab00a5d8c543295a1d5022407347ed55f8e285fd
“Your <oliver.jurgen90@gmail.com>
2023-09-25 02:12:54 -0400

    change port

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5001;
-// const PORT = 5001;
+// const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 // console.log({ PORT });
 
 app.use(express.static("dist"));
