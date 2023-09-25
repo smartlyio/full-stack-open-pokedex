0f60d8449960d1c4ff8656ea4843d484da1db2e0
“Your <oliver.jurgen90@gmail.com>
2023-09-25 02:25:21 -0400

    mock fail health check

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ app.get("/version", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
+  throw "error...";
+  // eslint-disable-next-line no-unreachable
   res.send("ok");
 });
+
+// app.get("/health", (req, res) => {
+//   res.send("ok");
+// });
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
