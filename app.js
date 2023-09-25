52718a48374d928d9446ee87b78bc61d0cd21ffc
“Your <oliver.jurgen90@gmail.com>
2023-09-25 02:23:43 -0400

    add version and health check

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const PORT = 5001;
 
 app.use(express.static("dist"));
 
+app.get("/version", (req, res) => {
+  res.send("1"); // change this string to ensure a new version deployed
+});
+
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
 app.listen(PORT, () => {
   /* eslint-disable no-console */
   console.log(`server started on port ${PORT}`);
