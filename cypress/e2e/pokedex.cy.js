4b7941e2f2258ea65175e24e2f66d9f96c1b94ec
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-09-25 16:18:25 -0700

    exercise 11.9 - test: finalizes e2e test with navigation to ivysaur page

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('can navigate from main page to certain pokemon', function() {
+    cy.visit('http://localhost:8000')
+    cy.get('a[href*="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+  })
 })
