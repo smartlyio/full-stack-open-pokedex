5c40da8d4a46bf8643471c39e9266f54ad75e0d8
“Your <oliver.jurgen90@gmail.com>
2023-09-25 22:27:13 -0400

    remove throw

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/version", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  throw "error...";
+  // throw "error...";
   // eslint-disable-next-line no-unreachable
   res.send("ok");
 });
