d2918e173f7d5ac43134c705a0c32fbfd642ac20
macwille <manninen94@gmail.com>
2023-09-27 16:26:26 +0300

    update fly configuration

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-
 app.get('/health', (req,res) => {
   res.send('ok')
 })
@@ -13,6 +11,8 @@ app.get('/health', (req,res) => {
 app.get('/version', (req,res) => {
   res.send('1')
 })
+
+app.use(express.static('dist'))
 
 
 app.listen(PORT, () => {
