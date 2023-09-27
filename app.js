827250cb6ec0a3eb3a27ee8d2df16af5fce2e0e3
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-09-27 14:58:25 -0700

    exercise 11.11 - refactor: adds tcp_checks and http_checks

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ const PORT = process.env.PORT || 8000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 8000')
 })
+
