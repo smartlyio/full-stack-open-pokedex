49dc94cf2b3822ab9aba78037bbae8da54be6ad8
gxgxn <gdsinghb99@gmail.com>
2023-10-06 00:09:32 -0300

    added health & version route

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+app.get('/health', (req, res) => {
+  res.status(200).send('ok')
+})
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
