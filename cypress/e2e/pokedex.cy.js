f79f0738da73bd91dcc00c5dd38f8ed5bb164d8e
user userFamilyName <user.userfamilyname@gmail.com>
2023-10-13 16:01:25 +0300

    test if can navigate to a pokemon page

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,11 @@ describe('Pokedex', function () {
 			'Pokémon and Pokémon character names are trademarks of Nintendo.'
 		);
 	});
+	it('can open a particular Pokemon page', function () {
+		cy.visit('http://localhost:5000');
+		cy.contains('ivysaur');
+		cy.get('.list-item:nth-child(2)').click();
+		cy.contains('ivysaur');
+		cy.contains('chlorophyll');
+	});
 });
