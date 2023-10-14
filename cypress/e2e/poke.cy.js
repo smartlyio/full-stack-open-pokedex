a96db0323b4b6d633150b9d5a5777813f3cb43e0
Wilson de Souza Pereira <raskholnikov@gmail.com>
2023-10-14 09:38:23 -0300

    part11 exercises -> 11.9

--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon page can be navigated to', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
