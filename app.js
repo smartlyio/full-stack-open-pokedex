2a8143159444a67899b36a91b812e5349ed5db16
user userFamilyName <user.userfamilyname@gmail.com>
2023-10-18 13:29:32 +0300

    Health check and rollback

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('dist'));
 
+app.get('/health', (req, res) => {
+	throw 'error...';
+	// eslint-disable-next-line no-unreachable
+	res.send('ok');
+});
+
 app.listen(PORT, () => {
 	console.log(`server started on port ${PORT}`);
 });
---
+++ src/routes/pokedex.js
@@ -0,0 +1,3 @@
+app.get('/health', (req, res) => {
+	res.send('ok');
+});
