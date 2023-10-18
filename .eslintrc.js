ed385a84b7b69f65283dc31b1d273f0b56d3d4e1
Enzo Acosta <enzoacostab@gmail.com>
2023-10-18 19:20:57 -0300

    exercise-11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,10 +23,7 @@ module.exports = {
       "error",
         2
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
+    "linebreak-style": 0,
     "quotes": [
       "error",
       "single"
