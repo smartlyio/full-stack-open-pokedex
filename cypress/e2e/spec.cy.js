56f9569c344678aac3a4fb9cab79ea94ba78d70f
Enzo Acosta <enzoacostab@gmail.com>
2023-10-19 16:18:41 -0300

    exercise-11.9

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
+})
