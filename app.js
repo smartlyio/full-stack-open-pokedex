656fe74cd0fbbc731b3ec0c614a5bbc0cba9bf7a
danggro <anangdwin55@gmail.com>
2023-10-19 13:26:59 +0700

    deploy render 11

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('10') // change this string to ensure a new version deployed
+  res.send('11') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
