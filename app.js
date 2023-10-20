b02e9350bd5a1b2525ba5b7a31c228dc4e187d99
danggro <anangdwin55@gmail.com>
2023-10-20 07:55:18 +0700

    skip versioning 1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('versioning 3')
+  res.send('skip versioning 1')
   // res.send('in branch feature')
 })
 
