d141769f0f17310cac31952b49925b0d8753624c
danggro <anangdwin55@gmail.com>
2023-10-20 20:12:51 +0700

    check failure 17

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('check failure 16')
+  res.send('check failure 17')
   // res.send('in branch feature')
 })
 
