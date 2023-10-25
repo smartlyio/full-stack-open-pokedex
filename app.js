bb242f75a45178c40a144eca2e69092a0833554b
user userFamilyName <user.userfamilyname@gmail.com>
2023-10-25 19:00:25 +0300

    fixed health check error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('dist'));
 app.get('/health', (req, res) => {
 	// throw 'error...';
 	// eslint-disable-next-line no-unreachable
-	res.send('notok');
+	res.send('ok');
 });
 
 app.listen(PORT, () => {
