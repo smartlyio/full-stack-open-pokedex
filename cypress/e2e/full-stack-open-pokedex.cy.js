60680acabfdf4a01ab4b827111926bff89bb844f
Lim Si Yong <142472330+NemoDeFish@users.noreply.github.com>
2023-10-29 00:08:17 +0800

    Added E2E tests for individual pokemon

--- cypress/e2e/full-stack-open-pokedex.cy.js
+++ cypress/e2e/full-stack-open-pokedex.cy.js
@@ -1,7 +1,16 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon page can be navigated to', function() {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
   })
 })
