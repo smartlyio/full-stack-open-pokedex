3087cfc2f97709b783f00ae5bf6c4c93747311f1
Lim Si Yong <142472330+NemoDeFish@users.noreply.github.com>
2023-10-28 23:31:48 +0800

    Fixed lint errors

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -1,35 +1,33 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
-import LoadingSpinner from "./LoadingSpinner";
-import { useApi } from "./useApi";
-import PokemonAbility from "./PokemonAbility";
-import ErrorMessage from "./ErrorMessage";
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import LoadingSpinner from './LoadingSpinner'
+import { useApi } from './useApi'
+import PokemonAbility from './PokemonAbility'
+import ErrorMessage from './ErrorMessage'
 
-const formatName = (nameWithDash) => nameWithDash.replace("-", " ");
+const formatName = (nameWithDash) => nameWithDash.replace('-', ' ')
 
 const PokemonPage = ({ previous, next }) => {
-  const { name } = useParams();
-  const { data: pokemon, error, isLoading } = useApi(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const { name } = useParams()
+  const { data: pokemon, error, isLoading } = useApi(`https://pokeapi.co/api/v2/pokemon/${name}`)
 
   if (isLoading) {
-    return <LoadingSpinner />;
+    return <LoadingSpinner />
   }
   if (error) {
-    return <ErrorMessage error={error} />;
+    return <ErrorMessage error={error} />
   }
 
-  const { type } = pokemon.types.find((type) => type.slot === 1);
-  const stats = pokemon.stats
-    .map((stat) => ({
-      name: formatName(stat.stat.name),
-      value: stat.base_stat,
-    }))
-    .reverse();
-  const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden);
-  const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true);
+  const { type } = pokemon.types.find((type) => type.slot === 1)
+  const stats = pokemon.stats.map((stat) => ({
+    name: formatName(stat.stat.name),
+    value: stat.base_stat
+  })).reverse()
+  const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
+  const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
 
   // eslint-disable-next-line no-console
-  console.log("hiddenAbility=", hiddenAbility);
+  console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
       <div className="links">
@@ -38,10 +36,7 @@ const PokemonPage = ({ previous, next }) => {
         {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
-        <div
-          className="pokemon-image"
-          style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }}
-        />
+        <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
         <div className="pokemon-info">
           <div className="pokemon-name">{pokemon.name}</div>
           <div className="pokemon-stats" data-testid="stats">
@@ -57,17 +52,13 @@ const PokemonPage = ({ previous, next }) => {
             </table>
           </div>
           <div className="pokemon-abilities">
-            {normalAbility && (
-              <PokemonAbility abilityName={formatName(normalAbility.ability.name)} />
-            )}
-            {hiddenAbility && (
-              <PokemonAbility abilityName={formatName(hiddenAbility.ability.name)} />
-            )}
+            {normalAbility && <PokemonAbility abilityName={formatName(normalAbility.ability.name)} />}
+            {hiddenAbility && <PokemonAbility abilityName={formatName(hiddenAbility.ability.name)} />}
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default PokemonPage;
+export default PokemonPage
