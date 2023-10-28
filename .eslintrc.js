5da35e1c42206ee61b4ec191d91935a823c864e0
Ren0xx <adamzaleski00@gmail.com>
2023-10-28 12:52:45 +0200

    Disabling linebreak eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     plugins: ["react", "jest"],
     rules: {
         indent: ["error", 2],
-        "linebreak-style": ["error", "unix"],
+        // "linebreak-style": ["warning", "unix"],
         quotes: ["error", "single"],
         semi: ["error", "never"],
         eqeqeq: "error",
