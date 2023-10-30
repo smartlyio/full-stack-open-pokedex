d88bb4495c5bca3525eb619363c1582633cd396d
oxana-sh <121@jazz.su>
2023-10-30 08:53:23 +0300

    Add .github/workflows/hello.yml

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '3jyxxn',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
