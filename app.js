a6e9499cf141778c3b391dfd7a7348dee4c58e36
Lim Si Yong <142472330+NemoDeFish@users.noreply.github.com>
2023-11-01 19:10:25 +0800

    Simulate deployment error

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
+app.get('/health',(req, res) => {
   res.send('ok')
 })
 
 app.get('/version', (req,res) => {
-  res.send('8') // change this string to ensure a new version deployed
+  res.send('9') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
