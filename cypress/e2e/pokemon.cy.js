d14145438d07ac818f27d775301e1fb0d520ce28
oxana-sh <121@jazz.su>
2023-11-01 07:13:07 +0300

    node 1 127.0.0.1:5000

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -1,7 +1,7 @@
 /* global cy */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://127.0.0.1:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
@@ -9,7 +9,7 @@ describe('Pokedex', function() {
 
 describe('Pokedex', function() {
   it('can navigate from the main page to a specific Pokemon page', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://127.0.0.1:5000')
     cy.contains('ivysaur').click() // Нажать на ссылку ivysaur
     cy.contains('chlorophyll') // Проверить наличие текста 'chlorophyll' на странице покемона
   })
