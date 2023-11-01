3ecb809c70b4085f3fc6f25459572f8103497fb8
oxana-sh <121@jazz.su>
2023-11-01 09:42:04 +0300

    Change service

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const app = express()
 
 // Порт задается переменной окружения PORT, если она установлена, или 5000 по умолчанию
 const PORT = process.env.PORT || 5000
-// Хост задается переменной окружения HOST, если она установлена, или "localhost" по умолчанию
-const HOST = process.env.HOST || '127.0.0.1'
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://${HOST}:${PORT}`)
+  console.log(`Server started on port ${PORT}`)
 })
