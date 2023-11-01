046a3bac48cdc805adee0c3686f8689f16dbff5d
oxana-sh <121@jazz.su>
2023-11-01 11:30:36 +0300

    Fly Deploy https://silent-hill-4231.fly.dev/

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
