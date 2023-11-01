ca169d433f1bd8ac5e8d47e52b46e706ed3ca223
oxana-sh <121@jazz.su>
2023-11-01 06:05:00 +0300

    Check if the server >> >>

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // Порт задается переменной окружения PORT, если она установлена, или 5000 по умолчанию
 const PORT = process.env.PORT || 5000
 // Хост задается переменной окружения HOST, если она установлена, или "localhost" по умолчанию
-const HOST = process.env.HOST || 'localhost'
+const HOST = process.env.HOST || '0.0.0.0'
 
 app.use(express.static('dist'))
 
