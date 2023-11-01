326a1c1adc41c69422d653c054c009210c89f3fd
oxana-sh <121@jazz.su>
2023-11-01 06:51:28 +0300

    node 18

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Порт задается переменной окружения PORT, если она установлена, или 5000 по умолчанию
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 // Хост задается переменной окружения HOST, если она установлена, или "localhost" по умолчанию
 const HOST = process.env.HOST || '127.0.0.1'
 
