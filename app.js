b5d0ae0693d3a88dde4c176d6fc6e1d50c8d1e96
Lim Si Yong <142472330+NemoDeFish@users.noreply.github.com>
2023-11-02 00:52:08 +0800

    Fixed deployment error

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req,res) => {
-  res.send('16') // change this string to ensure a new version deployed
+  res.send('17') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
