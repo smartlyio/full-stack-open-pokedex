cde2dff8875a5cfb13f78923a1423a7bf1f6e76d
oxana-sh <121@jazz.su>
2023-11-01 06:09:36 +0300

    Server start 127.0.0.1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 // Порт задается переменной окружения PORT, если она установлена, или 5000 по умолчанию
 const PORT = process.env.PORT || 5000
 // Хост задается переменной окружения HOST, если она установлена, или "localhost" по умолчанию
-const HOST = process.env.HOST || '0.0.0.0'
+const HOST = process.env.HOST || '127.0.0.1'
 
 app.use(express.static('dist'))
 
