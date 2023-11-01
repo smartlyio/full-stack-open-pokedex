d6f749ea380b4bc559571b079f87e176837f66bb
oxana-sh <121@jazz.su>
2023-11-01 11:37:20 +0300

    Fly Deploy https://silent-hill-4231.fly.dev/

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 // Порт задается переменной окружения PORT, если она установлена, или 5000 по умолчанию
 const PORT = process.env.PORT || 8080
 
@@ -8,10 +14,4 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
-})
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
-app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
 })
