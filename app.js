c51b06c3972f0666032a11f37104ee5a39ef499e
oxana-sh <121@jazz.su>
2023-11-01 09:45:17 +0300

    Change service

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Порт задается переменной окружения PORT, если она установлена, или 5000 по умолчанию
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
