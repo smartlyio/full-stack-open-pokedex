56c53b5d6a3810dd434a23ee9440f30574092ecd
oxana-sh <121@jazz.su>
2023-11-02 10:55:13 +0300

    Ser

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express()
 // Порт задается переменной окружения PORT, если она установлена, или 5000 по умолчанию
 const PORT = process.env.PORT || 8080
 
-app.get('/health', (req, res) => {
-  res.status(200).send('ok');
-});
+//app.get('/health', (req, res) => {
+ // res.status(200).send('ok');
+//});
 
 app.use(express.static('dist'))
 
