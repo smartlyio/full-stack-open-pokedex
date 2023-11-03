ebbd5096b3ccb05ee9ed541d9f7838273198906f
oxana-sh <121@jazz.su>
2023-11-03 09:11:40 +0300

    Deploy  RENDER

--- build_step.sh
+++ build_step.sh
@@ -4,6 +4,9 @@ echo "Build script"
 # Установка зависимостей
 npm install
 # Сборка приложения
+# Установка webpack-cli без запросов
+npm install -g webpack-cli --yes
+
 npm run build
 # Установка порта и запуск приложения
 PORT=8080 npm run start-prod
