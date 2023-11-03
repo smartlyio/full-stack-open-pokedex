bf0c512fbf3e5bb26a7955de1bdce3d6fd82ab9e
Slimani Imed Eddine Abderrahmane <imeds2000@gmail.com>
2023-11-03 11:55:21 +0100

    add health check endpoint

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get('/version', (req, res) => {
 	res.send('1') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+	res.send('ok')
+})
+
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
