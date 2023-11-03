4f1873213346f612ec05ce980411803b88e84462
Slimani Imed Eddine Abderrahmane <imeds2000@gmail.com>
2023-11-03 01:05:29 +0100

    second e2e test passes

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,3 +5,11 @@ describe('Pokedex', function () {
 		cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
 	})
 })
+
+describe('Pokemon Navigation Test', () => {
+	it('should navigate to Ivysaur\'s page and check for content', () => {
+		cy.visit('http://localhost:5000')
+		cy.contains('ivysaur').click()
+		cy.contains('chlorophyll').should('exist')
+	})
+})
