f8445fee40e912fdde00d8188203a030da910550
oxana-sh <121@jazz.su>
2023-11-03 08:40:51 +0300

    Deploy  RENDER

--- build_step.sh
+++ build_step.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 echo "Build script"
-
 # Установка зависимостей
 npm install
-
-
 # Сборка приложения
 npm run build
-
 # Установка порта и запуск приложения
 PORT=8080 npm run start-prod
