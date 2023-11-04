2029b441edfea2dea838e87e92a26bf551c80183
oxana-sh <121@jazz.su>
2023-11-04 07:47:30 +0300

    Test script deploy

--- build_step.sh
+++ build_step.sh
@@ -2,13 +2,5 @@
 
 echo "Build script"
 
-# Установка зависимостей
-npm install
-
-yarn add -D webpack-cli@4.4.0
-
-# Сборка приложения
-npm run build
-
-# Установка порта и запуск приложения
-PORT=8080 node app.js
+webpack --mode production
+node app.js
