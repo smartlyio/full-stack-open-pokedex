fae8bf04648db35bedd46d19e5e8ceb8547690ab
Ericko Manarivo <manarivoericko4@gmail.com>
2023-11-04 11:28:19 +0300

    11.11 Health check and rollback

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') 
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 3000')
