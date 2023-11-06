f368c40ee57df024c2826a5c25c1fbcf7771b804
Daniele Castagna <daniele.castagna@outlook.com>
2023-11-06 11:51:26 +0200

    11.9 testi

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
+    "cypress/globals": true,
     "node": true
   },
   "extends": [
@@ -17,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
