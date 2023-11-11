41b85af893ec74ae8dcab5dda53e3c25a52cc11c
Adam Wozniak <adamwozhere@gmail.com>
2023-11-11 18:12:47 +0000

    simulate broken deployment (/health endpoint)

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('1.1') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
+  // simulate broken deployment
+  throw 'error...healthcheck'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
