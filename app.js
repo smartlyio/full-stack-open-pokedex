c920213ec3b7272a261c858d55306ee4f44eb903
Henri Forss <hforss@gmail.com>
2023-11-19 18:08:48 +0200

    11.11

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
+  // throw 'error...'
+  // // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('7') // change this string to ensure a new version deployed
+  res.send('8') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
