9d4199d35b80d015934120092ee006bb88f3954c
joan-kii <joan.sb@hotmail.com>
2023-11-22 05:10:22 +0100

    add exercise 11.6

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-if (typeof document !== 'undefined') {
-  ReactDOM.render(<App />, document.getElementById('app'))
-}
+ReactDOM.render(<App />, document.getElementById('app'))
