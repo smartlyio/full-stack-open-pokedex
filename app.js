32d704f7ae916e9ecf3be5e5cf45ac843481cb9f
Akseli Harkki <akseli.harkki@joinex.com>
2023-11-23 15:33:17 +0200

    version 6

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('5') // change this string to ensure a new version deployed
+  res.send('6') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
