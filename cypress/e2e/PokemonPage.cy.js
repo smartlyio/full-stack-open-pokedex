8a3629858ee5345bea2539368d5b5f42960b45ef
joan-kii <joan.sb@hotmail.com>
2023-11-24 04:49:23 +0100

    add exercise 11.9

--- cypress/e2e/PokemonPage.cy.js
+++ cypress/e2e/PokemonPage.cy.js
@@ -1,7 +1,17 @@
+/* eslint-disable no-undef */
+
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('pokemon card can be open', function() {
+    cy.get('.list-item').contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
