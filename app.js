49a535788b4105756dfcedfc8b7479be5e4e55cb
joan-kii <joan.sb@hotmail.com>
2023-11-24 05:33:52 +0100

    add exercise 11.10

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const process = require('process')
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+/* eslint-disable no-unused-var */
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
---
+++ build_step.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Build script"
+
+npm install
+npm run build
