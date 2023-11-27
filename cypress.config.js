d022d46a7f90cd10b0c20f416c91dba373465df0
Henry Palonen <henry.palonen@alsus.fi>
2023-11-27 15:42:09 +0200

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'cypress/integration/**/*.{js,jsx}'
+    specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}'
   },
 })
 
