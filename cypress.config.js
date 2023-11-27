52faf26ba447a8a0b458dbe720f3934abacf16f8
Henry Palonen <henry.palonen@alsus.fi>
2023-11-27 13:37:09 +0200

    e2e tests updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/integration/**/*.{js,jsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
