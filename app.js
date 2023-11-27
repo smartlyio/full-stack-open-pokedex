b7f1fb5891979fc148d5380ec91472a0c39d1bb5
HoangCaesar <vu.nguyen@kpmg.fi>
2023-11-27 12:05:04 +0200

    Ex 11.12: health check

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
