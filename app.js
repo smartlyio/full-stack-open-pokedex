0fe235f525950e8034b7663b8a2b78252d0c088d
Regmiram <chry.rgm@gmail.com>
2023-11-28 17:11:15 +0545

    health check api

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('1.1') // change this string to ensure a new version deployed
+})
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 app.use(express.static('dist'))
 
