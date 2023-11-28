57e337352acd4e0ae9422037976cab6be2fb99e5
Regmiram <chry.rgm@gmail.com>
2023-11-28 13:12:27 +0545

    Fix the code

--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -1,41 +1,47 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import axiosMock from 'axios'
-import { act } from 'react-dom/test-utils'
-import '@testing-library/jest-dom/extend-expect'
-import App from '../src/App'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import axiosMock from "axios";
+import { act } from "react-dom/test-utils";
+import "@testing-library/jest-dom/extend-expect";
+import App from "../src/App";
 
-jest.mock('axios')
+jest.mock("axios");
 
-describe('<App />', () => {
-  it('fetches data', async () => {
-    axiosMock.get.mockResolvedValueOnce(
-      {
-        data: {
-          results: [{ url: 'https://pokeapi.co/api/v2/pokemon/1/', name: 'bulbasaur', id: 1 }]
-        }
-      }
-    )
+describe("<App />", () => {
+  it("fetches data", async () => {
+    axiosMock.get.mockResolvedValueOnce({
+      data: {
+        results: [
+          {
+            url: "https://pokeapi.co/api/v2/pokemon/1/",
+            name: "bulbasaur",
+            id: 1,
+          },
+        ],
+      },
+    });
     await act(async () => {
-      render(<App />)
-    })
-    expect(axiosMock.get).toHaveBeenCalledTimes(1)
-    expect(axiosMock.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/?limit=50')
-  })
+      render(<App />);
+    });
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(axiosMock.get).toHaveBeenCalledWith(
+      "https://pokeapi.co/api/v2/pokemon/?limit=50"
+    );
+  });
 
-  it('shows LoadingSpinner', async () => {
-    axiosMock.get.mockResolvedValueOnce({})
+  it("shows LoadingSpinner", async () => {
+    axiosMock.get.mockResolvedValueOnce({});
     await act(async () => {
-      const { getByAltText } = render(<App />)
-      expect(getByAltText('Loading...')).toBeVisible()
-    })
-  })
+      const { getByAltText } = render(<App />);
+      expect(getByAltText("Loading...")).toBeVisible();
+    });
+  });
 
-  it('shows error', async () => {
-    axiosMock.get.mockRejectedValueOnce(new Error())
+  it("shows error", async () => {
+    axiosMock.get.mockRejectedValueOnce(new Error());
     await act(async () => {
-      render(<App />)
-    })
-    expect(screen.getByTestId('error')).toBeVisible()
-  })
-})
+      render(<App />);
+    });
+    expect(screen.getByTestId("error")).toBeVisible();
+  });
+});
