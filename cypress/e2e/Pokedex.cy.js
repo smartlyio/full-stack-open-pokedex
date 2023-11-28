a98b538d2ee2827bdd84fe0c2adb94bf0a6df4df
Regmiram <chry.rgm@gmail.com>
2023-11-28 15:01:03 +0545

    Simple end to end tests

--- cypress/e2e/Pokedex.cy.js
+++ cypress/e2e/Pokedex.cy.js
@@ -5,3 +5,15 @@ describe('Pokedex', function () {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+describe('Pokemon Page can be navigated to', () => {
+  it('Navigates to Ivysaur page and checks for chlorophyll ability', () => {
+    // Visit the main page
+    cy.visit('http://localhost:5000/')
+
+    // Find and click on the link for Ivysaur
+    cy.contains('ivysaur').click()
+
+    // Wait for the page to load and check for the presence of chlorophyll ability
+    cy.contains('chlorophyll')
+  })
+})
