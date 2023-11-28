d0b311bcbe301f385af7d64f22e56c236c485eb4
Regmiram <chry.rgm@gmail.com>
2023-11-28 21:17:59 +0545

    diploy check

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 app.get('/version', (req, res) => {
-  res.send('1.1') // change this string to ensure a new version deployed
+  res.send('1.3 diploy check') // change this string to ensure a new version deployed
 })
 app.get('/health', (req, res) => {
   res.send('ok')
