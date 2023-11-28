bc27a848e5d49b5d317e503abe1b5f9d6ad793f4
Henry Palonen <henry.palonen@alsus.fi>
2023-11-28 14:28:37 +0200

    Deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,7 @@ module.exports = {
     "node": true
   },
   "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
+    "eslint:recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
