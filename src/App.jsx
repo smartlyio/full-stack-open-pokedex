242bd6d511c4328a313c535b98a9fa309ca55382
Henry Palonen <henry.palonen@alsus.fi>
2023-11-28 14:52:20 +0200

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+//import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { useApi } from './useApi'
 import LoadingSpinner from './LoadingSpinner'
