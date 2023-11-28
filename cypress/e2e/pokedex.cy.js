c7b2216d58be7ea48bb3a73a1c3148f4c0c42d66
Lachlan Young <lachie.s.young@gmail.com>
2023-11-28 21:16:14 +1100

    question 11.9, part 2

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,3 +5,12 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Navigation Test', () => {
+  it('navigates from the main page to Ivysaur page', () => {
+    cy.visit('http://localhost:3000')
+    cy.contains('a', 'ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.contains('chlorophyll').should('exist')
+  })
+})
