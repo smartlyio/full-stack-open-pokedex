dd646af5fa8856b2162231348d023b83e2697685
Lachlan Young <lachie.s.young@gmail.com>
2023-11-30 18:35:57 +1100

    question 11.11 - health check error

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  throw 'test error...'
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
