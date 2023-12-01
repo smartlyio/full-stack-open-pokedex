26f85c158068f4680f2ade1f26e5d0f428b2af48
juandavid015 <juandavidgr1002@gmail.com>
2023-12-01 14:04:01 -0500

    update: correcting lint issues

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -4,20 +4,15 @@ import LoadingSpinner from './LoadingSpinner'
 import { useApi } from './useApi'
 import PokemonAbility from './PokemonAbility'
 import ErrorMessage from './ErrorMessage'
-
 const formatName = (nameWithDash) => nameWithDash.replace('-', ' ')
-
 const PokemonPage = ({ previous, next }) => {
   const { name } = useParams()
   const { data: pokemon, error, isLoading } = useApi(`https://pokeapi.co/api/v2/pokemon/${name}`)
-
   if (isLoading) {
     return <LoadingSpinner />
-  }
-  if (error) {
+  } if (error) {
     return <ErrorMessage error={error} />
   }
-
   const { type } = pokemon.types.find((type) => type.slot === 1)
   const stats = pokemon.stats.map((stat) => ({
     name: formatName(stat.stat.name),
@@ -25,7 +20,7 @@ const PokemonPage = ({ previous, next }) => {
   })).reverse()
   const normalAbility = pokemon.abilities.find((ability) => !ability.is_hidden)
   const hiddenAbility = pokemon.abilities.find((ability) => ability.is_hidden === true)
-
+  // eslint-disable-next-line no-console
   console.log('hiddenAbility=', hiddenAbility)
   return (
     <>
