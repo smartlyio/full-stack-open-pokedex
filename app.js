bc2c1b8becd6c0399a770612bdb95044108056b2
kukko100 <karim_el-zaben@hotmail.com>
2023-12-02 14:41:52 +0200

    linted code

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
-});
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
+})
