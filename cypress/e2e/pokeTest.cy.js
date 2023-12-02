d92310372e26ab6f1ebae4644733d556556921e6
kukko100 <karim_el-zaben@hotmail.com>
2023-12-02 17:12:35 +0200

    finished exrercise 11.9, everything works

--- cypress/e2e/pokeTest.cy.js
+++ cypress/e2e/pokeTest.cy.js
@@ -6,3 +6,11 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Navigation to Pokémon Page', function() {
+  it('can navigate to Ivysaur page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll').should('be.visible')
+  })
+})
