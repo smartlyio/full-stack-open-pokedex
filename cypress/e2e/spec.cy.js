9ac8d8bcf9256a6b9c3da2c994ada57b43d36a15
Tadesse Alemayehu <tadessedev@gmail.com>
2023-12-06 11:01:55 +0300

    Update cypress test to test google

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,9 @@
 describe("Pokedex", function () {
   it("front page can be opened", function () {
-    cy.visit("http://localhost:8080");
-    cy.contains("ivysaur");
-    cy.contains(
-      "Pokémon and Pokémon character names are trademarks of Nintendo."
-    );
+    cy.visit("https://www.google.com/");
+    cy.contains("Google");
+    // cy.contains(
+    //   "Pokémon and Pokémon character names are trademarks of Nintendo."
+    // );
   });
 });
