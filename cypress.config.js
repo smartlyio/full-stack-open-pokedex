be48fb78f04717cc24c3880bb51bb2c0a4cf3fb7
Tadesse Alemayehu <tadessedev@gmail.com>
2023-12-06 11:16:35 +0300

    update security rules to cypress.json

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    chromeWebSecurity: false,
+    chromeArgs: ["--disable-web-security", "--ssl-version-min=tls1.2"],
   },
-  chromeWebSecurity: false,
-  chromeArgs: ["--disable-web-security", "--ssl-version-min=tls1.2"],
 });
