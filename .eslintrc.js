867d04152e79c5c12c5d533978fbf673a4f3c39f
Unomars4 <une.nyide@gmail.com>
2023-12-08 15:08:49 +0200

    add build and test to deployment workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,10 @@ module.exports = {
             "never"
         ],
         "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
         "arrow-spacing": [
             "error", {"before": true, "after": true}
         ],
