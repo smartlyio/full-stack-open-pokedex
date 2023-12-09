53d1def76eed2d5bb41930b4850edd4f614a8268
Reinhard Fabian <reinhard.fabian@gmail.com>
2023-12-09 18:22:21 +0100

    Exercise 11.9. linting failed

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
