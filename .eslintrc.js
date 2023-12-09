420ba2a80eb2d8a70ca0ce592194d671e783dc81
Reinhard Fabian <reinhard.fabian@gmail.com>
2023-12-09 17:21:44 +0100

    Exercise 11.6.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
+    'node': true,
     "jest/globals": true
   },
   "extends": [
@@ -43,7 +44,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": 0,
     "react/prop-types": 0
   }
 }
