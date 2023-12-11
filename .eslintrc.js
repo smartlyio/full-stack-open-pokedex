b199b1831fd6484ffbe085643e36ac17d812e8cf
Unomars4 <une.nyide@gmail.com>
2023-12-11 17:16:10 +0200

    fix pipeline workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
     "env": {
         "node": true,
         "es6": true,
-        "jest/globals": true,
-        "cypress/globals": true
+        "jest/globals": true
     },
     "extends": [
         "eslint:recommended",
