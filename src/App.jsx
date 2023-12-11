246718ecadba829a55b5ca9b9ec2f5bba51c9714
Juho Siitonen <juho_tuomas@msn.com>
2023-12-11 03:25:38 +0200

    repaired lint suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <PokemonList pokemonList={pokemonList} />
         </Route>
         <Route path="/pokemon/:name" render={(routeParams) => {
