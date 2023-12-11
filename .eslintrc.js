0777f0c01b43965a4cca62942b40e1848be095e2
Unomars4 <une.nyide@gmail.com>
2023-12-11 17:10:12 +0200

    add delployment to pipeline workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "env": {
         "node": true,
         "es6": true,
-        "jest/globals": true
+        "jest/globals": true,
+        "cypress/globals": true
     },
     "extends": [
         "eslint:recommended",
