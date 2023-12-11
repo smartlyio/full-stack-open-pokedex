a029371df15214d381b418aef784826bdd6b9a38
Reinhard Fabian <reinhard.fabian@gmail.com>
2023-12-11 12:42:07 +0100

    Exercise 11.11.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
+const { version } = require('./package.json')
+
 const PORT = process.env.PORT || 5000
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send(version)
+})
 
 app.use(express.static('dist'))
 
