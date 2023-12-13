c839be02f2f331faa0c1afad94fdade0039d5ca6
Adrián Ramos <ramosadrian@pm.me>
2023-12-13 15:51:56 -0300

    Add health check path

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
