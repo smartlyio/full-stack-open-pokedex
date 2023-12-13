3df7b1f3d9b0ef1dce65011ce5f9bd924938e4b4
A. Urquiola <patchamama.go@gmail.com>
2023-12-13 21:49:24 +0100

    updated port to pass tests

--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -2,7 +2,7 @@
 
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:5001')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
@@ -10,7 +10,7 @@ describe('Pokedex', function () {
   })
 
   it('pokemon page can be navigated to', function () {
-    cy.visit('http://localhost:5001')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
