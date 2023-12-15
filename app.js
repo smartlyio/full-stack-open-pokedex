48602ed4acd7852f6f761d4ef1ec78b627918592
Juho Siitonen <juho_tuomas@msn.com>
2023-12-15 02:25:57 +0200

    error in health check

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
