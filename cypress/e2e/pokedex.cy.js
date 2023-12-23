38679590a2de031246eb1ce8bfe564cd2d6a9d69
cbernard10 <clement.bernard@protonmail.com>
2023-12-23 18:02:35 +0100

    cypress update

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,13 @@ describe("Pokedex", function () {
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
+
+  it("pokemon page can be navigated to", function () {
+    cy.visit("http://localhost:5000");
+    cy.contains("ivysaur").click();
+    cy.contains("chlorophyll");
+    cy.contains("overgrow");
+    cy.contains("ivysaur");
+    cy.contains("Previous") && cy.contains("Next");
+  });
 });
