3ec255ed40dba0edd972f2ef729ea7e9d1d15dca
cbernard10 <clement.bernard@protonmail.com>
2023-12-23 19:14:25 +0100

    updated dpeloy settings

--- app.js
+++ app.js
@@ -11,3 +11,12 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log("server started on port 5000");
 });
+
+app.get("/health", (req, res) => {
+  throw new Error("oops");
+  res.send("ok");
+});
+
+app.get("/version", (req, res) => {
+  res.send("v7 deployed");
+});
