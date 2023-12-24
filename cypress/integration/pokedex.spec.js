02053fdf5ad4e6ea5dd8e9e258588fcbb24764c0
RyKizil <eray_kizilmese@hotmail.com>
2023-12-24 16:52:59 +0100

    test deploy

--- cypress/integration/pokedex.spec.js
+++ cypress/integration/pokedex.spec.js
@@ -5,10 +5,11 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
-
 describe('Pokedex', function() {
   it('front page can be opened', function() {
     // eslint-disable-next-line no-undef
-
+    /*cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')*/
   })
 })
