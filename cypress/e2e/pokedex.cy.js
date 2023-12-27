9f148d44c79969c0fc2257afe2afbd8216c4f64e
Magellan <ppetrov12@abv.bg>
2023-12-27 23:23:24 +0200

    11.9

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('can navigate from main page to a pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('butterfree').click()
+    cy.contains('tinted lens')
+  })
 })
