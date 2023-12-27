62918572979ab4e2ef361054f8f479a6bb957e93
eams1798 <eams1798@gmail.com>
2023-12-27 01:02:22 -0500

    setting Cypress in the workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'jest/globals': true
+    'jest/globals': true,
+    'cypress/globals': true
   },
   'extends': [
     'eslint:recommended',
@@ -18,7 +19,7 @@ module.exports = {
     'sourceType': 'module'
   },
   'plugins': [
-    'react', 'jest'
+    'react', 'jest', 'cypress'
   ],
   'rules': {
     'indent': [
