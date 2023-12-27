12e566cf06ced881e333873c2737695a931e6005
Marc Bäckman <marcbackman.lapinamk@gmail.com>
2023-12-27 02:05:55 +0200

    navigation test

--- cypress/e2e/pokemon.cy.js
+++ cypress/e2e/pokemon.cy.js
@@ -7,3 +7,15 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Navigation', () => {
+  it('should navigate to Ivysaur page', () => {
+    cy.visit('http://localhost:5000');
+
+    cy.contains('ivysaur').click();
+
+    // Validate that the page has loaded successfully
+    cy.url().should('include', '/pokemon/ivysaur');
+    cy.get('.pokemon-abilities').should('contain', 'chlorophyll');
+  });
+});
