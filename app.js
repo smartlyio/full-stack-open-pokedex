4986c488a4883f457dbd168a538cdde5a3ef1bc9
Magellan <ppetrov12@abv.bg>
2023-12-28 00:35:34 +0200

    add endpoint for version control

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string when deploying new ver
+})
