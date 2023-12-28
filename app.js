f56bb781d313d78c2c0deeadb8505f91ca12b23d
Marc Bäckman <marcbackman.lapinamk@gmail.com>
2023-12-28 04:05:24 +0200

    weird fix

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
