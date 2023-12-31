46050b3392227bdd19e2de2fc80d22d25c9c6884
jarilappalainen <jari.lappalainen@iki.fi>
2023-12-31 13:12:11 +0200

    turn off many warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
+    "no-console": "off",
     "react/prop-types": 0
   }
 }
