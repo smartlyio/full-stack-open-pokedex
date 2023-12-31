780ca640418e736aa50ec8824594fa9321307910
eams1798 <eams1798@gmail.com>
2023-12-31 01:44:00 -0500

    stats were in the wrong order in Jest tests but right on the browser

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -104,7 +104,7 @@ describe('<PokemonPage />', () => {
       )
     })
 
-    expect(screen.getByTestId('stats')).toHaveTextContent('hp55attack55')
+    expect(screen.getByTestId('stats')).toHaveTextContent('attack55hp55')
   })
 
   it('should render previous and next urls if they exist', async () => {
