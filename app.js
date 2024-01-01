b92e34fa03db1a1c661cac9f6291c94b72098d7d
Karel Parkkola <karel.parkkola@gmail.com>
2024-01-01 15:36:54 +0200

    Fix health check

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/health', (req, res) => {
-  res.status(500).send('error')
-  //res.send('ok')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
