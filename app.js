38c076c5db4ba014d9884dfc8bf43b65ff394006
Karel Parkkola <karel.parkkola@gmail.com>
2024-01-01 15:14:44 +0200

    Health check

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
