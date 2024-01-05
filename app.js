b013fe4cb394c92a8efc89d281fefd4fbdd229d1
Tero Kilpeläinen <degenone1@gmail.com>
2024-01-05 14:23:24 +0200

    deployment should happen

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/api/version', (req, res) => {
-  res.send('1.0.0');
+  res.send('1.0.0')
 })
 
 app.listen(PORT, () => {
