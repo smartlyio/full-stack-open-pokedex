4c65fa83e690ee4fb2931ce73b8fd8e388247545
viksy <viktorasenov006@gmail.com>
2024-01-05 18:09:15 +0200

    fixed lint pipeline

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   "env": {
-    "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
