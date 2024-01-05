f7c635463da1d4c8663ed12e79bafbcca02e476d
Tero Kilpeläinen <degenone1@gmail.com>
2024-01-05 14:21:49 +0200

    deployment should not happen due to linting errors

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/api/version', (req, res) => {
+  res.send('1.0.0');
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`server started on port ${PORT}`)
 })
