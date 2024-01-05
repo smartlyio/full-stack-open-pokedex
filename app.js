e43a1a0849c18c21ae81708857295e6c00e5aaf3
Tero Kilpeläinen <degenone1@gmail.com>
2024-01-05 14:40:33 +0200

    testing failed health check

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1.0.0')
+  res.send('1.0.1')
 })
 
 app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
