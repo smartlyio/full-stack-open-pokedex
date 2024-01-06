5bca8b19bf3023002129c7700e7d326738c5a058
viksy <viktorasenov006@gmail.com>
2024-01-06 04:36:10 +0200

    eslint fix

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/version', (req, res) => {
+app.get('/version', (_req, res) => {
   res.send('1')
 })
 
+// eslint-disable-next-line no-unused-vars
 app.get('/health', (req, res) => {
   throw 'error...'
 })
