b0c94ee6b20f336a99d0b200cca95a01accd5a89
Michael Maffie <mikemaffie@gmail.com>
2024-01-07 16:42:09 -0500

    test if render works

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.createRoot(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
