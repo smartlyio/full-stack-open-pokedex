5d9e445f2cf905a8f537e5387f350495367a83c5
Michael Maffie <mikemaffie@gmail.com>
2024-01-07 16:46:37 -0500

    fix: change reactDom for React 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.createRoot(<App />, document.getElementById('app'))
