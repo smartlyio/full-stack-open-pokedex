adf8e46c3c0cb4b6b76378fcb0dfdf45deefe15c
Michael Maffie <mikemaffie@gmail.com>
2024-01-07 17:02:54 -0500

    test: add test to navigate to pokemon

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -3,5 +3,7 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5001')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.contains('charmander').click()
+    cy.contains('charmander')
   })
 })
