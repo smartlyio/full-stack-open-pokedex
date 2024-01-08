12143582bfe15acad1c87b01c198d01f1aae2779
miahro <mikko.ahro@gmail.com>
2024-01-08 16:24:35 +0200

    Ex 11.9 part 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    "node": true
+    "node": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
@@ -17,7 +18,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
