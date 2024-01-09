ee0144999779d7267bed7cfad2bd9a5fe65e6d1d
crawwwler <shahinkopite@gmail.com>
2024-01-09 23:57:31 +0330

    ex.11. fixed w bug

--- routers/checks.js
+++ routers/checks.js
@@ -1,7 +1,6 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
 
-router('/health', (request, response) => {
+router.get('/health', (request, response) => {
   response.send('OK')
 })
 
