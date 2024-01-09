de25edbcb5438297d1bc05b5648322aac578e898
Sami <samialeksi.tuomela@gmail.com>
2024-01-09 19:38:03 +0200

    testing if tag updates with changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/health', (req, res) => {
-  res.send('ok new branch fr')
+  res.send('ok tag testin')
 })
 
 app.listen(PORT, () => {
