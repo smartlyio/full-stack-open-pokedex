363ec05330b388d9046f477927fc377e4a1fd174
crawwwler <shahinkopite@gmail.com>
2024-01-09 23:47:20 +0330

    ex.11. fixed

--- routers/checks.js
+++ routers/checks.js
@@ -2,9 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router('/health', (request, response) => {
-  throw new Error('ERROR')
-
-  // eslint-disable-next-line no-unreachable
   response.send('OK')
 })
 
