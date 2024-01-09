19526b299a70b590addf1b6fc9c7387c459eca48
Guillermo <gbiatturi@gmail.com>
2024-01-09 20:57:59 -0300

    Exercise 11.12

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('1')
+  res.send('2')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
