2029474049ff0465edb9605f9e3f6f8cd46aa523
miahro <mikko.ahro@gmail.com>
2024-01-10 10:13:05 +0200

    Ex 11.13

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1.01') // change this string to ensure a new version deployed
+  res.send('1.2') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, '0.0.0.0' ,() => {
