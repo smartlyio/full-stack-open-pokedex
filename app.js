b8b274a5c29a31b119bbc53d9b4d332e15dd3f36
Sami <samialeksi.tuomela@gmail.com>
2024-01-10 15:44:44 +0200

    second line with the #skip

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 app.get('/health', (req, res) => {
-  res.send('ok merge only tag testin')
+  res.send('ok merge only tag testing')
 })
 
 app.listen(PORT, () => {
