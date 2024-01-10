f676da30320e17bb6749ef6c88385644ba0e4c7c
yg <eng.yg@yahoo.com>
2024-01-10 21:23:55 +0800

    11.6 Fix the code

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,12 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
-    "react/prop-types": 0
+    "no-console": "off",
+    "react/prop-types": 0,
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    }
   }
 }
