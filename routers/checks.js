886bf55ea36f87f0db2becb26e47c5015382e7aa
crawwwler <shahinkopite@gmail.com>
2024-01-11 02:28:08 +0330

    rollback testing behaviour 1

--- routers/checks.js
+++ routers/checks.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/health', (request, response) => {
-  response.status(500).json({ error: 'internal error' })
+  response.send('OK')
 })
 
 module.exports = router
