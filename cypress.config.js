9144f244fc44ab99148985bb8e744840b1e55e95
yg <eng.yg@yahoo.com>
2024-01-11 11:08:48 +0800

    cypress config change lint

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
+
+
 
 module.exports = defineConfig({
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     supportFile: false
+
   },
-});
+
+})
+
