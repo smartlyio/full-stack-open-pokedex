d213074a4d13f3aeb60de14b8febd23b0a9976ab
yg <eng.yg@yahoo.com>
2024-01-11 12:48:53 +0800

    Added cypress - pokemon page can be navigated to

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -3,5 +3,12 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('pokemon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+    cy.contains('80')
   })
 })
