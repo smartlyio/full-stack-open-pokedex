0a363b204605deeb9cef6742a3d2f054452979a3
Pulkit <kpulkit15234@gmail.com>
2024-01-14 22:48:50 +0530

    p11q9 cy testing for navigation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,10 +3,22 @@ describe('Pokedex', function () {
     // eslint-disable-next-line
     cy.visit('http://localhost:5000/')
     // eslint-disable-next-line
-    cy.contains("ivysaur")
+    cy.contains('ivysaur')
     // eslint-disable-next-line
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+  it('navigate from the main page to the page of a particular Pokemon', function () {
+    // eslint-disable-next-line
+    cy.visit('http://localhost:5000/')
+    // eslint-disable-next-line
+    cy.get('#ivysaur').click()
+    // eslint-disable-next-line
+    cy.url().should('include', 'pokemon/ivysaur')
+    // eslint-disable-next-line
+    cy.contains('chlorophyll')
+    // eslint-disable-next-line
+    cy.contains('overgrow')
   })
 })
--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -8,6 +8,7 @@ const PokemonList = ({ pokemonList }) => {
         <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
           <div
             className="list-item-name"
+            id={name}
           >
             {name}
           </div>
