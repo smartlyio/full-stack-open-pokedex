045dd3da831e01706ae384d63c2c4b7106b6014c
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-01-15 22:09:20 -0500

    Exercise 11.9 final

--- cypress/e2e/poke_dex.cy.js
+++ cypress/e2e/poke_dex.cy.js
@@ -6,4 +6,12 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+  it('pokemon page can be navigated to', function () {
+    cy.visit('http://localhost:5000/')
+    cy.contains('charmander').click()
+
+    cy.contains('Previous')
+    cy.contains('Home')
+    cy.contains('Next')
+  })
 })
