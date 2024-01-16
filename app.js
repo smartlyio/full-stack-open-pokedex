a7069c50288845c31873b65e7e454043d09f3742
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2024-01-16 13:06:39 -0500

    Add health check

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
+  res.send('ok')
+})
 app.listen(PORT, () => {
-  this_causes_error
-  // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
