e9699121a77676c9921f0263cd9a5a1e267554bd
Pulkit <kpulkit15234@gmail.com>
2024-01-17 19:37:40 +0530

    p11q12 fixing health route again

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/version', (_req, res) => {
-  res.send('2')
+  res.send('6')
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// app.get('/health', (req, res) => {
+//   // eslint-disable-next-line no-constant-condition
+//   if (true) throw ('error...  ')
+//   res.send('ok')
+// })
 
 app.use(express.static('dist'))
 
