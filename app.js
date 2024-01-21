3e6f027bd4282d836b85612d0d211bba2541a0fa
Nicolás Rodriguez <18080513+nico-rodriguez@users.noreply.github.com>
2024-01-21 13:11:23 -0300

    Add health endpoint

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
