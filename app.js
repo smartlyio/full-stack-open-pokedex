34a9ec16c69fc0785cba11ca890303a7296d1dd1
jarilappalainen <jari.lappalainen@iki.fi>
2024-01-28 17:39:59 +0200

    add health checks

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ app.use(express.static('dist'))
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
+
+app.get('/health', (req, res) => {
+  if (true) throw('error...  ')
+  res.send('ok')
+})
