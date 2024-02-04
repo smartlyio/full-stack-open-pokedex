dafcb206f32d661de3605cf5666cd3f7dffc05b3
jarilappalainen <jari.lappalainen@iki.fi>
2024-02-04 16:21:45 +0200

    try smaller fly conf

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.listen(PORT, () => {
 })
 
 app.get('/health', (req, res) => {
-  // if (res[0] !== 'x') throw('error...  ')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
 })
