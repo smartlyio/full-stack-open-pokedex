fd98c2cdfa1b6433b3eb32688aed53d01e1f3d27
jarilappalainen <jari.lappalainen@iki.fi>
2024-02-04 21:49:16 +0200

    change error text

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.listen(PORT, () => {
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
+  if (true) throw('health check returning failure')
   res.send('ok')
 })
 
