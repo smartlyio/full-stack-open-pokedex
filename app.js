85df35cb8ee5444e7c121034d702ca154235db41
kateina <katerinaveljanoskamk@gmail.com>
2024-03-04 19:50:51 +0100

    corect
    corect

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom';
 import App from './App.';
 
-// Server.js
+/* global process */
 const express = require("express");
 const app = express();
+// Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    // console.log("server started on port 5000");
 });
-
-// Client.jsx
 
 
 const root = createRoot(document.getElementById('root'));
