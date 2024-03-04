471fc5625d103145baf01b31b74dd31e04d87368
jimmy Daume <160715220+Daumejimmy@users.noreply.github.com>
2024-03-04 13:51:40 +0100

    Dateien Bearbeitung
    app.js bearbeitet
    pokemon page bearbeitet
    YML datei erstellt

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+import React from 'react';
+import { createRoot } from 'react-dom';
+import App from './App.';
+/* global process */
 const express = require("express");
 const app = express();
-
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
-
 app.use(express.static("dist"));
-
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
+// console.log("server started on port 5000");
 });
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
