ca3e0cf7fbc9abcf00dca7400fc1984dbcdb4152
hajarbakir <hajar.bakir@docc.techstarter.de>
2024-03-04 13:06:55 +0100

    Update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,48 +2,30 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "node": true,
+    "jest": true  // "jest" direkt hier hinzufügen
   },
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended"
   ],
   "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
-    },
     "ecmaVersion": 2018,
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react"
   ],
   "rules": {
-    "indent": [
-      "error",
-        2
-      ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "semi": ["error", "never"],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", {"before": true, "after": true}],
+    "no-console": "off", // Sie können dies auf "error" setzen, wenn Sie Konsolenmeldungen nicht erlauben möchten
     "react/prop-types": 0
   }
-}
+};
