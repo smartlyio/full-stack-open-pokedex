2f506840e09f6b7a4761ba8fe3bb3b4f856ee8b3
kateina <katerinaveljanoskamk@gmail.com>
2024-03-05 14:04:17 +0100

    update index.ysx
    update index.ysx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom'
 import App from './App'
 
 const root = createRoot(document.getElementById('root'));
