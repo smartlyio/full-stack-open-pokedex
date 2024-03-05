531ecd9a885d3164d2e927d4cdc4c179171613b7
kateina <katerinaveljanoskamk@gmail.com>
2024-03-05 10:58:58 +0100

    pull_request:mein.yml
    katerina

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import App from './App.';
 const express = require("express");
 const app = express();
 // Heroku dynamisch setzt einen Port
+//
 const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 app.listen(PORT, () => {
