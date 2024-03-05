341b63a4c2d029ef3c7c3643ebe4c717588bdf63
kateina <katerinaveljanoskamk@gmail.com>
2024-03-05 10:34:12 +0100

    mien.yml
    da

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom';
 import App from './App.';
-
-/* global process */
 const express = require("express");
 const app = express();
-// Heroku dynamically sets a port
+// Heroku dynamisch setzt einen Port
 const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 app.listen(PORT, () => {
-    // console.log("server started on port 5000");
+    //console.log(`Server started on port ${PORT}`);
 });
-
-
 const root = createRoot(document.getElementById('root'));
 root.render( < App / > );
