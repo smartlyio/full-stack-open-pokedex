70241a1f1071624859e910c8591e3fdddf289d28
kateina <katerinaveljanoskamk@gmail.com>
2024-03-05 12:13:37 +0100

    check status

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ createRoot(document.getElementById('root')).render( < App / > );
 
 const express = require("express");
 const app = express();
-// Heroku dynamisch setzt einen Port
-//
 const PORT = process.env.PORT || 5000;
 app.use(express.static("dist"));
 app.listen(PORT, () => {
-    // console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
