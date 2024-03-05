3aefe9a0b0ed687943d940858533f1d6ac1cdfb7
kateina <katerinaveljanoskamk@gmail.com>
2024-03-05 13:45:34 +0100

    update
    update

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import App from './App';
-
-// Instead of ReactDOM.render, use createRoot
-createRoot(document.getElementById('root')).render( < App / > );
+/* global process */
 
 const express = require("express");
 const app = express();
+
+// Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
+
 app.use(express.static("dist"));
+
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+    // console.log("server started on port 5000");
 });
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
