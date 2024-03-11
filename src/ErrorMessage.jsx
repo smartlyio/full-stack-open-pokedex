2cfaabc8234f482770c51dca5274cb292222364b
Toni Hukka <berendin@gmail.com>
2024-03-11 07:22:10 +0200

    test123

--- src/ErrorMessage.jsx
+++ src/ErrorMessage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ErrorMessage = ({ error }) => (
-  <div data-testid="error">An error occuredz3: {error.toString()}</div>
+  <div data-testid="error">An error occuredz4: {error.toString()}</div>
 )
 
 export default ErrorMessage
